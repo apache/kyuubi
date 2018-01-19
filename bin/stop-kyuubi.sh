58a9a3f998ede4fd13bcea203d7de3b2ea438f7e
Kent Yao <yaooqinn@hotmail.com>
2018-01-19 23:43:24 +0800

    add stop script

--- bin/stop-kyuubi.sh
+++ bin/stop-kyuubi.sh
@@ -25,32 +25,4 @@ set -a
 . "${KYUUBI_HOME}/bin/kyuubi-env.sh"
 set +a
 
-function usage {
-  echo "Usage: ./bin/start-kyuubi.sh <args...>"
-}
-
-if [[ "$@" = *--help ]] || [[ "$@" = *-h ]]; then
-  usage
-  exit 0
-fi
-
-## Find the Kyuubi Jar
-KYUUBI_JAR_DIR="$KYUUBI_HOME/target"
-KYUUBI_JAR_NUM="$(ls ${KYUUBI_JAR_DIR} | grep kyuubi- | grep .jar | wc -l)"
-
-if [ ${KYUUBI_JAR_NUM} = "0" ]; then
-  echo "Kyuubi Server: need to build kyuubi first. Run ./bin/mvn clean package" >&2
-  exit 1
-fi
-
-if [ ${KYUUBI_JAR_NUM} != "1" ]; then
-  echo "Kyuubi Server: duplicated kyuubi jars found. Run ./bin/mvn clean package" >&2
-  exit 1
-fi
-
-KYUUBI_JAR=${KYUUBI_JAR_DIR}/"$(ls ${KYUUBI_JAR_DIR} |grep kyuubi- | grep .jar)"
-
-echo "Kyuubi Server: jar founded:" ${KYUUBI_JAR} >&2
-
-exec "${SPARK_HOME}"/sbin/spark-daemon.sh submit ${CLASS} 1 "$@" "$KYUUBI_JAR"
-
+"${SPARK_HOME}/sbin"/spark-daemon.sh stop "$CLASS" 1
