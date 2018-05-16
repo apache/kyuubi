81490693efa71a9f2dddd411bedf95fbf9876411
Kent Yao <yaooqinn@hotmail.com>
2018-05-16 15:33:24 +0800

    make kyuubi first class loader compatible with spark's isolated class loader for hive

--- src/main/scala/yaooqinn/kyuubi/server/KyuubiServer.scala
+++ src/main/scala/yaooqinn/kyuubi/server/KyuubiServer.scala
@@ -17,6 +17,7 @@
 
 package yaooqinn.kyuubi.server
 
+import java.io.File
 import java.util.concurrent.atomic.AtomicBoolean
 
 import org.apache.hadoop.security.UserGroupInformation
@@ -110,11 +111,16 @@ object KyuubiServer extends Logging {
     conf.set("spark.hadoop.hive.cluster.delegation.token.store.class",
       "org.apache.hadoop.hive.thrift.MemoryTokenStore")
 
+    // Spark' hive metastore client uses Isolated ClassLoader for instantiation, which is conflict
+    // with [[KyuubiFirstClassLoader]] while using default builtin hive jars. This may be a Spark's
+    // bug, so we bypassing it here by directly setting it to the builtin dir.
     conf.getOption(KyuubiSparkUtil.METASTORE_JARS) match {
       case None | Some("builtin") =>
-        val sparkHome = sys.env.get("SPARK_HOME")
-        info(sparkHome.get)
-      case _ =>
+        val metastoreJars = KyuubiSparkUtil.SPARK_JARS_DIR + File.separator + "*"
+        info(s"Using Spark's bulitin ones for ${KyuubiSparkUtil.METASTORE_JARS}")
+        conf.set(KyuubiSparkUtil.METASTORE_JARS, metastoreJars)
+      case Some(other) =>
+        info(s"Using user specified ${KyuubiSparkUtil.METASTORE_JARS}=$other")
     }
     // Set missing Kyuubi configs to SparkConf
     KyuubiConf.getAllDefaults.foreach(kv => conf.setIfMissing(kv._1, kv._2))
