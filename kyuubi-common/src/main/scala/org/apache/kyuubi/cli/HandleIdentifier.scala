052adb5d2bcb2239c07166eeab65bf21fe83de69
Kent Yao <yaooqinn@hotmail.com>
2020-07-02 15:56:38 +0800

    Add OperationHandle

---
+++ kyuubi-common/src/main/scala/org/apache/kyuubi/cli/HandleIdentifier.scala
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.cli
+
+import java.nio.ByteBuffer
+import java.util.{Objects, UUID}
+
+import org.apache.hive.service.rpc.thrift.THandleIdentifier
+
+case class HandleIdentifier(publicId: UUID, secretId: UUID) {
+
+  def toTHandleIdentifier: THandleIdentifier = {
+    val guid = new Array[Byte](16)
+    val pbb = ByteBuffer.wrap(guid)
+    val secret = new Array[Byte](16)
+    val sbb = ByteBuffer.wrap(secret)
+    pbb.putLong(publicId.getMostSignificantBits)
+    pbb.putLong(publicId.getLeastSignificantBits)
+    sbb.putLong(secretId.getMostSignificantBits)
+    sbb.putLong(secretId.getLeastSignificantBits)
+    new THandleIdentifier(ByteBuffer.wrap(guid), ByteBuffer.wrap(secret))
+  }
+
+  override def hashCode(): Int = (Objects.hashCode(publicId) + 31) * 31 + Objects.hashCode(secretId)
+
+  override def equals(obj: Any): Boolean = obj match {
+    case HandleIdentifier(pid, sid) =>
+      Objects.equals(publicId, pid) && Objects.equals(secretId, sid)
+    case _ => false
+  }
+
+  override def toString: String = publicId.toString
+}
+
+object HandleIdentifier {
+  def apply(): HandleIdentifier = {
+    val publicId = UUID.randomUUID()
+    val secretId = UUID.randomUUID()
+    new HandleIdentifier(publicId, secretId)
+  }
+
+  def apply(tHandleId: THandleIdentifier): HandleIdentifier = {
+    val pbb = ByteBuffer.wrap(tHandleId.getGuid)
+    val publicId = new UUID(pbb.getLong, pbb.getLong)
+
+    val sbb = ByteBuffer.wrap(tHandleId.getSecret)
+    val secretId = new UUID(sbb.getLong, sbb.getLong)
+
+    new HandleIdentifier(publicId, secretId)
+  }
+}
