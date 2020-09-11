31cbee72ef7264ae85c4bd1a5d65244c4f3f6113
Kent Yao <yaooqinn@hotmail.com>
2020-09-11 19:28:38 +0800

    add some test for execute statement operation

--- externals/kyuubi-spark-sql-engine/src/test/scala/org/apache/kyuubi/engine/spark/operation/SparkOperationSuite.scala
+++ externals/kyuubi-spark-sql-engine/src/test/scala/org/apache/kyuubi/engine/spark/operation/SparkOperationSuite.scala
@@ -17,13 +17,15 @@
 
 package org.apache.kyuubi.engine.spark.operation
 
+import java.sql.{Date, SQLException, Timestamp}
+
 import org.apache.hive.service.cli.HiveSQLException
 import org.apache.hive.service.rpc.thrift.TOpenSessionReq
 import org.apache.spark.sql.catalyst.analysis.FunctionRegistry
 import org.apache.spark.sql.catalyst.catalog.CatalogTableType
 import org.apache.spark.sql.types._
-
 import org.apache.kyuubi.Utils
+
 import org.apache.kyuubi.engine.spark.WithSparkSQLEngine
 import org.apache.kyuubi.operation.meta.ResultSetSchemaConstant._
 
@@ -394,8 +396,6 @@ class SparkOperationSuite extends WithSparkSQLEngine {
     }
   }
 
-
-
   test("get columns operation should handle interval column properly") {
     val viewName = "view_interval"
     val ddl = s"CREATE GLOBAL TEMP VIEW $viewName as select interval 1 day as i"
@@ -465,6 +465,236 @@ class SparkOperationSuite extends WithSparkSQLEngine {
           assert(resultSet.getString(SPECIFIC_NAME) === exprInfo.getClassName)
         }
       }
+    }
+  }
+
+  test("execute statement -  select null") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT NULL AS col")
+      assert(resultSet.next())
+      assert(resultSet.getString("col") === null)
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.NULL)
+      assert(metaData.getPrecision(1) === 0)
+      assert(metaData.getScale(1) === 0)
+    }
+  }
+
+  test("execute statement -  select boolean") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT false AS col")
+      assert(resultSet.next())
+      assert(!resultSet.getBoolean("col"))
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.BOOLEAN)
+      assert(metaData.getPrecision(1) === 1)
+      assert(metaData.getScale(1) === 0)
+    }
+  }
+
+  test("execute statement -  select tinyint") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT 1Y AS col")
+      assert(resultSet.next())
+      assert(resultSet.getByte("col") === 1.toByte)
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.TINYINT)
+      assert(metaData.getPrecision(1) === 3)
+      assert(metaData.getScale(1) === 0)
+    }
+  }
+
+  test("execute statement -  select smallint") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT 1S AS col")
+      assert(resultSet.next())
+      assert(resultSet.getShort("col") === 1.toShort)
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.SMALLINT)
+      assert(metaData.getPrecision(1) === 5)
+      assert(metaData.getScale(1) === 0)
+    }
+  }
+
+  test("execute statement -  select int") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT 4 AS col")
+      assert(resultSet.next())
+      assert(resultSet.getInt("col") === 4)
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.INTEGER)
+      assert(metaData.getPrecision(1) === 10)
+      assert(metaData.getScale(1) === 0)
+    }
+  }
+
+  test("execute statement -  select long") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT 4L AS col")
+      assert(resultSet.next())
+      assert(resultSet.getLong("col") === 4L)
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.BIGINT)
+      assert(metaData.getPrecision(1) === 19)
+      assert(metaData.getScale(1) === 0)
+    }
+  }
+
+  test("execute statement -  select float") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT cast(1.2 as float) AS col")
+      assert(resultSet.next())
+      assert(resultSet.getFloat("col") === 1.2f)
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.FLOAT)
+      assert(metaData.getPrecision(1) === 7)
+      assert(metaData.getScale(1) === 7)
+    }
+  }
+
+  test("execute statement -  select double") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT 4.2D AS col")
+      assert(resultSet.next())
+      assert(resultSet.getDouble("col") === 4.2d)
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.DOUBLE)
+      assert(metaData.getPrecision(1) === 15)
+      assert(metaData.getScale(1) === 15)
+    }
+  }
+
+  test("execute statement -  select decimal") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT 1.2BD as col1, 1.23BD AS col2")
+      assert(resultSet.next())
+      assert(resultSet.getBigDecimal("col1") === Decimal(1.2).toJavaBigDecimal)
+      assert(resultSet.getBigDecimal("col2") === Decimal(1.23).toJavaBigDecimal)
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.DECIMAL)
+      assert(metaData.getColumnType(2) === java.sql.Types.DECIMAL)
+      assert(metaData.getPrecision(1) == 2)
+      assert(metaData.getPrecision(2) == 3)
+      assert(metaData.getScale(1) == 1)
+      assert(metaData.getScale(2) == 2)
+    }
+  }
+
+  test("execute statement -  select string") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT 'kentyao' AS col")
+      assert(resultSet.next())
+      assert(resultSet.getString("col") === "kentyao")
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.VARCHAR)
+      assert(metaData.getPrecision(1) === Int.MaxValue)
+      assert(metaData.getScale(1) === 0)
+    }
+  }
+
+  test("execute statement -  select binary") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT cast('kyuubi' as binary) AS col")
+      assert(resultSet.next())
+      assert(resultSet.getObject("col") === "kyuubi".getBytes)
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.BINARY)
+      assert(metaData.getPrecision(1) === Int.MaxValue)
+      assert(metaData.getScale(1) === 0)
+    }
+  }
+
+  test("execute statement -  select date") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT DATE '2018-11-17' AS col")
+      assert(resultSet.next())
+      assert(resultSet.getDate("col") === Date.valueOf("2018-11-17"))
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.DATE)
+      assert(metaData.getPrecision(1) === 10)
+      assert(metaData.getScale(1) === 0)
+    }
+  }
+
+  test("execute statement -  select timestamp") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT TIMESTAMP '2018-11-17 13:33:33' AS col")
+      assert(resultSet.next())
+      assert(resultSet.getTimestamp("col") === Timestamp.valueOf("2018-11-17 13:33:33"))
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.TIMESTAMP)
+      assert(metaData.getPrecision(1) === 29)
+      assert(metaData.getScale(1) === 9)
+    }
+  }
+
+  test("execute statement -  select interval") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT interval '1' day AS col")
+      assert(resultSet.next())
+      assert(resultSet.getString("col") === "1 days")
+      assert(resultSet.getMetaData.getColumnType(1) === java.sql.Types.VARCHAR)
+      val metaData = resultSet.getMetaData
+      assert(metaData.getPrecision(1) === Int.MaxValue)
+      assert(metaData.getScale(1) === 0)
+    }
+  }
+
+  test("execute statement -  select array") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT array() AS col1, array(1) AS col2")
+      assert(resultSet.next())
+      assert(resultSet.getObject("col1") === "[]")
+      assert(resultSet.getObject("col2") === "[1]")
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.ARRAY)
+      assert(metaData.getPrecision(1) === Int.MaxValue)
+      assert(metaData.getPrecision(2) == Int.MaxValue)
+      assert(metaData.getScale(1) == 0)
+      assert(metaData.getScale(2) == 0)
+    }
+  }
+
+  test("execute statement - select map") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery("SELECT map() AS col1, map(1, 2, 3, 4) AS col2")
+      assert(resultSet.next())
+      assert(resultSet.getObject("col1") === "{}")
+      assert(resultSet.getObject("col2") === "{1:2,3:4}")
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.JAVA_OBJECT)
+      assert(metaData.getPrecision(1) === Int.MaxValue)
+      assert(metaData.getPrecision(2) == Int.MaxValue)
+      assert(metaData.getScale(1) == 0)
+      assert(metaData.getScale(2) == 0)
+    }
+  }
+
+  test("execute statement - select struct") {
+    withJdbcStatement() { statement =>
+      val resultSet = statement.executeQuery(
+        "SELECT struct('1', '2') AS col1, named_struct('a', 2, 'b', 4) AS col2")
+      assert(resultSet.next())
+      assert(resultSet.getObject("col1") === "{\"col1\":\"1\",\"col2\":\"2\"}")
+      assert(resultSet.getObject("col2") === "{\"a\":2,\"b\":4}")
+      val metaData = resultSet.getMetaData
+      assert(metaData.getColumnType(1) === java.sql.Types.STRUCT)
+      assert(metaData.getPrecision(1) === Int.MaxValue)
+      assert(metaData.getPrecision(2) == Int.MaxValue)
+      assert(metaData.getScale(1) == 0)
+      assert(metaData.getScale(2) == 0)
+    }
+  }
+
+  test("execute statement - analysis exception") {
+    val sql = "select date_sub(date'2011-11-11', '1.2')"
+
+    withJdbcStatement() { statement =>
+      val e = intercept[SQLException] {
+        statement.executeQuery(sql)
+      }
+      assert(e.getMessage
+        .contains("The second argument of 'date_sub' function needs to be an integer."))
     }
   }
 
