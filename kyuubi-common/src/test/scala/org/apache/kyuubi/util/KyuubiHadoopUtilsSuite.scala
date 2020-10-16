f6dd53a1d11e8fdd5f6c543317048865d516a540
Kent Yao <yaooqinn@hotmail.com>
2020-10-17 00:05:51 +0800

    KyuubiHadoopUtilsSuite

--- kyuubi-common/src/test/scala/org/apache/kyuubi/util/KyuubiHadoopUtilsSuite.scala
+++ kyuubi-common/src/test/scala/org/apache/kyuubi/util/KyuubiHadoopUtilsSuite.scala
@@ -17,17 +17,22 @@
 
 package org.apache.kyuubi.util
 
-import org.apache.hadoop.conf.Configuration
-
+import org.apache.kyuubi.KyuubiFunSuite
 import org.apache.kyuubi.config.KyuubiConf
 
-object KyuubiHadoopUtils {
+class KyuubiHadoopUtilsSuite extends KyuubiFunSuite {
 
-  def newHadoopConf(conf: KyuubiConf): Configuration = {
-    val hadoopConf = new Configuration()
-    conf.getAllWithPrefix("kyuubi", "hadoop").foreach {
-      case (k, v) => hadoopConf.set(k, v)
-    }
-    hadoopConf
+  test("new hadoop conf with kyuubi conf") {
+    val abc = "kyuubi.hadoop.abc"
+    val xyz = "kyuubi.hadoop.xyz"
+    val test = "hadoop.test"
+    val kyuubiConf = new KyuubiConf()
+      .set(abc, "xyz")
+      .set(xyz, "abc")
+      .set(test, "t")
+    val hadoopConf = KyuubiHadoopUtils.newHadoopConf(kyuubiConf)
+    assert(hadoopConf.get(abc.stripPrefix("kyuubi.")) === "xyz")
+    assert(hadoopConf.get(xyz.stripPrefix("kyuubi.")) === "abc")
+    assert(hadoopConf.get("hadoop.test") === null)
   }
 }
