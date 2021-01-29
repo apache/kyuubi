c9724790316ce6ea729b51175871d9af2c754afb
cz1019sniper <tianlove1019@163.com>
2021-01-29 22:57:05 +0800

    Update Kyuubi Doc Version

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ limitations under the License.
 author = 'Kent Yao'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
