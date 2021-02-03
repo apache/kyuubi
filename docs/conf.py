024cdf44142b53d571c85c3f19f2604129c6af30
Kent Yao <yao@apache.org>
2021-02-03 19:47:58 +0800

    Prepare Kyuubi v1.0.4

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ limitations under the License.
 author = 'Kent Yao'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.3'
+release = '1.0.4'
 
 
 # -- General configuration ---------------------------------------------------
