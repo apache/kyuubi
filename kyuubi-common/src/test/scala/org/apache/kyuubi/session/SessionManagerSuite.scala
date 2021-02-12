d0fa46033f5e931088a469673d2a5d6f3cd95a09
Honglun <283471011@qq.com>
2021-02-12 16:46:24 +0800

    Simplify common code for test cases.

--- kyuubi-common/src/test/scala/org/apache/kyuubi/session/SessionManagerSuite.scala
+++ kyuubi-common/src/test/scala/org/apache/kyuubi/session/SessionManagerSuite.scala
@@ -18,77 +18,19 @@
 package org.apache.kyuubi.session
 
 import java.time.Duration
-import java.util
 
 import org.apache.hive.service.rpc.thrift._
-import org.apache.thrift.protocol.TBinaryProtocol
-import org.apache.thrift.transport.TSocket
 
-import org.apache.kyuubi.{KyuubiFunSuite, Utils}
 import org.apache.kyuubi.config.KyuubiConf
-import org.apache.kyuubi.service.NoopServer
-import org.apache.kyuubi.service.authentication.PlainSASLHelper
+import org.apache.kyuubi.service.FrontendServiceSuite
 
-class SessionManagerSuite extends KyuubiFunSuite {
+class SessionManagerSuite extends FrontendServiceSuite {
 
-  private val server = new NoopServer()
-  private val conf = KyuubiConf()
+  override val conf = KyuubiConf()
     .set(KyuubiConf.FRONTEND_BIND_PORT, 0)
     .set("kyuubi.test.server.should.fail", "false")
-
-  val user: String = System.getProperty("user.name")
-
-  def startServer(): Unit = {
-    server.initialize(conf)
-    server.start()
-  }
-
-  override def afterAll(): Unit = {
-    server.getServices.foreach(_.stop())
-    super.afterAll()
-  }
-
-  protected def withThriftClient(f: TCLIService.Iface => Unit): Unit = {
-    val hostAndPort = server.connectionUrl.split(":")
-    val host = hostAndPort.head
-    val port = hostAndPort(1).toInt
-    val socket = new TSocket(host, port)
-    val transport =
-      PlainSASLHelper.getPlainTransport(Utils.currentUser, "anonymous", socket)
-
-    val protocol = new TBinaryProtocol(transport)
-    val client = new TCLIService.Client(protocol)
-    transport.open()
-    try {
-      f(client)
-    } finally {
-      socket.close()
-    }
-  }
-
-  protected def withSessionHandle(
-    conf: java.util.Map[String, String]
-  )(f: (TCLIService.Iface, TSessionHandle) => Unit): Unit = {
-    withThriftClient { client =>
-      val req = new TOpenSessionReq()
-      req.setUsername(user)
-      req.setPassword("anonymous")
-      req.setConfiguration(conf)
-      val resp = client.OpenSession(req)
-      val handle = resp.getSessionHandle
-
-      try {
-        f(client, handle)
-      } finally {
-        val tCloseSessionReq = new TCloseSessionReq(handle)
-        try {
-           client.CloseSession(tCloseSessionReq)
-        } catch {
-          case e: Exception => error(s"Failed to close $handle", e)
-        }
-      }
-    }
-  }
+    .set(KyuubiConf.SESSION_CHECK_INTERVAL, Duration.ofSeconds(5).toMillis)
+    .set(KyuubiConf.SESSION_TIMEOUT, Duration.ofSeconds(5).toMillis)
 
   def waitFor(waitForTime: Long)(f: () => Boolean): Unit = {
     val startTime = System.currentTimeMillis()
@@ -103,17 +45,12 @@ class SessionManagerSuite extends KyuubiFunSuite {
   }
 
   test("close expired operations") {
-    conf.set(KyuubiConf.SESSION_CHECK_INTERVAL, Duration.ofSeconds(5).toMillis)
-    conf.set(KyuubiConf.SESSION_TIMEOUT, Duration.ofSeconds(5).toMillis)
-
-    val reqConf = new util.HashMap[String, String]()
-    reqConf.put(
+    sessionConf.put(
       KyuubiConf.OPERATION_IDLE_TIMEOUT.key,
       String.valueOf(Duration.ofSeconds(20).toMillis)
     )
 
-    startServer()
-    withSessionHandle(reqConf) { (client, handle) =>
+    withSessionHandle{ (client, handle) =>
       val req = new TCancelOperationReq()
       val req1 = new TGetSchemasReq(handle)
       val resp1 = client.GetSchemas(req1)
