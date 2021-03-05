bbf458bc17bfaa6879514659fd45e11cf867667e
Kent Yao <yao@apache.org>
2021-03-05 14:31:16 +0800

    time stamp formatter

--- externals/kyuubi-spark-sql-engine/src/main/scala/org/apache/kyuubi/schema/RowSet.scala
+++ externals/kyuubi-spark-sql-engine/src/main/scala/org/apache/kyuubi/schema/RowSet.scala
@@ -214,18 +214,12 @@ object RowSet {
     createBuilder().appendPattern("yyyy-MM-dd")
       .toFormatter(Locale.US)
       .withChronology(IsoChronology.INSTANCE)
-      .withResolverStyle(ResolverStyle.STRICT)
   }
 
   private lazy val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.US)
 
   private lazy val timestampFormatter: DateTimeFormatter = {
-    createBuilder()
-      .append(DateTimeFormatter.ISO_LOCAL_DATE)
-      .appendLiteral(' ')
-      .appendValue(ChronoField.HOUR_OF_DAY, 2).appendLiteral(':')
-      .appendValue(ChronoField.MINUTE_OF_HOUR, 2).appendLiteral(':')
-      .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+    createBuilder().appendPattern("yyyy-MM-dd HH:mm:ss")
       .appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, true)
       .toFormatter(Locale.US)
       .withChronology(IsoChronology.INSTANCE)
