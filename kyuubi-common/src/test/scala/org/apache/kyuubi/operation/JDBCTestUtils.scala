4a433dd31bbe2e54f7952519a55f721d7dd476ce
Min Zhao <zhaomin1423@163.com>
2021-07-04 17:47:52 +0800

    move log 'Closing ...' to correct line

--- kyuubi-common/src/test/scala/org/apache/kyuubi/operation/JDBCTestUtils.scala
+++ kyuubi-common/src/test/scala/org/apache/kyuubi/operation/JDBCTestUtils.scala
@@ -98,8 +98,9 @@ trait JDBCTestUtils extends KyuubiFunSuite {
       info("Closing statements")
       statements.foreach(_.close())
       info("Closed statements")
-      connections.foreach(_.close())
       info("Closing connections")
+      connections.foreach(_.close())
+      info("Closed connections")
     }
   }
 
