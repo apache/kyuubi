5ce5058c8175522d3cd8ce910e80668b50f6de61
张宇翔 <zhang1002@126.com>
2021-07-14 13:17:00 +0800

    1. Add unit test 2. Add some comment 3. Code optimization 4. Use queue to store kyuubiStatementInfo

--- externals/kyuubi-spark-monitor/src/main/scala/org/apache/kyuubi/engine/spark/monitor/KyuubiStatementMonitor.scala
+++ externals/kyuubi-spark-monitor/src/main/scala/org/apache/kyuubi/engine/spark/monitor/KyuubiStatementMonitor.scala
@@ -17,16 +17,65 @@
 
 package org.apache.kyuubi.engine.spark.monitor
 
-import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ArrayBlockingQueue
 
-import org.apache.kyuubi.engine.spark.monitor.entity.KyuubiStatementInfo
+import org.apache.kyuubi.engine.spark.monitor.entity.{KyuubiInfo, KyuubiStatementInfo}
 
 object KyuubiStatementMonitor {
 
-  private val operationStatementMap = new ConcurrentHashMap[String, KyuubiStatementInfo]()
+  /**
+   * This blockingQueue store kyuubiStatementInfo.
+   *
+   * Notice:
+   *    1. When we remove items from this queue, we should ensure those statements have finished.
+   *       If not, we should put them into this queue again.
+   *    2. There have two kinds of threshold to trigger when to remove items from this queue:
+   *      a. time
+   *      b. this queue's current size
+   */
+  // TODO: Capacity should make configurable
+  private val kyuubiStatementQueue = new ArrayBlockingQueue[KyuubiStatementInfo](10)
 
-  def addStatementDetailForOperationId(
-      operationId: String, kyuubiStatementInfo: KyuubiStatementInfo): Unit = {
-    operationStatementMap.putIfAbsent(operationId, kyuubiStatementInfo)
+  /**
+   * This function is used for putting kyuubiStatementInfo into blockingQueue(statementQueue).
+   * Every time we put an item into this queue, we should judge this queue's current size at first.
+   * If the size is less than threshold, we need to remove items from this queue.
+   * @param kyuubiStatementInfo
+   */
+  // TODO: Lack size type threshold and time type threshold
+  def putStatementInfoIntoQueue(kyuubiStatementInfo: KyuubiStatementInfo): Unit = {
+    if (kyuubiStatementQueue.size() >= 7) {
+      removeAndDumpStatementInfoFromQueue()
+    }
+    kyuubiStatementQueue.put(kyuubiStatementInfo)
+  }
+
+  /**
+   * This function is used for removing kyuubiStatementInfo from blockingQueue(statementQueue)
+   * and dumpping them to a file by threshold.
+   */
+  // TODO: Need ensure those items have finished. If not, we should put them into this queue again.
+  def removeAndDumpStatementInfoFromQueue(): Unit = {
+    // TODO: Just for test
+    kyuubiStatementQueue.clear()
+  }
+
+  // TODO: For test
+  def getQueueSize[T](c: Class[T]): Int = {
+    if (c.equals(KyuubiStatementInfo.getClass)) {
+      return kyuubiStatementQueue.size()
+    } else {
+      return -1
+    }
+  }
+
+  // TODO: For test
+  def copyQueue[T](c: Class[T]): ArrayBlockingQueue[KyuubiInfo] = {
+    if (c.equals(KyuubiStatementInfo.getClass)) {
+      val kyuubistatementQueueCopy = new ArrayBlockingQueue[KyuubiInfo](kyuubiStatementQueue.size())
+      kyuubistatementQueueCopy.addAll(kyuubiStatementQueue)
+      return kyuubistatementQueueCopy
+    }
+    return null
   }
 }
---
+++ externals/kyuubi-spark-sql-engine/src/test/scala/org/apache/kyuubi/engine/spark/KyuubiStatementMonitorSuite.scala
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.engine.spark
+
+import org.apache.hive.service.rpc.thrift.{TExecuteStatementReq, TGetOperationStatusReq, TOperationHandle}
+import org.apache.hive.service.rpc.thrift.TCLIService.Iface
+import org.apache.hive.service.rpc.thrift.TOperationState._
+
+import org.apache.kyuubi.engine.spark.monitor.KyuubiStatementMonitor
+import org.apache.kyuubi.engine.spark.monitor.entity.KyuubiStatementInfo
+import org.apache.kyuubi.operation.HiveJDBCTests
+
+class KyuubiStatementMonitorSuite extends WithSparkSQLEngine with HiveJDBCTests {
+
+  override protected def jdbcUrl: String = getJdbcUrl
+  override def withKyuubiConf: Map[String, String] = Map.empty
+
+  test("test event tracking: statement") {
+    val sql = "select timestamp'2021-06-01'"
+    val total: Int = 7
+    withSessionHandle { (client, handle) =>
+      for ( a <- 1 to total ) {
+        val req = new TExecuteStatementReq()
+        req.setSessionHandle(handle)
+        req.setStatement(sql)
+        val tExecuteStatementResp = client.ExecuteStatement(req)
+        val operationHandle = tExecuteStatementResp.getOperationHandle
+        waitForOperationToComplete(client, operationHandle)
+      }
+      val size = KyuubiStatementMonitor.getQueueSize(KyuubiStatementInfo.getClass)
+
+      assert(size === total)
+
+      var kyuubiStatementQueue = KyuubiStatementMonitor.copyQueue(KyuubiStatementInfo.getClass)
+      var iterator = kyuubiStatementQueue.iterator()
+      while (iterator.hasNext) {
+        val kyuubiStatementInfo = iterator.next().asInstanceOf[KyuubiStatementInfo]
+        assert(kyuubiStatementInfo.statement !== null)
+        assert(kyuubiStatementInfo.statementId !== null)
+        assert(kyuubiStatementInfo.sessionId !== null)
+        assert(kyuubiStatementInfo.queryExecution !== null)
+        assert(kyuubiStatementInfo.stateToTime.size === 4)
+      }
+      kyuubiStatementQueue = null
+      iterator = null
+
+      // Test for clear kyuubiStatementQueue
+      // This function is used for avoiding mem leak
+      val req = new TExecuteStatementReq()
+      req.setSessionHandle(handle)
+      req.setStatement(sql)
+      val tExecuteStatementResp = client.ExecuteStatement(req)
+      val operationHandle = tExecuteStatementResp.getOperationHandle
+      waitForOperationToComplete(client, operationHandle)
+
+      assert(KyuubiStatementMonitor.getQueueSize(KyuubiStatementInfo.getClass) === 1)
+    }
+  }
+
+  private def waitForOperationToComplete(client: Iface, op: TOperationHandle): Unit = {
+    val req = new TGetOperationStatusReq(op)
+    var state = client.GetOperationStatus(req).getOperationState
+    while (state == INITIALIZED_STATE || state == PENDING_STATE || state == RUNNING_STATE) {
+      state = client.GetOperationStatus(req).getOperationState
+    }
+  }
+}
