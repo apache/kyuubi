07f82667063ced7c5a159b1cee1921a4bf308a9b
hongdongdong <hongdongdong@cmss.chinamobile.com>
2021-07-20 17:00:26 +0800

    fix test

--- kyuubi-common/src/test/scala/org/apache/kyuubi/config/KyuubiConfSuite.scala
+++ kyuubi-common/src/test/scala/org/apache/kyuubi/config/KyuubiConfSuite.scala
@@ -102,10 +102,10 @@ class KyuubiConfSuite extends KyuubiFunSuite {
     sys.props.put("___userc___.spark.user.test", "c")
     val conf = KyuubiConf()
 
-    val all1 = conf.getUserDefaults("kyuubi").getAll.filter(_._1 != "kyuubi.testing")
+    val all1 = conf.getUserDefaults("kyuubi").getAll.filter(_._1.startsWith("spark."))
     assert(all1.size === 1)
     assert(all1("spark.user.test") === "a")
-    val all2 = conf.getUserDefaults("userb").getAll.filter(_._1 != "kyuubi.testing")
+    val all2 = conf.getUserDefaults("userb").getAll.filter(_._1.startsWith("spark."))
     assert(all2.size === 1)
     assert(all2("spark.user.test") === "b")
     assert(conf.getUserDefaults("userc").getOption("spark.user.test").get === "c")
