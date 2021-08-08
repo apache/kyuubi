2a4317a5a9d3d6e5ea09fcaf3f5151b591a6064d
张宇翔 <zhang1002@126.com>
2021-08-09 01:17:02 +0800

    remove some ut

--- externals/kyuubi-spark-sql-engine/src/test/scala/org/apache/kyuubi/engine/spark/KyuubiStatementMonitorSuite.scala
+++ externals/kyuubi-spark-sql-engine/src/test/scala/org/apache/kyuubi/engine/spark/KyuubiStatementMonitorSuite.scala
@@ -17,7 +17,7 @@
 
 package org.apache.kyuubi.engine.spark
 
-import java.util.concurrent.{ArrayBlockingQueue, ConcurrentHashMap}
+import java.util.concurrent.ConcurrentHashMap
 
 import org.apache.hive.service.rpc.thrift._
 import org.apache.hive.service.rpc.thrift.TCLIService.Iface
@@ -27,7 +27,7 @@ import org.scalatest.PrivateMethodTester
 import org.scalatest.time.SpanSugar._
 
 import org.apache.kyuubi.engine.spark.monitor.KyuubiStatementMonitor
-import org.apache.kyuubi.engine.spark.monitor.entity.{KyuubiJobInfo, KyuubiStatementInfo}
+import org.apache.kyuubi.engine.spark.monitor.entity.KyuubiJobInfo
 import org.apache.kyuubi.operation.{HiveJDBCTests, OperationHandle}
 
 class KyuubiStatementMonitorSuite extends WithSparkSQLEngine with HiveJDBCTests
@@ -35,32 +35,6 @@ class KyuubiStatementMonitorSuite extends WithSparkSQLEngine with HiveJDBCTests
 
   override protected def jdbcUrl: String = getJdbcUrl
   override def withKyuubiConf: Map[String, String] = Map.empty
-
-  test("add kyuubiStatementInfo into queue") {
-    var baseSql = "select timestamp'2021-06-0"
-    val total: Int = 7
-    // Clear kyuubiStatementQueue first
-    val getQueue = PrivateMethod[
-      ArrayBlockingQueue[KyuubiStatementInfo]](Symbol("kyuubiStatementQueue"))()
-    val kyuubiStatementQueue = KyuubiStatementMonitor.invokePrivate(getQueue)
-    kyuubiStatementQueue.clear()
-    withSessionHandle { (client, handle) =>
-
-      for ( a <- 1 to total ) {
-        val sql = baseSql + a + "'"
-        val req = new TExecuteStatementReq()
-        req.setSessionHandle(handle)
-        req.setStatement(sql)
-        val tExecuteStatementResp = client.ExecuteStatement(req)
-        val operationHandle = tExecuteStatementResp.getOperationHandle
-
-        val kyuubiStatementInfo = kyuubiStatementQueue.poll()
-//        assert(
-//          kyuubiStatementInfo.statementId === OperationHandle(operationHandle).identifier.toString)
-//        assert(sql === kyuubiStatementInfo.statement)
-      }
-    }
-  }
 
   test("add kyuubiJobInfo into queue and remove them when threshold reached") {
     val sql = "select timestamp'2021-06-01'"
