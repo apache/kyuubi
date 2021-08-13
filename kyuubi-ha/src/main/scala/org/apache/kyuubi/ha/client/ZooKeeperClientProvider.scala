29d489214e3dff06b3aea42463aa6d7923ff6d26
timothy65535 <timothy65535@163.com>
2021-08-13 18:09:14 +0800

    Add ZooKeeperClientProvider

---
+++ kyuubi-ha/src/main/scala/org/apache/kyuubi/ha/client/ZooKeeperClientProvider.scala
@@ -0,0 +1,108 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.ha.client
+
+import java.io.{File, IOException}
+import javax.security.auth.login.Configuration
+
+import org.apache.curator.framework.{CuratorFramework, CuratorFrameworkFactory}
+import org.apache.curator.retry._
+import org.apache.hadoop.security.UserGroupInformation
+import org.apache.hadoop.security.token.delegation.ZKDelegationTokenSecretManager.JaasConfiguration
+
+import org.apache.kyuubi.Logging
+import org.apache.kyuubi.config.KyuubiConf
+import org.apache.kyuubi.ha.HighAvailabilityConf._
+import org.apache.kyuubi.util.KyuubiHadoopUtils
+
+object ZooKeeperClientProvider extends Logging {
+
+  import RetryPolicies._
+
+  /**
+   * Create a [[CuratorFramework]] instance to be used as the ZooKeeper client
+   * Use the [[ZooKeeperACLProvider]] to create appropriate ACLs
+   */
+  def buildZookeeperClient(conf: KyuubiConf): CuratorFramework = {
+    setUpZooKeeperAuth(conf)
+    val connectionStr = conf.get(HA_ZK_QUORUM)
+    val sessionTimeout = conf.get(HA_ZK_SESSION_TIMEOUT)
+    val connectionTimeout = conf.get(HA_ZK_CONN_TIMEOUT)
+    val baseSleepTime = conf.get(HA_ZK_CONN_BASE_RETRY_WAIT)
+    val maxSleepTime = conf.get(HA_ZK_CONN_MAX_RETRY_WAIT)
+    val maxRetries = conf.get(HA_ZK_CONN_MAX_RETRIES)
+    val retryPolicyName = conf.get(HA_ZK_CONN_RETRY_POLICY)
+    val retryPolicy = RetryPolicies.withName(retryPolicyName) match {
+      case ONE_TIME => new RetryOneTime(baseSleepTime)
+      case N_TIME => new RetryNTimes(maxRetries, baseSleepTime)
+      case BOUNDED_EXPONENTIAL_BACKOFF =>
+        new BoundedExponentialBackoffRetry(baseSleepTime, maxSleepTime, maxRetries)
+      case UNTIL_ELAPSED => new RetryUntilElapsed(maxSleepTime, baseSleepTime)
+      case _ => new ExponentialBackoffRetry(baseSleepTime, maxRetries)
+    }
+    CuratorFrameworkFactory.builder()
+      .connectString(connectionStr)
+      .sessionTimeoutMs(sessionTimeout)
+      .connectionTimeoutMs(connectionTimeout)
+      .aclProvider(new ZooKeeperACLProvider(conf))
+      .retryPolicy(retryPolicy)
+      .build()
+  }
+
+  /**
+   * Creates a zookeeper client before calling `f` and close it after calling `f`.
+   */
+  def withZkClient(conf: KyuubiConf)(f: CuratorFramework => Unit): Unit = {
+    val zkClient = buildZookeeperClient(conf)
+    try {
+      zkClient.start()
+      f(zkClient)
+    } finally {
+      try {
+        zkClient.close()
+      } catch {
+        case e: IOException => error("Failed to release the zkClient", e)
+      }
+    }
+  }
+
+  /**
+   * For a kerberized cluster, we dynamically set up the client's JAAS conf.
+   *
+   * @param conf SparkConf
+   * @return
+   */
+  @throws[Exception]
+  def setUpZooKeeperAuth(conf: KyuubiConf): Unit = {
+    if (conf.get(HA_ZK_ACL_ENABLED)) {
+      val keyTabFile = conf.get(KyuubiConf.SERVER_KEYTAB)
+      val maybePrincipal = conf.get(KyuubiConf.SERVER_PRINCIPAL)
+      val kerberized = maybePrincipal.isDefined && keyTabFile.isDefined
+      if (UserGroupInformation.isSecurityEnabled && kerberized) {
+        if (!new File(keyTabFile.get).exists()) {
+          throw new IOException(s"${KyuubiConf.SERVER_KEYTAB.key} does not exists")
+        }
+        System.setProperty("zookeeper.sasl.clientconfig", "KyuubiZooKeeperClient")
+        var principal = maybePrincipal.get
+        principal = KyuubiHadoopUtils.getServerPrincipal(principal)
+        val jaasConf = new JaasConfiguration("KyuubiZooKeeperClient", principal, keyTabFile.get)
+        Configuration.setConfiguration(jaasConf)
+      }
+    }
+  }
+}
