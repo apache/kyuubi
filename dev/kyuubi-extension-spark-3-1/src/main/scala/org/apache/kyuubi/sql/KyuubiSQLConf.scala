55ef6af7ac4800ded8280b98e1990a8611b240ec
张宇翔 <zhang1002@126.com>
2021-09-02 14:29:43 +0800

    some modification

--- dev/kyuubi-extension-spark-3-1/src/main/scala/org/apache/kyuubi/sql/KyuubiSQLConf.scala
+++ dev/kyuubi-extension-spark-3-1/src/main/scala/org/apache/kyuubi/sql/KyuubiSQLConf.scala
@@ -71,9 +71,9 @@ object KyuubiSQLConf {
   val SQL_CLASSIFICATION_ENABLED =
     buildConf("kyuubi.spark.sql.classification.enabled")
       .doc("When true, allows Kyuubi engine to judge this SQL's classification " +
-        "and set it into sessionConf." +
+        "and set it into sessionConf. " +
         "Through this configuration item, Spark can optimizing configuration dynamic")
-      .version("1.3.0")
+      .version("1.4.0")
       .booleanConf
       .createWithDefault(true)
 }
