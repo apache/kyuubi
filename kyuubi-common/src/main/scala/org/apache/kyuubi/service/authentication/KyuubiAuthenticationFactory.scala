98f936405cf27f0c0ebda059ab70268ab57ee057
fwang12 <fwang12@ebay.com>
2021-10-21 18:19:23 +0800

    revert sth

--- kyuubi-common/src/main/scala/org/apache/kyuubi/service/authentication/KyuubiAuthenticationFactory.scala
+++ kyuubi-common/src/main/scala/org/apache/kyuubi/service/authentication/KyuubiAuthenticationFactory.scala
@@ -73,18 +73,16 @@ class KyuubiAuthenticationFactory(conf: KyuubiConf) {
           } catch {
             case e: TTransportException => throw new LoginException(e.getMessage)
           }
-          server.wrapTransportFactory(transportFactory)
           Some(transportFactory)
 
         case _ => None
       }
 
       val transportFactory = plainAuthType.map { authType =>
-        PlainSASLHelper.getTransportFactory(authType.toString, conf)
+        PlainSASLHelper.getTransportFactory(authType.toString, conf, kerberosTransportFactory)
       }.orElse(kerberosTransportFactory).orNull
 
-      // hadoopAuthServer.map(_.wrapTransportFactory(transportFactory)).getOrElse(transportFactory)
-      transportFactory
+      hadoopAuthServer.map(_.wrapTransportFactory(transportFactory)).getOrElse(transportFactory)
     }
   }
 
