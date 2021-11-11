b773fd9df8478ab71525069818c0f72015bf1474
fwang12 <fwang12@ebay.com>
2021-11-11 21:41:28 +0800

    refactor

--- kyuubi-common/src/main/scala/org/apache/kyuubi/config/KyuubiConf.scala
+++ kyuubi-common/src/main/scala/org/apache/kyuubi/config/KyuubiConf.scala
@@ -1011,7 +1011,7 @@ object KyuubiConf {
 
   val ENGINE_SYNC_INIT: ConfigEntry[Boolean] =
     buildConf("engine.sync.init")
-      .doc("when opening kyuubi session, whether to init backend engine with sync")
+      .doc("when opening kyuubi session, whether to init backend engine synchronously")
       .version("1.4.0")
       .booleanConf.
       createWithDefault(true)
