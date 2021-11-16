ef12757b4c077f7790bc0081b4e1801f01dc0680
fwang12 <fwang12@ebay.com>
2021-11-16 11:18:53 +0800

    rewrite opensession in KyuubiThriftBinaryFrontendService

--- kyuubi-server/src/main/scala/org/apache/kyuubi/server/KyuubiThriftBinaryFrontendService.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/server/KyuubiThriftBinaryFrontendService.scala
@@ -20,13 +20,15 @@ package org.apache.kyuubi.server
 import org.apache.commons.codec.binary.Base64
 import org.apache.hive.service.rpc.thrift.{TOpenSessionReq, TOpenSessionResp}
 
+import org.apache.kyuubi.KyuubiSQLException
 import org.apache.kyuubi.ha.client.{KyuubiServiceDiscovery, ServiceDiscovery}
 import org.apache.kyuubi.service.{Serverable, Service, ThriftBinaryFrontendService}
-import org.apache.kyuubi.session.{KyuubiSessionImpl, SessionHandle}
+import org.apache.kyuubi.session.KyuubiSessionImpl
 
 class KyuubiThriftBinaryFrontendService(
     override val serverable: Serverable)
   extends ThriftBinaryFrontendService("KyuubiThriftBinaryFrontendService") {
+  import ThriftBinaryFrontendService._
 
   override lazy val discoveryService: Option[Service] = {
     if (ServiceDiscovery.supportServiceDiscovery(conf)) {
@@ -37,18 +39,31 @@ class KyuubiThriftBinaryFrontendService(
   }
 
   override def OpenSession(req: TOpenSessionReq): TOpenSessionResp = {
-    val resp = super.OpenSession(req)
-    if (resp.getSessionHandle != null) {
-      val sessionHandle = SessionHandle(resp.getSessionHandle)
+    debug(req.toString)
+    info("Client protocol version: " + req.getClient_protocol)
+    val resp = new TOpenSessionResp
+    try {
+      val sessionHandle = getSessionHandle(req, resp)
+
+      val respConfiguration = new java.util.HashMap[String, String]()
       val launchEngineOp = be.sessionManager.getSession(sessionHandle)
         .asInstanceOf[KyuubiSessionImpl].launchEngineOp
       if (launchEngineOp.shouldRunAsync) {
         val opHandleIdentifier = launchEngineOp.getHandle.identifier.toTHandleIdentifier
-        resp.getConfiguration.put("kyuubi.session.launch.engine.handle.guid",
+        respConfiguration.put("kyuubi.session.launch.engine.handle.guid",
           Base64.encodeBase64String(opHandleIdentifier.getGuid))
-        resp.getConfiguration.put("kyuubi.session.launch.engine.handle.secret",
+        respConfiguration.put("kyuubi.session.launch.engine.handle.secret",
           Base64.encodeBase64String(opHandleIdentifier.getSecret))
       }
+
+      resp.setSessionHandle(sessionHandle.toTSessionHandle)
+      resp.setConfiguration(respConfiguration)
+      resp.setStatus(OK_STATUS)
+      Option(CURRENT_SERVER_CONTEXT.get()).foreach(_.setSessionHandle(sessionHandle))
+    } catch {
+      case e: Exception =>
+        error("Error opening session: ", e)
+        resp.setStatus(KyuubiSQLException.toTStatus(e, verbose = true))
     }
     resp
   }
