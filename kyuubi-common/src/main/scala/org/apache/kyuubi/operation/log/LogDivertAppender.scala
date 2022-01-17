2ec02b4df534eaadac7a2f7e4891a293174a9a82
Fei Wang <fwang12@ebay.com>
2022-01-17 09:45:11 +0800

    fix LogDivertAppender

--- kyuubi-common/src/main/scala/org/apache/kyuubi/operation/log/LogDivertAppender.scala
+++ kyuubi-common/src/main/scala/org/apache/kyuubi/operation/log/LogDivertAppender.scala
@@ -21,43 +21,123 @@ import java.io.CharArrayWriter
 
 import scala.collection.JavaConverters._
 
-import org.apache.log4j._
-import org.apache.log4j.spi.{Filter, LoggingEvent}
+import org.apache.logging.log4j.{Level, LogManager, Marker}
+import org.apache.logging.log4j.core.{Filter, LogEvent, Logger, StringLayout}
+import org.apache.logging.log4j.core.Filter.Result
+import org.apache.logging.log4j.core.LifeCycle.State
+import org.apache.logging.log4j.core.appender.{AbstractWriterAppender, ConsoleAppender, WriterManager}
+import org.apache.logging.log4j.core.config.Property
+import org.apache.logging.log4j.core.layout.PatternLayout
+import org.apache.logging.log4j.message.Message
 
-class LogDivertAppender extends WriterAppender {
+class LogDivertAppender(
+    name: String,
+    layout: StringLayout,
+    filter: Filter,
+    ignoreExceptions: Boolean,
+    immediateFlush: Boolean,
+    properties: Array[Property],
+    manager: WriterManager)
+  extends AbstractWriterAppender[WriterManager](
+    name, layout, filter, ignoreExceptions, immediateFlush, properties, manager) {
+  import LogDivertAppender._
 
   final private val writer = new CharArrayWriter
 
-  final private val lo = Logger.getRootLogger
-    .getAllAppenders.asScala
-    .find(_.isInstanceOf[ConsoleAppender])
-    .map(_.asInstanceOf[Appender].getLayout)
-    .getOrElse(new PatternLayout("%d{yy/MM/dd HH:mm:ss} %p %c{2}: %m%n"))
+  def this() = this("KyuubiEngineLogDivertAppender", initLayout(), null, false, true,
+    Array.empty,
+    new WriterManager(new CharArrayWriter(), "KyuubiEngineLogDivertAppender", initLayout(), true))
 
-  setName("KyuubiSparkSQLEngineLogDivertAppender")
-  setWriter(writer)
-  setLayout(lo)
-
-  addFilter { _: LoggingEvent =>
-    if (OperationLog.getCurrentOperationLog == null) Filter.DENY else Filter.NEUTRAL
-  }
+  addFilter(new NameFilter())
 
   /**
    * Overrides WriterAppender.subAppend(), which does the real logging. No need
    * to worry about concurrency since log4j calls this synchronously.
    */
-  override protected def subAppend(event: LoggingEvent): Unit = {
-    super.subAppend(event)
+  override def append(event: LogEvent): Unit = {
+    super.append(event)
     // That should've gone into our writer. Notify the LogContext.
     val logOutput = writer.toString
     writer.reset()
     val log = OperationLog.getCurrentOperationLog
     if (log != null) log.write(logOutput)
   }
+
+  class NameFilter extends Filter {
+    private var state: State = State.INITIALIZED
+
+    override def getOnMismatch: Result = Result.NEUTRAL
+
+    override def getOnMatch: Result = Result.NEUTRAL
+
+    // scalastyle:off
+    override def filter(logger: Logger, level: Level, marker: Marker, msg: String, objects: java.lang.Object*): Result = Result.NEUTRAL
+
+    override def filter(logger: Logger, level: Level, marker: Marker, s: String, o: Any): Result = Result.NEUTRAL
+
+    override def filter(logger: Logger, level: Level, marker: Marker, s: String, o: Any, o1: Any): Result = Result.NEUTRAL
+
+    override def filter(logger: Logger, level: Level, marker: Marker, s: String, o: Any, o1: Any, o2: Any): Result = Result.NEUTRAL
+
+    override def filter(logger: Logger, level: Level, marker: Marker, s: String, o: Any, o1: Any, o2: Any, o3: Any): Result = Result.NEUTRAL
+
+    override def filter(logger: Logger, level: Level, marker: Marker, s: String, o: Any, o1: Any, o2: Any, o3: Any, o4: Any): Result = Result.NEUTRAL
+
+    override def filter(logger: Logger, level: Level, marker: Marker, s: String, o: Any, o1: Any, o2: Any, o3: Any, o4: Any, o5: Any): Result = Result.NEUTRAL
+
+    override def filter(logger: Logger, level: Level, marker: Marker, s: String, o: Any, o1: Any, o2: Any, o3: Any, o4: Any, o5: Any, o6: Any): Result = Result.NEUTRAL
+
+    override def filter(logger: Logger, level: Level, marker: Marker, s: String, o: Any, o1: Any, o2: Any, o3: Any, o4: Any, o5: Any, o6: Any, o7: Any): Result = Result.NEUTRAL
+
+    override def filter(logger: Logger, level: Level, marker: Marker, s: String, o: Any, o1: Any, o2: Any, o3: Any, o4: Any, o5: Any, o6: Any, o7: Any, o8: Any): Result = Result.NEUTRAL
+
+    override def filter(logger: Logger, level: Level, marker: Marker, s: String, o: Any, o1: Any, o2: Any, o3: Any, o4: Any, o5: Any, o6: Any, o7: Any, o8: Any, o9: Any): Result = Result.NEUTRAL
+
+    override def filter(logger: Logger, level: Level, marker: Marker, o: Any, throwable: Throwable): Result = Result.NEUTRAL
+
+    override def filter(logger: Logger, level: Level, marker: Marker, message: Message, throwable: Throwable): Result = Result.NEUTRAL
+    // scalastyle:on
+
+    override def filter(event: LogEvent): Result = {
+      if (OperationLog.getCurrentOperationLog == null) {
+        Filter.Result.DENY
+      } else {
+        Filter.Result.NEUTRAL
+      }
+    }
+
+    override def getState: State = state
+
+    override def initialize(): Unit = {
+      state = State.INITIALIZED
+    }
+
+    override def start(): Unit = {
+      state = State.STARTED
+    }
+
+    override def stop(): Unit = {
+      state = State.STOPPED
+    }
+
+    override def isStarted: Boolean = state eq State.STARTED
+
+    override def isStopped: Boolean = state eq State.STOPPED
+  }
 }
 
 object LogDivertAppender {
+  def initLayout(): StringLayout = {
+    LogManager.getRootLogger.asInstanceOf[org.apache.logging.log4j.core.Logger]
+      .getAppenders.values().asScala
+      .find(ap => ap.isInstanceOf[ConsoleAppender] && ap.getLayout.isInstanceOf[StringLayout])
+      .map(_.getLayout.asInstanceOf[StringLayout])
+      .getOrElse(PatternLayout.newBuilder().withPattern(
+        "%d{yy/MM/dd HH:mm:ss} %p %c{2}: %m%n").build())
+  }
+
   def initialize(): Unit = {
-    org.apache.log4j.Logger.getRootLogger.addAppender(new LogDivertAppender())
+    org.apache.logging.log4j.LogManager.getRootLogger()
+      .asInstanceOf[org.apache.logging.log4j.core.Logger].addAppender(new LogDivertAppender())
   }
 }
