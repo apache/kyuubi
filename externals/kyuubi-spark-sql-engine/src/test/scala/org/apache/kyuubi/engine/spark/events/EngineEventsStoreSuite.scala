86608fa5127c3c90b9a4c1a89a7da1ce627cb848
wForget <643348094@qq.com>
2022-01-26 18:13:29 +0800

    fix tests

--- externals/kyuubi-spark-sql-engine/src/test/scala/org/apache/kyuubi/engine/spark/events/EngineEventsStoreSuite.scala
+++ externals/kyuubi-spark-sql-engine/src/test/scala/org/apache/kyuubi/engine/spark/events/EngineEventsStoreSuite.scala
@@ -17,6 +17,79 @@
 
 package org.apache.kyuubi.engine.spark.events
 
-import org.apache.kyuubi.KyuubiFunSuite
+import scala.collection.JavaConverters._
 
-class EngineEventsStoreSuite extends KyuubiFunSuite {}
+import org.apache.spark.kyuubi.SparkContextHelper
+
+import org.apache.kyuubi.engine.spark.WithSparkSQLEngine
+import org.apache.kyuubi.operation.HiveJDBCTestHelper
+
+class EngineEventsStoreSuite extends WithSparkSQLEngine with HiveJDBCTestHelper {
+
+  var store: EngineEventsStore = _
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    val kvStore = SparkContextHelper.getKvStore(spark.sparkContext)
+    store = new EngineEventsStore(kvStore)
+  }
+
+  override protected def beforeEach(): Unit = {
+    val kvstore = SparkContextHelper.getKvStore(spark.sparkContext)
+    kvstore.view(classOf[SessionEvent]).closeableIterator().asScala.foreach(j => {
+      kvstore.delete(j.getClass, j.sessionId)
+    })
+    kvstore.view(classOf[SparkOperationEvent]).closeableIterator().asScala.foreach(j => {
+      kvstore.delete(j.getClass, j.statementId)
+    })
+  }
+
+  test("EngineEventsStore session test") {
+    assert(store.getSessionList.isEmpty)
+    assert(store.getSessionCount == 0)
+    withJdbcStatement() { statement =>
+      statement.execute(
+        """
+          |SELECT
+          |  l.id % 100 k,
+          |  sum(l.id) sum,
+          |  count(l.id) cnt,
+          |  avg(l.id) avg,
+          |  min(l.id) min,
+          |  max(l.id) max
+          |from range(0, 100000L, 1, 100) l
+          |  left join range(0, 100000L, 2, 100) r ON l.id = r.id
+          |GROUP BY 1""".stripMargin)
+    }
+    assert(store.getSessionList.size == 1)
+    assert(store.getSessionCount == 1)
+  }
+
+  test("EngineEventsStore statement test") {
+    assert(store.getStatementList.isEmpty)
+    assert(store.getStatementCount == 0)
+    val sql = """
+                |SELECT
+                |  l.id % 100 k,
+                |  sum(l.id) sum,
+                |  count(l.id) cnt,
+                |  avg(l.id) avg,
+                |  min(l.id) min,
+                |  max(l.id) max
+                |from range(0, 100000L, 1, 100) l
+                |  left join range(0, 100000L, 2, 100) r ON l.id = r.id
+                |GROUP BY 1""".stripMargin
+    withJdbcStatement() { statement =>
+      statement.execute(sql)
+    }
+    val statementList = store.getStatementList
+    assert(statementList.size == 1)
+    assert(store.getStatementCount == 1)
+    val statementId = statementList(0).statementId
+    assert(store.getStatement(statementId).get.statement === sql)
+  }
+
+  override def withKyuubiConf: Map[String, String] = Map.empty
+
+  override protected def jdbcUrl: String = getJdbcUrl
+}
