4bbd48f89f233d463376dfce7b346cd1b776f30b
Cheng Pan <chengpan@apache.org>
2022-02-11 18:04:33 +0800

    Remove workaround

--- kyuubi-common/src/test/scala/org/apache/kyuubi/IcebergSuiteMixin.scala
+++ kyuubi-common/src/test/scala/org/apache/kyuubi/IcebergSuiteMixin.scala
@@ -42,7 +42,5 @@ trait IcebergSuiteMixin extends DataLakeSuiteMixin {
     s"spark.sql.catalog.$catalog" -> "org.apache.iceberg.spark.SparkCatalog",
     s"spark.sql.catalog.$catalog.type" -> "hadoop",
     s"spark.sql.catalog.$catalog.warehouse" -> warehouse.toString,
-    // https://github.com/apache/iceberg/pull/4024
-    s"spark.sql.catalog.$catalog.default-namespace" -> "default",
     "spark.jars" -> extraJars)
 }
