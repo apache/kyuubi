677e2a40ab421c33d27d4e5092ec892a382d4a80
sychen <sychen@trip.com>
2022-02-11 09:29:50 +0800

    [KYUUBI #1889] Improve the user experience of the configuration parameters of the document
    ### _Why are the changes needed?_
    https://kyuubi.apache.org/docs/latest/deployment/settings.html
    The configuration parameters in the document are too long and a newline is inserted(`<br/>`), which makes it difficult to search.
    And if the user copies the configuration parameters, it may become several lines.
    
    Use `<code>` to replace `<br/>`.
    
    https://github.com/apache/incubator-kyuubi/issues/1889
    
    ### _How was this patch tested?_
    - [x] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [x] Add screenshots for manual tests if appropriate
    
    - [ ] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #1890 from cxzl25/KYUUBI-1889.
    
    Closes #1889
    
    765b0622 [sychen] format metrics.md
    abeb0601 [sychen] add newline
    28865ad7 [sychen] increase max width
    2f9c7e05 [sychen] use <code>
    
    Authored-by: sychen <sychen@trip.com>
    Signed-off-by: ulysses-you <ulyssesyou@apache.org>

--- docs/conf.py
+++ docs/conf.py
@@ -118,7 +118,8 @@ pygments_style = 'sphinx'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
+html_style = 'css/custom.css'
 htmlhelp_basename = 'Recommonmarkdoc'
 
 github_doc_root = 'https://github.com/apache/incubator-kyuubi/tree/master/docs/'
