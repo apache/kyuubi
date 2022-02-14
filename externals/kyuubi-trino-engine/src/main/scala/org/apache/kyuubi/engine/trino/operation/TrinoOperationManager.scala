d117f7107a57477ae753dac4814b80ca8ce8f991
hongdongdong <hongdongdong@cmss.chinamobile.com>
2022-02-14 10:40:05 +0800

    [KYUUBI #1886] Add GetSchemas for trino engine

--- externals/kyuubi-trino-engine/src/main/scala/org/apache/kyuubi/engine/trino/operation/TrinoOperationManager.scala
+++ externals/kyuubi-trino-engine/src/main/scala/org/apache/kyuubi/engine/trino/operation/TrinoOperationManager.scala
@@ -47,7 +47,10 @@ class TrinoOperationManager extends OperationManager("TrinoOperationManager") {
   override def newGetSchemasOperation(
       session: Session,
       catalog: String,
-      schema: String): Operation = null
+      schema: String): Operation = {
+    val op = new GetSchemas(session, catalog, schema)
+    addOperation(op)
+  }
 
   override def newGetTablesOperation(
       session: Session,
