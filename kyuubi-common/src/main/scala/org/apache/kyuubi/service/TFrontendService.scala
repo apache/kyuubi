f8efcb7125579510356fe8e0e568d632d91b3d49
Cheng Pan <chengpan@apache.org>
2022-03-12 21:04:41 +0800

    [KYUUBI #2103] Revert "[KYUUBI #1948] Upgrade thrift version to 0.16.0"
    ### _Why are the changes needed?_
    
    This PR reverts "[KYUUBI #1948] Upgrade thrift version to 0.16.0" in branch-1.5 and some subsequent commits due to issues caused by breaking changes in Thrift, e.g. #2101, #2093
    
    We need more time to evaluate it and add more test cases to ensure stability before the next release.
    
    ### _How was this patch tested?_
    - [ ] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [x] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #2103 from pan3793/revert.
    
    Closes #2103
    
    Closes #1948
    
    575dfe8d [Cheng Pan] properties order
    4c94481e [Cheng Pan] fix deps
    bfbdeb89 [Cheng Pan] Add log4j deps for kyuubi-hive-jdbc test
    bad4e90e [Cheng Pan] surefire
    f2ae8b9b [Cheng Pan] Revert "[KYUUBI #1948] Upgrade thrift version to 0.16.0"
    4501f743 [Cheng Pan] Revert "[KYUUBI #1948][FOLLOWUP] Remove TFramedTransport in extension-spark-3-1 and extension-spark-3-2"
    cc04df49 [Cheng Pan] Revert "[KYUUBI #1996] Clean up log4j2 conf and enhance surefire plugin conf"
    52a20e17 [Cheng Pan] Revert "[KYUUBI #1948][FOLLOWUP] Relocate fb303 classes"
    
    Authored-by: Cheng Pan <chengpan@apache.org>
    Signed-off-by: Cheng Pan <chengpan@apache.org>

--- kyuubi-common/src/main/scala/org/apache/kyuubi/service/TFrontendService.scala
+++ kyuubi-common/src/main/scala/org/apache/kyuubi/service/TFrontendService.scala
@@ -554,9 +554,5 @@ private[kyuubi] object TFrontendService {
     }
 
     def getSessionHandle: SessionHandle = sessionHandle
-
-    override def unwrap[T](aClass: Class[T]): T = null.asInstanceOf[T]
-
-    override def isWrapperFor(aClass: Class[_]): Boolean = false
   }
 }
