c45a71ce88752c274c4d6f0b022134304ca9f05f
jiaoqingbo <1178404354@qq.com>
2022-03-14 17:18:14 +0800

    delete empty line

--- externals/kyuubi-flink-sql-engine/bin/flink-sql-engine.sh
+++ externals/kyuubi-flink-sql-engine/bin/flink-sql-engine.sh
@@ -26,19 +26,14 @@ if [[ -z "$FLINK_HOME" || ! -d "$FLINK_HOME" ]]; then
   exit 1
 fi
 
-
 # do NOT let config.sh detect FLINK_HOME
 _FLINK_HOME_DETERMINED=1 . "$FLINK_HOME/bin/config.sh"
 
 FLINK_IDENT_STRING=${FLINK_IDENT_STRING:-"$USER"}
 FLINK_SQL_CLIENT_JAR=$(find "$FLINK_OPT_DIR" -regex ".*flink-sql-client.*.jar")
-
-
 CC_CLASSPATH=`constructFlinkClassPath`
 
 FLINK_SQL_ENGINE_HOME="$(cd `dirname $0`/..; pwd)"
-
-
 if [[ "$FLINK_SQL_ENGINE_HOME" == "$KYUUBI_HOME/externals/engines/flink" ]]; then
   FLINK_SQL_ENGINE_LIB_DIR="$FLINK_SQL_ENGINE_HOME/lib"
   FLINK_SQL_ENGINE_JAR=$(find "$FLINK_SQL_ENGINE_LIB_DIR" -regex ".*/kyuubi-flink-sql-engine_.*\.jar")
@@ -50,17 +45,8 @@ else
   echo -e "\nFLINK_SQL_ENGINE_HOME $FLINK_SQL_ENGINE_HOME doesn't match production directory, assuming in development environment..."
   FLINK_SQL_ENGINE_LIB_DIR="$FLINK_SQL_ENGINE_HOME/target"
   FLINK_SQL_ENGINE_JAR=$(find "$FLINK_SQL_ENGINE_LIB_DIR" -regex '.*/kyuubi-flink-sql-engine_.*\.jar$' | grep -v '\-javadoc.jar$' | grep -v '\-tests.jar$')
-
   _FLINK_SQL_ENGINE_HADOOP_CLIENT_JARS=$(find $FLINK_SQL_ENGINE_LIB_DIR -regex '.*/hadoop-client-.*\.jar$' | tr '\n' ':')
-
-
-
   FLINK_HADOOP_CLASSPATH="${_FLINK_SQL_ENGINE_HADOOP_CLIENT_JARS%:}:${HADOOP_CONF_DIR}:${YARN_CONF_DIR}"
-
-
-
-
-
   log_file="unused.log"
   log4j2_conf_file="file:$FLINK_CONF_DIR/log4j-session.properties" # which send all logs to console
   logback_conf_file="unused.xml"
@@ -77,8 +63,6 @@ log_setting=(
 if [ -n "$FLINK_SQL_ENGINE_JAR" ]; then
   exec $JAVA_RUN ${FLINK_SQL_ENGINE_DYNAMIC_ARGS} "${log_setting[@]}" -cp ${FULL_CLASSPATH} \
     org.apache.kyuubi.engine.flink.FlinkSQLEngine "$@"
-
-
 else
   (>&2 echo "[ERROR] Flink SQL Engine JAR file 'kyuubi-flink-sql-engine*.jar' should be located in $FLINK_SQL_ENGINE_LIB_DIR.")
   exit 1
