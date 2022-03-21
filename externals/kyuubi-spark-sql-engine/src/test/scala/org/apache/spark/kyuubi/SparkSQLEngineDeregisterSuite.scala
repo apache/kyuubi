fcd5ff4d405efd6d73eb2d4c8921b25f63a26541
Nick Song <chun2184@163.com>
2022-03-21 09:49:44 +0800

    minor version need greater then 2

--- externals/kyuubi-spark-sql-engine/src/test/scala/org/apache/spark/kyuubi/SparkSQLEngineDeregisterSuite.scala
+++ externals/kyuubi-spark-sql-engine/src/test/scala/org/apache/spark/kyuubi/SparkSQLEngineDeregisterSuite.scala
@@ -60,7 +60,7 @@ class SparkSQLEngineDeregisterExceptionSuite extends SparkSQLEngineDeregisterSui
     super.withKyuubiConf ++ Map(ENGINE_DEREGISTER_EXCEPTION_CLASSES.key -> {
       sparkMajorMinorVersion match {
         // see https://issues.apache.org/jira/browse/SPARK-35958
-        case (3, minor) if minor >= 2 => "org.apache.spark.SparkArithmeticException"
+        case (3, minor) if minor > 2 => "org.apache.spark.SparkArithmeticException"
         case _ => classOf[ArithmeticException].getCanonicalName
       }
     })
@@ -85,7 +85,7 @@ class SparkSQLEngineDeregisterExceptionTTLSuite extends WithDiscoverySparkSQLEng
       ENGINE_DEREGISTER_EXCEPTION_CLASSES.key -> {
         sparkMajorMinorVersion match {
           // see https://issues.apache.org/jira/browse/SPARK-35958
-          case (3, minor) if minor >= 2 => "org.apache.spark.SparkArithmeticException"
+          case (3, minor) if minor > 2 => "org.apache.spark.SparkArithmeticException"
           case _ => classOf[ArithmeticException].getCanonicalName
         }
       },
