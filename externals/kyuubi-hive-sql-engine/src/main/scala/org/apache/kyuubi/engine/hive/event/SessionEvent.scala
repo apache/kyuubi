0bd9d9ee1e77975a410fb179afe152f63753e75e
Min Zhao <zhaomin1423@163.com>
2022-03-23 22:45:49 +0800

    add hive event

---
+++ externals/kyuubi-hive-sql-engine/src/main/scala/org/apache/kyuubi/engine/hive/event/SessionEvent.scala
@@ -0,0 +1,49 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kyuubi.engine.hive.event
+
+import org.apache.kyuubi.Utils
+import org.apache.kyuubi.engine.hive.session.HiveSessionImpl
+import org.apache.kyuubi.events.KyuubiEvent
+
+case class SessionEvent(sessionId: String,
+                        engineId: String,
+                        username: String,
+                        ip: String,
+                        serverIp: String,
+                        startTime: Long,
+                        var endTime: Long = -1L,
+                        var totalOperations: Int = 0) extends KyuubiEvent {
+
+  override def partitions: Seq[(String, String)] =
+    ("day", Utils.getDateFromTimestamp(startTime)) :: Nil
+
+}
+
+object SessionEvent {
+
+  def apply(session: HiveSessionImpl): SessionEvent = {
+    new SessionEvent(
+      session.handle.identifier.toString,
+      engineId = "",
+      session.user,
+      session.ipAddress,
+      serverIp = "",
+      session.createTime
+    )
+  }
+}
