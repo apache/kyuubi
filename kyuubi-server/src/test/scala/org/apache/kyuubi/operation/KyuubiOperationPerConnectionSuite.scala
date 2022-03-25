e8445b7f28920c35aa2bd8e53c545143cd902dd2
Fei Wang <fwang12@ebay.com>
2022-03-26 00:23:44 +0800

    [KYUUBI #2102] Support to retry the internal thrift request call and add engine liveness probe to enable fast fail before retry
    ### _Why are the changes needed?_
    
    To close #2102
    
    Support to retry all the internal thrift request calls(except RenewDelegationToken now), and fast fail if the remote engine is not stable or not alive.
    
    In this PR, it supports engine liveness probe.
    
    If it is enabled, a companion thrift client will be created and open a liveness probe session when opening remote engine session.
    It will send some simple thrift request(GetInfo) to check whether the remote engine is alive, and fast fail before retry if remote engine is not connectable.
    
    #### Why not use the same thrift client to check engine liveness before retry?
    I tried that, but met `out of resp sequence` error.
    For example:
    1. send getOperationStatus request
    2.  read time out
    3. send GetInfoType request
    4. receive getOperationStatus response (out of resp sequence)
    
    ### _How was this patch tested?_
    - [x] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [x] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #2122 from turboFei/retry_rpc.
    
    Closes #2102
    
    3926ba04 [Fei Wang] adress comments
    ade4ede6 [Fei Wang] add timeout
    1b7a64f9 [Fei Wang] Only check remote engine alive before retry
    98e03f8e [Fei Wang] refactor
    fac388cf [Fei Wang] remove unused import
    9c6d8737 [Fei Wang] add ut
    9b595650 [Fei Wang] Support to retry the thrift request and engine alive probe
    
    Authored-by: Fei Wang <fwang12@ebay.com>
    Signed-off-by: Fei Wang <fwang12@ebay.com>

--- kyuubi-server/src/test/scala/org/apache/kyuubi/operation/KyuubiOperationPerConnectionSuite.scala
+++ kyuubi-server/src/test/scala/org/apache/kyuubi/operation/KyuubiOperationPerConnectionSuite.scala
@@ -216,6 +216,27 @@ class KyuubiOperationPerConnectionSuite extends WithKyuubiServer with HiveJDBCTe
       assert(kyuubiConnection.isClosed)
     }
   }
+
+  test("KYUUBI #2102 - support engine alive probe to fast fail on engine broken") {
+    withSessionConf(Map(
+      KyuubiConf.ENGINE_ALIVE_PROBE_ENABLED.key -> "true",
+      KyuubiConf.ENGINE_ALIVE_PROBE_INTERVAL.key -> "100",
+      KyuubiConf.ENGINE_ALIVE_TIMEOUT.key -> "3000",
+      KyuubiConf.OPERATION_THRIFT_CLIENT_REQUEST_MAX_ATTEMPTS.key -> "10000",
+      KyuubiConf.ENGINE_REQUEST_TIMEOUT.key -> "100"))(Map.empty)(Map.empty) {
+      withSessionHandle { (client, handle) =>
+        val executeStmtReq = new TExecuteStatementReq()
+        executeStmtReq.setStatement("select java_method('java.lang.System', 'exit', 1)")
+        executeStmtReq.setSessionHandle(handle)
+        executeStmtReq.setRunAsync(false)
+        val startTime = System.currentTimeMillis()
+        val executeStmtResp = client.ExecuteStatement(executeStmtReq)
+        assert(executeStmtResp.getStatus.getStatusCode === TStatusCode.ERROR_STATUS)
+        val elapsedTime = System.currentTimeMillis() - startTime
+        assert(elapsedTime > 3 * 1000 && elapsedTime < 20 * 1000)
+      }
+    }
+  }
 }
 
 class TestSessionConfAdvisor extends SessionConfAdvisor {
