cffabce7418c8855858e79adaa669ee7d611e80f
Kent Yao <yao@apache.org>
2022-03-29 19:47:30 +0800

    rm dead code

--- dev/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/PrivilegeObject.scala
+++ dev/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/PrivilegeObject.scala
@@ -17,8 +17,6 @@
 
 package org.apache.kyuubi.plugin.spark.authz
 
-import org.apache.commons.lang3.StringUtils
-
 import org.apache.kyuubi.plugin.spark.authz.PrivilegeObjectActionType.PrivilegeObjectActionType
 import org.apache.kyuubi.plugin.spark.authz.PrivilegeObjectType.PrivilegeObjectType
 
@@ -27,61 +25,5 @@ case class PrivilegeObject(
     actionType: PrivilegeObjectActionType,
     dbname: String,
     objectName: String,
-    columns: Seq[String]) extends Comparable[PrivilegeObject] {
-
-  private def compare(left: Seq[String], right: Seq[String]): Int = {
-    if (left == right) {
-      0
-    } else if (left == null) {
-      -1
-    } else if (right == null) {
-      1
-    } else {
-      left.zip(right).foreach { case (l, r) =>
-        val comp = StringUtils.compare(l, r)
-        if (comp != 0) {
-          return comp
-        }
-      }
-      left.size - right.size
-    }
-  }
-
-  override def compareTo(o: PrivilegeObject): Int = {
-    var comp = privilegeObjectType.compareTo(o.privilegeObjectType)
-    if (comp == 0) {
-      comp = StringUtils.compare(dbname, o.dbname)
-      if (comp == 0) {
-        comp = StringUtils.compare(objectName, o.objectName)
-        if (comp == 0) {
-          compare(columns, o.columns)
-        } else {
-          comp
-        }
-      } else {
-        comp
-      }
-    } else {
-      comp
-    }
-  }
-}
-
-object PrivilegeObject {
-  def apply(
-      typ: PrivilegeObjectType,
-      actionType: PrivilegeObjectActionType,
-      dbname: String,
-      objectName: String,
-      columns: Seq[String]): PrivilegeObject = {
-    new PrivilegeObject(typ, actionType, dbname, objectName, columns)
-  }
-
-  def apply(
-      typ: PrivilegeObjectType,
-      actionType: PrivilegeObjectActionType,
-      dbname: String,
-      objectName: String): PrivilegeObject = {
-    new PrivilegeObject(typ, actionType, dbname, objectName, Nil)
-  }
+    columns: Seq[String] = Nil) {
 }
