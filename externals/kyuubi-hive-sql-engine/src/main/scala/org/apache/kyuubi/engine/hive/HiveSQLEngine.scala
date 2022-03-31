9da22e66f3b8dc47de49260a94d4241529186169
Min Zhao <zhaomin1423@163.com>
2022-03-31 15:57:18 +0800

    [KYUUBI #2028] Hive Backend Engine - Events support
    ### _Why are the changes needed?_
    
    Add events support for hive engine.
    
    ### _How was this patch tested?_
    - [ ] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [x] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #2213 from zhaomin1423/hive_event.
    
    Closes #2028
    
    6e39f032 [Min Zhao] [KYUUBI #2028] Hive Backend Engine - Events support
    ae274c93 [Min Zhao] [KYUUBI #2028] Hive Backend Engine - Events support
    c71f0559 [Min Zhao] [KYUUBI #2028] Hive Backend Engine - Events support
    d58aae88 [Min Zhao] [KYUUBI #2028] Hive Backend Engine - Events support
    a1c08f09 [Min Zhao] [KYUUBI #2028] Hive Backend Engine - Events support
    aba2f93d [Min Zhao] [KYUUBI #2028] Hive Backend Engine - Events support
    0f7e1e06 [Min Zhao] add hive event
    0bd9d9ee [Min Zhao] add hive event
    
    Authored-by: Min Zhao <zhaomin1423@163.com>
    Signed-off-by: Kent Yao <yao@apache.org>

--- externals/kyuubi-hive-sql-engine/src/main/scala/org/apache/kyuubi/engine/hive/HiveSQLEngine.scala
+++ externals/kyuubi-hive-sql-engine/src/main/scala/org/apache/kyuubi/engine/hive/HiveSQLEngine.scala
@@ -17,15 +17,23 @@
 
 package org.apache.kyuubi.engine.hive
 
+import java.net.InetAddress
+
+import scala.util.control.NonFatal
+
 import org.apache.hadoop.hive.conf.HiveConf
 import org.apache.hadoop.hive.conf.HiveConf.ConfVars
 
 import org.apache.kyuubi.{Logging, Utils}
 import org.apache.kyuubi.config.KyuubiConf
+import org.apache.kyuubi.config.KyuubiConf.{ENGINE_EVENT_JSON_LOG_PATH, ENGINE_EVENT_LOGGERS}
+import org.apache.kyuubi.engine.hive.events.HiveEngineEvent
+import org.apache.kyuubi.engine.hive.events.handler.HiveJsonLoggingEventHandler
+import org.apache.kyuubi.events.{EventBus, EventLoggerType, KyuubiEvent}
 import org.apache.kyuubi.ha.HighAvailabilityConf.HA_ZK_CONN_RETRY_POLICY
 import org.apache.kyuubi.ha.client.RetryPolicies
 import org.apache.kyuubi.service.{AbstractBackendService, AbstractFrontendService, Serverable}
-import org.apache.kyuubi.util.SignalRegister
+import org.apache.kyuubi.util.{KyuubiHadoopUtils, SignalRegister}
 
 class HiveSQLEngine extends Serverable("HiveSQLEngine") {
   override val backendService: AbstractBackendService = new HiveBackendService(this)
@@ -46,8 +54,15 @@ object HiveSQLEngine extends Logging {
   var currentEngine: Option[HiveSQLEngine] = None
   val hiveConf = new HiveConf()
   val kyuubiConf = new KyuubiConf()
+  kyuubiConf.set(ENGINE_EVENT_LOGGERS.key, "JSON")
 
   def startEngine(): HiveSQLEngine = {
+    try {
+      initLoggerEventHandler(kyuubiConf)
+    } catch {
+      case NonFatal(e) =>
+        warn(s"Failed to initialize Logger EventHandler: ${e.getMessage}", e)
+    }
     kyuubiConf.setIfMissing(KyuubiConf.FRONTEND_THRIFT_BINARY_BIND_PORT, 0)
     kyuubiConf.setIfMissing(HA_ZK_CONN_RETRY_POLICY, RetryPolicies.N_TIME.toString)
 
@@ -72,10 +87,31 @@ object HiveSQLEngine extends Logging {
     val engine = new HiveSQLEngine()
     info(s"Starting ${engine.getName}")
     engine.initialize(kyuubiConf)
+    EventBus.post(HiveEngineEvent(engine))
     engine.start()
+    val event = HiveEngineEvent(engine)
+    info(event)
+    EventBus.post(event)
     Utils.addShutdownHook(() => engine.stop())
     currentEngine = Some(engine)
     engine
+  }
+
+  private def initLoggerEventHandler(conf: KyuubiConf): Unit = {
+    val hadoopConf = KyuubiHadoopUtils.newHadoopConf(conf)
+    conf.get(ENGINE_EVENT_LOGGERS).map(EventLoggerType.withName).foreach {
+      case EventLoggerType.JSON =>
+        val hostName = InetAddress.getLocalHost.getCanonicalHostName
+        val handler = HiveJsonLoggingEventHandler(
+          s"Hive-$hostName",
+          ENGINE_EVENT_JSON_LOG_PATH,
+          hadoopConf,
+          conf)
+        EventBus.register[KyuubiEvent](handler)
+      case logger =>
+        throw new IllegalArgumentException(s"Unrecognized event logger: $logger")
+
+    }
   }
 
   def main(args: Array[String]): Unit = {
@@ -83,9 +119,14 @@ object HiveSQLEngine extends Logging {
     try {
       startEngine()
     } catch {
-      case t: Throwable =>
-        error(t)
-        currentEngine.foreach(_.stop())
+      case t: Throwable => currentEngine match {
+          case Some(engine) =>
+            engine.stop()
+            val event = HiveEngineEvent(engine).copy(diagnostic = t.getMessage)
+            EventBus.post(event)
+          case _ =>
+            error(s"Failed to start Hive SQL engine: ${t.getMessage}.", t)
+        }
     }
   }
 }
