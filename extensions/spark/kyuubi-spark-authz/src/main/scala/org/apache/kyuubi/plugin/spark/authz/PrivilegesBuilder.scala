513ea83438e0927aa8c6d222d8cf3d983ffb416f
Kent Yao <yao@apache.org>
2022-03-31 15:37:40 +0800

    [KYUUBI #1451] Introduce Kyuubi Spark AuthZ Module with column-level fine-grained authorization
    ### _Why are the changes needed?_
    
     - #1451
    
    ### Kyuubi Spark AuthZ Extension
    
    #### Functions
    
    - [x] Column-level fine-grained authorization
    - [ ] Row-level fine-grained authorization, a.k.a. Row-level filtering
    - [ ] Data masking
    
    #### Build
    
    ```shell
    build/mvn clean package -pl :kyuubi-spark-authz_2.12 -Dspark.version=3.2.1 -Dranger.version=2.2.0
    ```
    
    #### Supported Apache Spark Versions
    
    `-Dspark.version=`
    
    - [x] master
    - [x] 3.3.x
    - [x] 3.2.x (default)
    - [x] 3.1.x
    - [x] 3.0.x
    - [x] 2.4.x
    - [ ] 2.3.x and earlier
    
    #### Supported Apache Ranger Versions
    
    `-Dranger.version=`
    
    - [x] 3.0.0-SNAPSHOT
    - [x] 2.2.x (default)
    - [x] 2.1.x
    - [x] 2.0.x
    - [x] 1.2.x
    - [x] 1.1.x
    - [x] 1.0.x
    - [x] 0.7.x
    - [x] 0.6.x
    
    ### TODO LIST
    - [ ] Doc
    - [ ] Auditing
    - [ ] Support row-level filtering
    - [ ] Support Data masking
    - [ ] Support v2 DDLs
    - [ ] Support Custom Commands like create hudi table etc.
    
    ### _How was this patch tested?_
    - [x] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [x] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #2160 from yaooqinn/1451.
    
    Closes #1451
    
    3ae4a833 [Kent Yao] nit
    c8ab1ef0 [Kent Yao] nit
    68e71ac7 [Kent Yao] readme
    1a1dc994 [Kent Yao] style
    f5fda710 [Kent Yao] support Spark 2.4.7
    d1d5318e [Kent Yao] support ranger 0.6
    6e57ce3b [Kent Yao] support ranger 0.7
    f0dce7ce [Kent Yao] minor
    120c6e3e [Kent Yao] Update extensions/spark/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/ranger/AccessResource.scala
    06f73e99 [Kent Yao] add comments
    344e3a8f [Kent Yao] simpler
    cffabce7 [Kent Yao] rm dead code
    ec5edae7 [Kent Yao] rm dead code
    1384f2b6 [Kent Yao] rm log4j 1
    a6c0d0bb [Kent Yao] reduce configs
    06c0452b [Kent Yao] deps
    cf599e15 [Kent Yao] ranger tests
    3adc6cf4 [Kent Yao] ranger tests
    94a841ee [Kent Yao] ranger tests
    c5af35fd [Kent Yao] starting working on ranger
    5cb52e3a [Kent Yao] queries
    5318dca2 [Kent Yao] clear cache before each
    3427b679 [Kent Yao] join inner
    cfada65d [Kent Yao] show
    4dca5cfa [Kent Yao] show clos
    a2d02ae0 [Kent Yao] truncate table
    7424ad0c [Kent Yao] set db & desc db
    046ac025 [Kent Yao] desc table & col
    caaaf619 [Kent Yao] DescribeColumnCommand
    77b2efcf [Kent Yao] drop/refresh function
    716e05be [Kent Yao] create function
    0d3dc247 [Kent Yao] create function
    666e4d4f [Kent Yao] Create table as select
    e9aaa0e9 [Kent Yao] Create table
    3a2389a2 [Kent Yao] Create View
    395ec13f [Kent Yao] CacheTableAsSelect
    1544f818 [Kent Yao] fix
    4bb9c624 [Kent Yao] more tests
    aa134ade [Kent Yao] alter view as
    131d95bb [Kent Yao] alter view as
    b1722d7e [Kent Yao] test & opt
    01ba1585 [Kent Yao] build default
    f9811b0d [Kent Yao] test add
    a9c044ac [Kent Yao] test
    14685630 [Kent Yao] test
    6c0b5ed9 [Kent Yao] format
    810f9992 [Kent Yao] [WIP] Migrate Spark security module from submarine to kyuubi
    a3fd5d20 [Kent Yao] [WIP] Migrate Spark security module from submarine to kyuubi
    3fe1bf4c [Kent Yao] [WIP] Migrate Spark security module from submarine to kyuubi
    d16a0c3c [Kent Yao] [WIP] Migrate Spark security module from submarine to kyuubi
    
    Authored-by: Kent Yao <yao@apache.org>
    Signed-off-by: Kent Yao <yao@apache.org>

---
+++ extensions/spark/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/PrivilegesBuilder.scala
@@ -0,0 +1,498 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.plugin.spark.authz
+
+import scala.collection.mutable.ArrayBuffer
+import scala.util.{Failure, Success, Try}
+
+import org.apache.spark.SPARK_VERSION
+import org.apache.spark.sql.catalyst.{FunctionIdentifier, TableIdentifier}
+import org.apache.spark.sql.catalyst.catalog.CatalogTable
+import org.apache.spark.sql.catalyst.catalog.CatalogTypes.TablePartitionSpec
+import org.apache.spark.sql.catalyst.expressions.{Expression, NamedExpression}
+import org.apache.spark.sql.catalyst.plans.logical.{Command, Filter, Join, LogicalPlan, Project}
+import org.apache.spark.sql.execution.datasources.LogicalRelation
+import org.apache.spark.sql.types.StructField
+
+import org.apache.kyuubi.plugin.spark.authz.PrivilegeObjectActionType._
+import org.apache.kyuubi.plugin.spark.authz.PrivilegeObjectType._
+
+object PrivilegesBuilder {
+
+  private val versionParts = SPARK_VERSION.split('.')
+  private val majorVersion: Int = versionParts.head.toInt
+  private val minorVersion: Int = versionParts(1).toInt
+
+  private def quoteIfNeeded(part: String): String = {
+    if (part.matches("[a-zA-Z0-9_]+") && !part.matches("\\d+")) {
+      part
+    } else {
+      s"`${part.replace("`", "``")}`"
+    }
+  }
+
+  private def quote(parts: Seq[String]): String = {
+    parts.map(quoteIfNeeded).mkString(".")
+  }
+
+  /**
+   * fixme error handling need improve here
+   */
+  private def getFieldVal[T](o: Any, name: String): T = {
+    Try {
+      val field = o.getClass.getDeclaredField(name)
+      field.setAccessible(true)
+      field.get(o)
+    } match {
+      case Success(value) => value.asInstanceOf[T]
+      case Failure(e) =>
+        val candidates = o.getClass.getDeclaredFields.map(_.getName).mkString("[", ",", "]")
+        throw new RuntimeException(s"$name not in $candidates", e)
+    }
+  }
+
+  private def databasePrivileges(db: String): PrivilegeObject = {
+    PrivilegeObject(DATABASE, PrivilegeObjectActionType.OTHER, db, db)
+  }
+
+  private def tablePrivileges(
+      table: TableIdentifier,
+      columns: Seq[String] = Nil,
+      actionType: PrivilegeObjectActionType = PrivilegeObjectActionType.OTHER): PrivilegeObject = {
+    PrivilegeObject(TABLE_OR_VIEW, actionType, table.database.orNull, table.table, columns)
+  }
+
+  private def functionPrivileges(
+      db: String,
+      functionName: String): PrivilegeObject = {
+    PrivilegeObject(FUNCTION, PrivilegeObjectActionType.OTHER, db, functionName)
+  }
+
+  private def collectLeaves(expr: Expression): Seq[NamedExpression] = {
+    expr.collect { case p: NamedExpression if p.children.isEmpty => p }
+  }
+
+  /**
+   * Build PrivilegeObjects from Spark LogicalPlan
+   *
+   * @param plan a Spark LogicalPlan used to generate SparkPrivilegeObjects
+   * @param privilegeObjects input or output spark privilege object list
+   * @param projectionList Projection list after pruning
+   */
+  private def buildQuery(
+      plan: LogicalPlan,
+      privilegeObjects: ArrayBuffer[PrivilegeObject],
+      projectionList: Seq[NamedExpression] = Nil): Unit = {
+
+    def mergeProjection(table: CatalogTable, plan: LogicalPlan): Unit = {
+      if (projectionList.isEmpty) {
+        privilegeObjects += tablePrivileges(
+          table.identifier,
+          table.schema.fieldNames)
+      } else {
+        val cols = projectionList.flatMap(collectLeaves)
+          .filter(plan.outputSet.contains).map(_.name).distinct
+        privilegeObjects += tablePrivileges(table.identifier, cols)
+      }
+    }
+
+    plan match {
+      case p: Project => buildQuery(p.child, privilegeObjects, p.projectList)
+
+      case j: Join =>
+        val cols =
+          projectionList ++ j.condition.map(expr => collectLeaves(expr)).getOrElse(Nil)
+        buildQuery(j.left, privilegeObjects, cols)
+        buildQuery(j.right, privilegeObjects, cols)
+
+      case f: Filter =>
+        val cols = projectionList ++ collectLeaves(f.condition)
+        buildQuery(f.child, privilegeObjects, cols)
+
+      case h if h.nodeName == "HiveTableRelation" =>
+        mergeProjection(getFieldVal[CatalogTable](h, "tableMeta"), h)
+
+      case l if l.nodeName == "LogicalRelation" =>
+        getFieldVal[Option[CatalogTable]](l, "catalogTable").foreach { t =>
+          mergeProjection(t, plan)
+        }
+
+      case u if u.nodeName == "UnresolvedRelation" =>
+        val tableNameM = u.getClass.getMethod("tableName")
+        val parts = tableNameM.invoke(u).asInstanceOf[String].split("\\.")
+        val db = quote(parts.init)
+        privilegeObjects += tablePrivileges(TableIdentifier(parts.last, Some(db)))
+
+      case p =>
+        for (child <- p.children) {
+          buildQuery(child, privilegeObjects, projectionList)
+        }
+    }
+  }
+
+  /**
+   * Build PrivilegeObjects from Spark LogicalPlan
+   * @param plan a Spark LogicalPlan used to generate Spark PrivilegeObjects
+   * @param inputObjs input privilege object list
+   * @param outputObjs output privilege object list
+   */
+  private def buildCommand(
+      plan: LogicalPlan,
+      inputObjs: ArrayBuffer[PrivilegeObject],
+      outputObjs: ArrayBuffer[PrivilegeObject]): Unit = {
+
+    def getPlanField[T](field: String): T = {
+      getFieldVal[T](plan, field)
+    }
+
+    def getTableName: TableIdentifier = {
+      getPlanField[TableIdentifier]("tableName")
+    }
+
+    def getTableIdent: TableIdentifier = {
+      getPlanField[TableIdentifier]("tableIdent")
+    }
+
+    def getMultipartIdentifier: TableIdentifier = {
+      val multipartIdentifier = getPlanField[Seq[String]]("multipartIdentifier")
+      assert(multipartIdentifier.nonEmpty)
+      val table = multipartIdentifier.last
+      val db = Some(quote(multipartIdentifier.init))
+      TableIdentifier(table, db)
+    }
+
+    def getQuery: LogicalPlan = {
+      getPlanField[LogicalPlan]("query")
+    }
+
+    plan.nodeName match {
+      case "AlterDatabasePropertiesCommand" |
+          "AlterDatabaseSetLocationCommand" |
+          "CreateDatabaseCommand" |
+          "DropDatabaseCommand" =>
+        val database = getPlanField[String]("databaseName")
+        outputObjs += databasePrivileges(database)
+
+      case "AlterTableAddColumnsCommand" =>
+        val table = getPlanField[TableIdentifier]("table")
+        val cols = getPlanField[Seq[StructField]]("colsToAdd").map(_.name)
+        outputObjs += tablePrivileges(table, cols)
+
+      case "AlterTableAddPartitionCommand" =>
+        val table = getTableName
+        val cols = getPlanField[Seq[(TablePartitionSpec, Option[String])]]("partitionSpecsAndLocs")
+          .flatMap(_._1.keySet).distinct
+        outputObjs += tablePrivileges(table, cols)
+
+      case "AlterTableChangeColumnCommand" =>
+        val table = getTableName
+        val cols = getPlanField[String]("columnName") :: Nil
+        outputObjs += tablePrivileges(table, cols)
+
+      case "AlterTableDropPartitionCommand" =>
+        val table = getTableName
+        val cols = getPlanField[Seq[TablePartitionSpec]]("specs").flatMap(_.keySet).distinct
+        outputObjs += tablePrivileges(table, cols)
+
+      case "AlterTableRenameCommand" =>
+        val oldTable = getPlanField[TableIdentifier]("oldName")
+        val newTable = getPlanField[TableIdentifier]("newName")
+        outputObjs += tablePrivileges(oldTable, actionType = PrivilegeObjectActionType.DELETE)
+        outputObjs += tablePrivileges(newTable)
+
+      case "AlterTableRenamePartitionCommand" =>
+        val table = getTableName
+        val cols = getPlanField[TablePartitionSpec]("oldPartition").keySet.toSeq
+        outputObjs += tablePrivileges(table, cols)
+
+      case "AlterTableSerDePropertiesCommand" =>
+        val table = getTableName
+        val cols = getPlanField[Option[TablePartitionSpec]]("partSpec")
+          .toSeq.flatMap(_.keySet)
+        outputObjs += tablePrivileges(table, cols)
+
+      case "AlterTableSetLocationCommand" =>
+        val table = getTableName
+        val cols = getPlanField[Option[TablePartitionSpec]]("partitionSpec")
+          .toSeq.flatMap(_.keySet)
+        outputObjs += tablePrivileges(table, cols)
+
+      case "AlterTableSetPropertiesCommand" |
+          "AlterTableUnsetPropertiesCommand" =>
+        val table = getTableName
+        outputObjs += tablePrivileges(table)
+
+      case "AlterViewAsCommand" =>
+        val view = getPlanField[TableIdentifier]("name")
+        outputObjs += tablePrivileges(view)
+        buildQuery(getQuery, inputObjs)
+
+      case "AlterViewAs" =>
+
+      case "AnalyzeColumnCommand" =>
+        val table = getTableIdent
+        val cols =
+          if (majorVersion >= 3) {
+            getPlanField[Option[Seq[String]]]("columnNames").getOrElse(Nil)
+          } else {
+            getPlanField[Seq[String]]("columnNames")
+          }
+        inputObjs += tablePrivileges(table, cols)
+
+      case "AnalyzePartitionCommand" =>
+        val table = getTableIdent
+        val cols = getPlanField[Map[String, Option[String]]]("partitionSpec")
+          .keySet.toSeq
+        inputObjs += tablePrivileges(table, cols)
+
+      case "AnalyzeTableCommand" |
+          "RefreshTableCommand" |
+          "RefreshTable" =>
+        inputObjs += tablePrivileges(getTableIdent)
+
+      case "AnalyzeTablesCommand" |
+          "ShowTablesCommand" =>
+        val db = getPlanField[Option[String]]("databaseName")
+        if (db.nonEmpty) {
+          inputObjs += databasePrivileges(db.get)
+        }
+
+      case "CacheTable" =>
+        // >= 3.2
+        outputObjs += tablePrivileges(getMultipartIdentifier)
+        val query = getPlanField[LogicalPlan]("table") // table to cache
+        buildQuery(query, inputObjs)
+
+      case "CacheTableCommand" =>
+        if (majorVersion == 3 && minorVersion == 1) {
+          outputObjs += tablePrivileges(getMultipartIdentifier)
+        } else {
+          outputObjs += tablePrivileges(getTableIdent)
+        }
+        getPlanField[Option[LogicalPlan]]("plan").foreach(buildQuery(_, inputObjs))
+
+      case "CacheTableAsSelect" =>
+        val view = getPlanField[String]("tempViewName")
+        outputObjs += tablePrivileges(TableIdentifier(view))
+
+        val query = getPlanField[LogicalPlan]("plan")
+        buildQuery(query, inputObjs)
+
+      case "CreateViewCommand" =>
+        val view = getPlanField[TableIdentifier]("name")
+        outputObjs += tablePrivileges(view)
+        val query =
+          if (majorVersion < 3 || (majorVersion == 3 && minorVersion <= 1)) {
+            getPlanField[LogicalPlan]("child")
+          } else {
+            getPlanField[LogicalPlan]("plan")
+          }
+        buildQuery(query, inputObjs)
+
+      case "CreateView" => // revisit this after spark has view catalog
+
+      case "CreateDataSourceTableCommand" | "CreateTableCommand" =>
+        val table = getPlanField[CatalogTable]("table").identifier
+        // fixme: do we need to add columns to check?
+        outputObjs += tablePrivileges(table)
+
+      case "CreateDataSourceTableAsSelectCommand" |
+          "OptimizedCreateHiveTableAsSelectCommand" |
+          "InsertIntoHiveTable" =>
+        val table = getPlanField[CatalogTable]("table").identifier
+        outputObjs += tablePrivileges(table)
+        buildQuery(getQuery, inputObjs)
+
+      case "CreateHiveTableAsSelectCommand" =>
+        val table = getPlanField[CatalogTable]("tableDesc").identifier
+        val cols = getPlanField[Seq[String]]("outputColumnNames")
+        outputObjs += tablePrivileges(table, cols)
+        buildQuery(getQuery, inputObjs)
+
+      case "CreateFunctionCommand" |
+          "DropFunctionCommand" |
+          "RefreshFunctionCommand" =>
+        val db = getPlanField[Option[String]]("databaseName")
+        val functionName = getPlanField[String]("functionName")
+        outputObjs += functionPrivileges(db.orNull, functionName)
+
+      case "CreateTableAsSelect" |
+          "ReplaceTableAsSelect" =>
+        val left = getPlanField[LogicalPlan]("name")
+        left.nodeName match {
+          case "ResolvedDBObjectName" =>
+            val nameParts = getPlanField[Seq[String]]("nameParts")
+            val db = Some(quote(nameParts.init))
+            outputObjs += tablePrivileges(TableIdentifier(nameParts.last, db))
+          case _ =>
+        }
+        buildQuery(getQuery, inputObjs)
+
+      case "CreateTableLikeCommand" =>
+        val target = getPlanField[TableIdentifier]("targetTable")
+        val source = getPlanField[TableIdentifier]("sourceTable")
+        inputObjs += tablePrivileges(source)
+        outputObjs += tablePrivileges(target)
+
+      case "CreateTempViewUsing" =>
+        outputObjs += tablePrivileges(getTableIdent)
+
+      case "DescribeColumnCommand" =>
+        val table = getPlanField[TableIdentifier]("table")
+        val cols = getPlanField[Seq[String]]("colNameParts").takeRight(1)
+        inputObjs += tablePrivileges(table, cols)
+
+      case "DescribeTableCommand" =>
+        val table = getPlanField[TableIdentifier]("table")
+        inputObjs += tablePrivileges(table)
+
+      case "DescribeDatabaseCommand" | "SetDatabaseCommand" =>
+        val database = getPlanField[String]("databaseName")
+        inputObjs += databasePrivileges(database)
+
+      case "DescribeFunctionCommand" =>
+        val func = getPlanField[FunctionIdentifier]("functionName")
+        inputObjs += functionPrivileges(func.database.orNull, func.funcName)
+
+      case "DropTableCommand" =>
+        outputObjs += tablePrivileges(getTableName)
+
+      case "ExplainCommand" =>
+
+      case "ExternalCommandExecutor" =>
+
+      case "InsertIntoDataSourceCommand" =>
+        val logicalRelation = getPlanField[LogicalRelation]("logicalRelation")
+        logicalRelation.catalogTable.foreach { t =>
+          val overwrite = getPlanField[Boolean]("overwrite")
+          val actionType = if (overwrite) INSERT_OVERWRITE else INSERT
+          outputObjs += tablePrivileges(t.identifier, actionType = actionType)
+        }
+        buildQuery(getQuery, inputObjs)
+
+      case "InsertIntoDataSourceDirCommand" |
+          "SaveIntoDataSourceCommand" |
+          "InsertIntoHadoopFsRelationCommand" |
+          "InsertIntoHiveDirCommand" =>
+        // TODO: Should get the table via datasource options?
+        buildQuery(getQuery, inputObjs)
+
+      case "LoadDataCommand" =>
+        val table = getPlanField[TableIdentifier]("table")
+        val overwrite = getPlanField[Boolean]("isOverwrite")
+        val actionType = if (overwrite) INSERT_OVERWRITE else INSERT
+        val cols = getPlanField[Option[TablePartitionSpec]]("partition")
+          .map(_.keySet).getOrElse(Nil)
+        outputObjs += tablePrivileges(table, cols.toSeq, actionType = actionType)
+
+      case "MergeIntoTable" =>
+
+      case "RepairTableCommand" =>
+        val enableAddPartitions = getPlanField[Boolean]("enableAddPartitions")
+        if (enableAddPartitions) {
+          outputObjs += tablePrivileges(getTableName, actionType = INSERT)
+        } else if (getPlanField[Boolean]("enableDropPartitions")) {
+          outputObjs += tablePrivileges(getTableName, actionType = DELETE)
+        } else {
+          inputObjs += tablePrivileges(getTableName)
+        }
+
+      case "SetCatalogAndNamespace" =>
+        getPlanField[Option[String]]("catalogName").foreach { catalog =>
+          // fixme do we really need to skip spark_catalog?
+          if (catalog != "spark_catalog") {
+            inputObjs += databasePrivileges(catalog)
+          }
+        }
+        getPlanField[Option[Seq[String]]]("namespace").foreach { nameParts =>
+          inputObjs += databasePrivileges(quote(nameParts))
+        }
+
+      case "SetCatalogCommand" =>
+        inputObjs += databasePrivileges(getPlanField[String]("catalogName"))
+
+      case "SetNamespaceCommand" =>
+        val namespace = quote(getPlanField[Seq[String]]("namespace"))
+        inputObjs += databasePrivileges(namespace)
+
+      case "TruncateTableCommand" =>
+        val table = getTableName
+        val cols = getPlanField[Option[TablePartitionSpec]]("partitionSpec")
+          .map(_.keySet).getOrElse(Nil)
+        outputObjs += tablePrivileges(table, cols.toSeq)
+
+      case "ShowColumnsCommand" =>
+        inputObjs += tablePrivileges(getTableName)
+
+      case "ShowCreateTableCommand" |
+          "ShowCreateTableAsSerdeCommand" |
+          "ShowTablePropertiesCommand" =>
+        val table = getPlanField[TableIdentifier]("table")
+        inputObjs += tablePrivileges(table)
+
+      case "ShowFunctionsCommand" =>
+        getPlanField[Option[String]]("db").foreach { db =>
+          inputObjs += databasePrivileges(db)
+        }
+
+      case "ShowPartitionsCommand" =>
+        val cols = getPlanField[Option[TablePartitionSpec]]("spec")
+          .map(_.keySet.toSeq).getOrElse(Nil)
+        inputObjs += tablePrivileges(getTableName, cols)
+
+      case _ =>
+      // AddArchivesCommand
+      // AddFileCommand
+      // AddJarCommand
+      // ClearCacheCommand
+      // DescribeQueryCommand
+      // ExplainCommand
+      // ListArchivesCommand
+      // ListFilesCommand
+      // ListJarsCommand
+      // RefreshResource
+      // ResetCommand
+      // SetCommand
+      // ShowDatabasesCommand
+      // StreamingExplainCommand
+      // UncacheTableCommand
+    }
+  }
+
+  /**
+   * Build input and output privilege objects from a Spark's LogicalPlan
+   *
+   * For `Command`s, build outputs if it has an target to write, build inputs for the
+   * inside query if exists.
+   *
+   * For other queries, build inputs.
+   *
+   * @param plan A Spark LogicalPlan
+   */
+  def build(plan: LogicalPlan): (Seq[PrivilegeObject], Seq[PrivilegeObject]) = {
+    val inputObjs = new ArrayBuffer[PrivilegeObject]
+    val outputObjs = new ArrayBuffer[PrivilegeObject]
+    plan match {
+      // RunnableCommand
+      case cmd: Command => buildCommand(cmd, inputObjs, outputObjs)
+      // Queries
+      case _ => buildQuery(plan, inputObjs)
+    }
+    (inputObjs, outputObjs)
+  }
+}
---
+++ extensions/spark/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/ranger/AccessRequest.scala
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.plugin.spark.authz.ranger
+
+import java.util.Date
+
+import scala.collection.JavaConverters._
+
+import org.apache.hadoop.security.UserGroupInformation
+import org.apache.ranger.plugin.policyengine.{RangerAccessRequestImpl, RangerPolicyEngine}
+
+import org.apache.kyuubi.plugin.spark.authz.ranger.AccessType._
+
+case class AccessRequest(accessType: AccessType) extends RangerAccessRequestImpl
+
+object AccessRequest {
+  def apply(
+      resource: AccessResource,
+      user: UserGroupInformation,
+      opType: String,
+      accessType: AccessType): AccessRequest = {
+    val userName = user.getShortUserName
+    val groups = user.getGroupNames.toSet.asJava
+    val req = new AccessRequest(accessType)
+    req.setResource(resource)
+    req.setUser(userName)
+    req.setUserGroups(groups)
+    try {
+      val getRoles = RangerSparkPlugin.getClass.getMethod(
+        "getRolesFromUserAndGroups",
+        classOf[String],
+        classOf[java.util.Set[String]])
+      getRoles.setAccessible(true)
+      val roles = getRoles.invoke(RangerSparkPlugin, userName, groups)
+      val setRoles = req.getClass.getMethod("setUserRoles", classOf[java.util.Set[String]])
+      setRoles.setAccessible(true)
+      setRoles.invoke(req, roles)
+    } catch {
+      case _: NoSuchMethodException =>
+    }
+    req.setAccessTime(new Date())
+    accessType match {
+      case USE => req.setAccessType(RangerPolicyEngine.ANY_ACCESS)
+      case _ => req.setAccessType(accessType.toString.toLowerCase)
+    }
+    try {
+      val getClusterName = RangerSparkPlugin.getClass.getMethod("getClusterName")
+      getClusterName.setAccessible(true)
+      val clusterName = getClusterName.invoke(RangerSparkPlugin)
+      val setClusterName = req.getClass.getMethod("setClusterName", classOf[String])
+      setClusterName.setAccessible(true)
+      setClusterName.invoke(req, clusterName)
+    } catch {
+      case _: NoSuchMethodException =>
+    }
+    req
+  }
+}
