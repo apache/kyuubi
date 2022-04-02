cca77ccb5803faa351526739e1ba158f8e660e6a
Cheng Pan <chengpan@apache.org>
2022-04-02 18:44:07 +0800

    fixup

--- dev/kyuubi-extension-spark-common/src/main/scala/org/apache/kyuubi/sql/KyuubiSQLConf.scala
+++ dev/kyuubi-extension-spark-common/src/main/scala/org/apache/kyuubi/sql/KyuubiSQLConf.scala
@@ -23,14 +23,14 @@ import org.apache.spark.sql.internal.SQLConf._
 object KyuubiSQLConf {
 
   val INSERT_REPARTITION_BEFORE_WRITE =
-    buildConf("kyuubi.spark.sql.optimizer.insertRepartitionBeforeWrite.enabled")
+    buildConf("spark.sql.optimizer.insertRepartitionBeforeWrite.enabled")
       .doc("Add repartition node at the top of query plan. An approach of merging small files.")
       .version("1.2.0")
       .booleanConf
       .createWithDefault(true)
 
   val INSERT_REPARTITION_NUM =
-    buildConf("kyuubi.spark.sql.optimizer.insertRepartitionNum")
+    buildConf("spark.sql.optimizer.insertRepartitionNum")
       .doc(s"The partition number if ${INSERT_REPARTITION_BEFORE_WRITE.key} is enabled. " +
         s"If AQE is disabled, the default value is ${SQLConf.SHUFFLE_PARTITIONS.key}. " +
         "If AQE is enabled, the default value is none that means depend on AQE.")
@@ -39,7 +39,7 @@ object KyuubiSQLConf {
       .createOptional
 
   val DYNAMIC_PARTITION_INSERTION_REPARTITION_NUM =
-    buildConf("kyuubi.spark.sql.optimizer.dynamicPartitionInsertionRepartitionNum")
+    buildConf("spark.sql.optimizer.dynamicPartitionInsertionRepartitionNum")
       .doc(s"The partition number of each dynamic partition if " +
         s"${INSERT_REPARTITION_BEFORE_WRITE.key} is enabled. " +
         "We will repartition by dynamic partition columns to reduce the small file but that " +
@@ -50,7 +50,7 @@ object KyuubiSQLConf {
       .createWithDefault(100)
 
   val FORCE_SHUFFLE_BEFORE_JOIN =
-    buildConf("kyuubi.spark.sql.optimizer.forceShuffleBeforeJoin.enabled")
+    buildConf("spark.sql.optimizer.forceShuffleBeforeJoin.enabled")
       .doc("Ensure shuffle node exists before shuffled join (shj and smj) to make AQE " +
         "`OptimizeSkewedJoin` works (complex scenario join, multi table join).")
       .version("1.2.0")
@@ -58,7 +58,7 @@ object KyuubiSQLConf {
       .createWithDefault(false)
 
   val FINAL_STAGE_CONFIG_ISOLATION =
-    buildConf("kyuubi.spark.sql.optimizer.finalStageConfigIsolation.enabled")
+    buildConf("spark.sql.optimizer.finalStageConfigIsolation.enabled")
       .doc("If true, the final stage support use different config with previous stage. " +
         "The prefix of final stage config key should be `spark.sql.finalStage.`." +
         "For example, the raw spark config: `spark.sql.adaptive.advisoryPartitionSizeInBytes`, " +
@@ -70,7 +70,7 @@ object KyuubiSQLConf {
 
   val SQL_CLASSIFICATION = "spark.sql.analyzer.classification"
   val SQL_CLASSIFICATION_ENABLED =
-    buildConf("kyuubi.spark.sql.analyzer.classification.enabled")
+    buildConf("spark.sql.analyzer.classification.enabled")
       .doc("When true, allows Kyuubi engine to judge this SQL's classification " +
         s"and set `$SQL_CLASSIFICATION` back into sessionConf. " +
         "Through this configuration item, Spark can optimizing configuration dynamic")
@@ -79,7 +79,7 @@ object KyuubiSQLConf {
       .createWithDefault(false)
 
   val INSERT_ZORDER_BEFORE_WRITING =
-    buildConf("kyuubi.spark.sql.optimizer.insertZorderBeforeWriting.enabled")
+    buildConf("spark.sql.optimizer.insertZorderBeforeWriting.enabled")
       .doc("When true, we will follow target table properties to insert zorder or not. " +
         "The key properties are: 1) kyuubi.zorder.enabled; if this property is true, we will " +
         "insert zorder before writing data. 2) kyuubi.zorder.cols; string split by comma, we " +
@@ -89,7 +89,7 @@ object KyuubiSQLConf {
       .createWithDefault(true)
 
   val ZORDER_GLOBAL_SORT_ENABLED =
-    buildConf("kyuubi.spark.sql.optimizer.zorderGlobalSort.enabled")
+    buildConf("spark.sql.optimizer.zorderGlobalSort.enabled")
       .doc("When true, we do a global sort using zorder. Note that, it can cause data skew " +
         "issue if the zorder columns have less cardinality. When false, we only do local sort " +
         "using zorder.")
@@ -98,7 +98,7 @@ object KyuubiSQLConf {
       .createWithDefault(true)
 
   val WATCHDOG_MAX_PARTITIONS =
-    buildConf("kyuubi.spark.sql.watchdog.maxPartitions")
+    buildConf("spark.sql.watchdog.maxPartitions")
       .doc("Set the max partition number when spark scans a data source. " +
         "Enable MaxPartitionStrategy by specifying this configuration. " +
         "Add maxPartitions Strategy to avoid scan excessive partitions " +
@@ -108,7 +108,7 @@ object KyuubiSQLConf {
       .createOptional
 
   val WATCHDOG_FORCED_MAXOUTPUTROWS =
-    buildConf("kyuubi.spark.sql.watchdog.forcedMaxOutputRows")
+    buildConf("spark.sql.watchdog.forcedMaxOutputRows")
       .doc("Add ForcedMaxOutputRows rule to avoid huge output rows of non-limit query " +
         "unexpectedly, it's optional that works with defined")
       .version("1.4.0")
@@ -116,7 +116,7 @@ object KyuubiSQLConf {
       .createOptional
 
   val DROP_IGNORE_NONEXISTENT =
-    buildConf("kyuubi.spark.sql.optimizer.dropIgnoreNonExistent")
+    buildConf("spark.sql.optimizer.dropIgnoreNonExistent")
       .doc("Do not report an error if DROP DATABASE/TABLE/VIEW/FUNCTION/PARTITION specifies " +
         "a non-existent database/table/view/function/partition")
       .version("1.5.0")
