0fad941929dbd0c78cc33aad90edb3d7004cf6b4
Kent Yao <yao@apache.org>
2022-04-24 15:11:09 +0800

    address comment

--- kyuubi-server/src/main/scala/org/apache/kyuubi/operation/BatchJobSubmission.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/operation/BatchJobSubmission.scala
@@ -107,7 +107,7 @@ class BatchJobSubmission(session: KyuubiBatchSessionImpl, batchRequest: BatchReq
         Thread.sleep(applicationCheckInterval)
       }
       val state = applicationStatus.get(ApplicationOperation.APP_STATE_KEY)
-      if (state == "KILLED" || state == "FINISHED") {
+      if (state == "KILLED" || state == "FINISHED" || state == "FAILED") {
         process.destroyForcibly()
         throw new RuntimeException("Batch job failed:" + applicationStatus.get.mkString(","))
       } else {
