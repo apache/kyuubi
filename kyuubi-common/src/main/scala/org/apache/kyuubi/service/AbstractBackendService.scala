afaf240c1d1a7f69daed671d49a9741a59c71d90
wforget <643348094@qq.com>
2022-04-28 23:29:01 +0800

    [KYUUBI-2490] Fix NPE in getOperationStatus

--- kyuubi-common/src/main/scala/org/apache/kyuubi/service/AbstractBackendService.scala
+++ kyuubi-common/src/main/scala/org/apache/kyuubi/service/AbstractBackendService.scala
@@ -160,7 +160,18 @@ abstract class AbstractBackendService(name: String)
     val operation = sessionManager.operationManager.getOperation(operationHandle)
     if (operation.shouldRunAsync) {
       try {
-        operation.getBackgroundHandle.get(timeout, TimeUnit.MILLISECONDS)
+        val startTime = System.currentTimeMillis()
+        var duration: Long = 0
+        var break = false
+        while (!break && duration < timeout) {
+          if (operation.getBackgroundHandle == null) {
+            Thread.sleep(100)
+          } else {
+            operation.getBackgroundHandle.get(timeout - duration, TimeUnit.MILLISECONDS)
+            break = true
+          }
+          duration = System.currentTimeMillis() - startTime
+        }
       } catch {
         case e: TimeoutException =>
           debug(s"$operationHandle: Long polling timed out, ${e.getMessage}")
