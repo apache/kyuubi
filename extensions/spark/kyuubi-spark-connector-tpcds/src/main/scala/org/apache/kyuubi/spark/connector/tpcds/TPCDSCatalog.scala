d009b898496a28f8e9fd43b34fe35e85a647bc12
Cheng Pan <chengpan@apache.org>
2022-05-01 15:59:59 +0800

    comments

--- extensions/spark/kyuubi-spark-connector-tpcds/src/main/scala/org/apache/kyuubi/spark/connector/tpcds/TPCDSCatalog.scala
+++ extensions/spark/kyuubi-spark-connector-tpcds/src/main/scala/org/apache/kyuubi/spark/connector/tpcds/TPCDSCatalog.scala
@@ -28,6 +28,7 @@ import org.apache.spark.sql.connector.expressions.Transform
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.util.CaseInsensitiveStringMap
 
+// TODO: implement SupportsNamespaces
 class TPCDSCatalog extends TableCatalog {
 
   val tables: Array[String] = Table.getBaseTables.asScala
