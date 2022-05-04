d2338f85afead0899829430124cc6d2318a55ab8
Fei Wang <fwang12@ebay.com>
2022-05-05 03:09:27 +0800

    reformat

--- kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/SwaggerScalaModelConverter.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/SwaggerScalaModelConverter.scala
@@ -21,10 +21,14 @@ package org.apache.kyuubi.server.api
 import java.lang.annotation.Annotation
 import java.lang.reflect.ParameterizedType
 import java.util.Iterator
-import com.fasterxml.jackson.databind.JavaType
+
+import scala.util.Try
+import scala.util.control.NonFatal
+
 import com.fasterxml.jackson.databind.`type`.ReferenceType
-import com.fasterxml.jackson.module.scala.introspect.{BeanIntrospector, PropertyDescriptor}
+import com.fasterxml.jackson.databind.JavaType
 import com.fasterxml.jackson.module.scala.{DefaultScalaModule, JsonScalaEnumeration}
+import com.fasterxml.jackson.module.scala.introspect.{BeanIntrospector, PropertyDescriptor}
 import io.swagger.v3.core.converter._
 import io.swagger.v3.core.jackson.ModelResolver
 import io.swagger.v3.core.util.{Json, PrimitiveType}
@@ -32,9 +36,6 @@ import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.{ArraySchema, Schema => SchemaAnnotation}
 import io.swagger.v3.oas.models.media.Schema
 import org.slf4j.LoggerFactory
-
-import scala.util.Try
-import scala.util.control.NonFatal
 
 class AnnotatedTypeForOption extends AnnotatedType
 
@@ -55,7 +56,10 @@ class SwaggerScalaModelConverter extends ModelResolver(SwaggerScalaModelConverte
   private val ProductClass = classOf[Product]
   private val AnyClass = classOf[Any]
 
-  override def resolve(`type`: AnnotatedType, context: ModelConverterContext, chain: Iterator[ModelConverter]): Schema[_] = {
+  override def resolve(
+      `type`: AnnotatedType,
+      context: ModelConverterContext,
+      chain: Iterator[ModelConverter]): Schema[_] = {
     val javaType = _mapper.constructType(`type`.getType)
     val cls = javaType.getRawClass
 
@@ -63,7 +67,9 @@ class SwaggerScalaModelConverter extends ModelResolver(SwaggerScalaModelConverte
       // Unbox scala options
       val annotatedOverrides = getRequiredSettings(`type`)
       if (_isOptional(`type`, cls)) {
-        val baseType = if (annotatedOverrides.headOption.getOrElse(false)) new AnnotatedType() else new AnnotatedTypeForOption()
+        val baseType =
+          if (annotatedOverrides.headOption.getOrElse(false)) new AnnotatedType()
+          else new AnnotatedTypeForOption()
         resolve(nextType(baseType, `type`, javaType), context, chain)
       } else if (!annotatedOverrides.headOption.getOrElse(true)) {
         resolve(nextType(new AnnotatedTypeForOption(), `type`, javaType), context, chain)
@@ -88,8 +94,11 @@ class SwaggerScalaModelConverter extends ModelResolver(SwaggerScalaModelConverte
     }
   }
 
-  private def caseClassSchema(cls: Class[_], `type`: AnnotatedType, context: ModelConverterContext,
-                              chain: Iterator[ModelConverter]): Option[Schema[_]] = {
+  private def caseClassSchema(
+      cls: Class[_],
+      `type`: AnnotatedType,
+      context: ModelConverterContext,
+      chain: Iterator[ModelConverter]): Option[Schema[_]] = {
     if (chain.hasNext) {
       Option(chain.next().resolve(`type`, context, chain)).map { schema =>
         val introspector = BeanIntrospector(cls)
@@ -98,7 +107,8 @@ class SwaggerScalaModelConverter extends ModelResolver(SwaggerScalaModelConverte
             case Seq() => {
               val propertyClass = getPropertyClass(property)
               val optionalFlag = isOption(propertyClass)
-              if (optionalFlag && schema.getRequired != null && schema.getRequired.contains(property.name)) {
+              if (optionalFlag && schema.getRequired != null && schema.getRequired.contains(
+                  property.name)) {
                 schema.getRequired.remove(property.name)
               } else if (!optionalFlag) {
                 addRequiredItem(schema, property.name)
@@ -118,10 +128,11 @@ class SwaggerScalaModelConverter extends ModelResolver(SwaggerScalaModelConverte
     }
   }
 
-  private def getRequiredSettings(annotatedType: AnnotatedType): Seq[Boolean] = annotatedType match {
-    case _: AnnotatedTypeForOption => Seq.empty
-    case _ => getRequiredSettings(nullSafeList(annotatedType.getCtxAnnotations))
-  }
+  private def getRequiredSettings(annotatedType: AnnotatedType): Seq[Boolean] =
+    annotatedType match {
+      case _: AnnotatedTypeForOption => Seq.empty
+      case _ => getRequiredSettings(nullSafeList(annotatedType.getCtxAnnotations))
+    }
 
   private def getRequiredSettings(annotations: Seq[Annotation]): Seq[Boolean] = {
     annotations.collect {
@@ -131,7 +142,9 @@ class SwaggerScalaModelConverter extends ModelResolver(SwaggerScalaModelConverte
     }
   }
 
-  private def matchScalaPrimitives(`type`: AnnotatedType, nullableClass: Class[_]): Option[Schema[_]] = {
+  private def matchScalaPrimitives(
+      `type`: AnnotatedType,
+      nullableClass: Class[_]): Option[Schema[_]] = {
     val annotations = Option(`type`.getCtxAnnotations).map(_.toSeq).getOrElse(Seq.empty)
     annotations.collectFirst { case ann: SchemaAnnotation => ann } match {
       case Some(_) => None
@@ -141,13 +154,14 @@ class SwaggerScalaModelConverter extends ModelResolver(SwaggerScalaModelConverte
             val pt = enumAnnotation.value().getGenericSuperclass.asInstanceOf[ParameterizedType]
             val args = pt.getActualTypeArguments
             val cls = args(0).asInstanceOf[Class[_]]
-            val sp: Schema[String] = PrimitiveType.STRING.createProperty().asInstanceOf[Schema[String]]
+            val sp: Schema[String] =
+              PrimitiveType.STRING.createProperty().asInstanceOf[Schema[String]]
             setRequired(`type`)
             try {
               val mainClass = getMainClass(cls)
               val valueMethods = mainClass.getMethods.toSeq.filter { m =>
                 m.getDeclaringClass != EnumClass &&
-                  m.getReturnType.getName == "scala.Enumeration$Value" && m.getParameterCount == 0
+                m.getReturnType.getName == "scala.Enumeration$Value" && m.getParameterCount == 0
               }
               val enumValues = valueMethods.map(_.invoke(None.orNull))
               enumValues.foreach { v =>
@@ -201,7 +215,10 @@ class SwaggerScalaModelConverter extends ModelResolver(SwaggerScalaModelConverte
     }
   }
 
-  private def nextType(baseType: AnnotatedType, `type`: AnnotatedType, javaType: JavaType): AnnotatedType = {
+  private def nextType(
+      baseType: AnnotatedType,
+      `type`: AnnotatedType,
+      javaType: JavaType): AnnotatedType = {
     baseType.`type`(underlyingJavaType(`type`, javaType))
       .ctxAnnotations(`type`.getCtxAnnotations)
       .parent(`type`.getParent)
@@ -249,19 +266,19 @@ class SwaggerScalaModelConverter extends ModelResolver(SwaggerScalaModelConverte
         }
       }
       case _ => property.field match {
-        case Some(field) => field.getType
-        case _ => property.setter match {
-          case Some(setter) if setter.getParameterCount == 1 => {
-            setter.getParameterTypes()(0)
-          }
-          case _ => property.beanSetter match {
-            case Some(setter) if setter.getParameterCount == 1 => {
-              setter.getParameterTypes()(0)
+          case Some(field) => field.getType
+          case _ => property.setter match {
+              case Some(setter) if setter.getParameterCount == 1 => {
+                setter.getParameterTypes()(0)
+              }
+              case _ => property.beanSetter match {
+                  case Some(setter) if setter.getParameterCount == 1 => {
+                    setter.getParameterTypes()(0)
+                  }
+                  case _ => AnyClass
+                }
             }
-            case _ => AnyClass
-          }
         }
-      }
     }
   }
 
@@ -276,19 +293,19 @@ class SwaggerScalaModelConverter extends ModelResolver(SwaggerScalaModelConverte
         }
       }
       case _ => property.field match {
-        case Some(field) => field.getAnnotations.toSeq
-        case _ => property.setter match {
-          case Some(setter) if setter.getParameterCount == 1 => {
-            setter.getAnnotations().toSeq
-          }
-          case _ => property.beanSetter match {
-            case Some(setter) if setter.getParameterCount == 1 => {
-              setter.getAnnotations().toSeq
+          case Some(field) => field.getAnnotations.toSeq
+          case _ => property.setter match {
+              case Some(setter) if setter.getParameterCount == 1 => {
+                setter.getAnnotations().toSeq
+              }
+              case _ => property.beanSetter match {
+                  case Some(setter) if setter.getParameterCount == 1 => {
+                    setter.getAnnotations().toSeq
+                  }
+                  case _ => Seq.empty
+                }
             }
-            case _ => Seq.empty
-          }
         }
-      }
     }
   }
 
