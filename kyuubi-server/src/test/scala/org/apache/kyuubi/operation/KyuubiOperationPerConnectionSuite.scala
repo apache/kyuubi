d4457906547907d41813fb8138dd15afc22abc60
sychen <sychen@ctrip.com>
2022-05-20 11:58:07 +0800

    3s

--- kyuubi-server/src/test/scala/org/apache/kyuubi/operation/KyuubiOperationPerConnectionSuite.scala
+++ kyuubi-server/src/test/scala/org/apache/kyuubi/operation/KyuubiOperationPerConnectionSuite.scala
@@ -232,7 +232,7 @@ class KyuubiOperationPerConnectionSuite extends WithKyuubiServer with HiveJDBCTe
     withSessionConf(Map(
       KyuubiConf.ENGINE_ALIVE_PROBE_ENABLED.key -> "true",
       KyuubiConf.ENGINE_ALIVE_PROBE_INTERVAL.key -> "1000",
-      KyuubiConf.ENGINE_ALIVE_TIMEOUT.key -> "10000",
+      KyuubiConf.ENGINE_ALIVE_TIMEOUT.key -> "3000",
       KyuubiConf.OPERATION_THRIFT_CLIENT_REQUEST_MAX_ATTEMPTS.key -> "10000",
       KyuubiConf.ENGINE_REQUEST_TIMEOUT.key -> "1000"))(Map.empty)(Map.empty) {
       withSessionHandle { (client, handle) =>
@@ -250,7 +250,7 @@ class KyuubiOperationPerConnectionSuite extends WithKyuubiServer with HiveJDBCTe
         val executeStmtResp = client.ExecuteStatement(executeStmtReq)
         assert(executeStmtResp.getStatus.getStatusCode === TStatusCode.ERROR_STATUS)
         val elapsedTime = System.currentTimeMillis() - startTime
-        assert(elapsedTime > 10 * 1000 && elapsedTime < 20 * 1000)
+        assert(elapsedTime > 3 * 1000 && elapsedTime < 20 * 1000)
       }
     }
   }
