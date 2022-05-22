8fc880311dff78b7fce6dd4ed4c3fe108a64de10
Fei Wang <fwang12@ebay.com>
2022-05-23 00:53:28 +0800

    schema refactor

--- kyuubi-server/src/main/resources/sql/mysql/statestore-shema.mysql.sql
+++ kyuubi-server/src/main/resources/sql/mysql/statestore-shema.mysql.sql
@@ -17,7 +17,7 @@
 
 CREATE TABLE IF NOT EXISTS BATCH_STATE(
     KEY_ID bigint PRIMARY KEY AUTO_INCREMENT,
-    ID varchar(128) NOT NULL,
+    ID varchar(36) NOT NULL,
     BATCH_TYPE varchar(1024) NOT NULL,
     BATCH_OWNER varchar(1024) NOT NULL,
     KYUUBI_INSTANCE varchar(1024) NOT NULL,
@@ -27,13 +27,13 @@ CREATE TABLE IF NOT EXISTS BATCH_STATE(
     APP_NAME varchar(1024),
     APP_URL varchar(1024),
     APP_STATE varchar(128),
-    APP_ERROR mediumtext,
+    APP_ERROR text,
     END_TIME bigint
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE IF NOT EXISTS BATCH_META(
-    BATCH_ID varchar(128) NOT NULL,
+    BATCH_ID varchar(36) NOT NULL,
     SESSION_CONF mediumtext,
     BATCH_TYPE varchar(1024),
     RESOURCE varchar(1024),
