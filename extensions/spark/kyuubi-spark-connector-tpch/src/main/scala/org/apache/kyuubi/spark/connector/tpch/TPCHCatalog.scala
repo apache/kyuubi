beb72754318f3b1f8ddb2c3090ae5be6dd9c0bf7
jiaoqingbo <1178404354@qq.com>
2022-05-24 16:51:10 +0800

    [KYUUBI #2663] Kyuubi Spark TPC-H Connector - Initial implementation

---
+++ extensions/spark/kyuubi-spark-connector-tpch/src/main/scala/org/apache/kyuubi/spark/connector/tpch/TPCHCatalog.scala
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.spark.connector.tpch
+
+import java.util
+
+import scala.collection.JavaConverters._
+
+import io.trino.tpch.TpchTable
+import org.apache.spark.sql.catalyst.analysis.{NoSuchNamespaceException, NoSuchTableException}
+import org.apache.spark.sql.connector.catalog.{Identifier, Table => SparkTable, TableCatalog, TableChange}
+import org.apache.spark.sql.connector.expressions.Transform
+import org.apache.spark.sql.types.StructType
+import org.apache.spark.sql.util.CaseInsensitiveStringMap
+
+class TPCHCatalog extends TableCatalog {
+
+  val tables: Array[String] = TpchTable.getTables.asScala
+    .map(_.getTableName).toArray
+
+  val scales: Array[Int] = TPCHStatisticsUtils.SCALES
+
+  val databases: Array[String] = scales.map("sf" + _)
+
+  var options: CaseInsensitiveStringMap = _
+
+  override def name: String = "tpch"
+
+  override def initialize(name: String, options: CaseInsensitiveStringMap): Unit = {
+    this.options = options
+  }
+
+  override def listTables(namespace: Array[String]): Array[Identifier] = namespace match {
+    case Array(db) if databases contains db => tables.map(Identifier.of(namespace, _))
+    case _ => throw new NoSuchNamespaceException(namespace.mkString("."))
+  }
+
+  override def loadTable(ident: Identifier): SparkTable = (ident.namespace, ident.name) match {
+    case (Array(db), table) if (databases contains db) && tables.contains(table.toLowerCase) =>
+      new TPCHTable(table.toLowerCase, scales(databases indexOf db), options)
+    case (_, _) => throw new NoSuchTableException(ident)
+  }
+
+  override def createTable(
+      ident: Identifier,
+      schema: StructType,
+      partitions: Array[Transform],
+      properties: util.Map[String, String]): SparkTable =
+    throw new UnsupportedOperationException
+
+  override def alterTable(ident: Identifier, changes: TableChange*): SparkTable =
+    throw new UnsupportedOperationException
+
+  override def dropTable(ident: Identifier): Boolean =
+    throw new UnsupportedOperationException
+
+  override def renameTable(oldIdent: Identifier, newIdent: Identifier): Unit =
+    throw new UnsupportedOperationException
+
+}
---
+++ extensions/spark/kyuubi-spark-connector-tpch/src/main/scala/org/apache/kyuubi/spark/connector/tpch/TPCHTable.scala
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.spark.connector.tpch
+
+import java.util
+import java.util.Optional
+
+import scala.collection.JavaConverters._
+import scala.collection.convert.ImplicitConversions.`list asScalaBuffer`
+
+import io.trino.tpch.{TpchColumnType, TpchEntity, TpchTable}
+import io.trino.tpch.TpchColumnType.Base._
+import org.apache.spark.sql.connector.catalog.{SupportsRead, Table => SparkTable, TableCapability}
+import org.apache.spark.sql.connector.read.ScanBuilder
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.util.CaseInsensitiveStringMap
+
+class TPCHTable(tbl: String, scale: Int, options: CaseInsensitiveStringMap)
+  extends SparkTable with SupportsRead {
+
+  // When true, use CHAR VARCHAR; otherwise use STRING
+  val useAnsiStringType: Boolean = options.getBoolean("useAnsiStringType", false)
+
+  val tpchTable: TpchTable[_] = TpchTable.getTable(tbl)
+
+  override def name: String = s"`sf$scale`.`$tbl`"
+
+  override def toString: String = s"TPCHTable($name)"
+
+  override def schema: StructType = {
+    StructType(
+      tpchTable.asInstanceOf[TpchTable[TpchEntity]].getColumns.zipWithIndex.map { case (c, _) =>
+        StructField(c.getColumnName, toSparkDataType(c.getType))
+      })
+  }
+
+  override def capabilities(): util.Set[TableCapability] =
+    Set(TableCapability.BATCH_READ).asJava
+
+  override def newScanBuilder(options: CaseInsensitiveStringMap): ScanBuilder = {
+    new TPCHBatchScan(tpchTable, scale, schema)
+  }
+
+  def toSparkDataType(tpchType: TpchColumnType): DataType = {
+    (tpchType.getBase, tpchType.getPrecision.asScala, tpchType.getScale.asScala) match {
+      case (INTEGER, None, None) => IntegerType
+      case (IDENTIFIER, None, None) => LongType
+      case (DOUBLE, None, None) => DoubleType
+      case (DATE, None, None) => DateType
+      case (VARCHAR, Some(precision), None) =>
+        if (useAnsiStringType) VarcharType(precision.toInt) else StringType
+      case (t, po, so) =>
+        throw new IllegalArgumentException(s"Unsupported TPC-H type: ($t, $po, $so)")
+    }
+  }
+
+  implicit final class RichOptional[T](val optional: Optional[T]) {
+    def asScala: Option[T] = optional match {
+      case null => null
+      case _ => if (optional.isPresent) Option(optional.get) else None
+    }
+  }
+}
