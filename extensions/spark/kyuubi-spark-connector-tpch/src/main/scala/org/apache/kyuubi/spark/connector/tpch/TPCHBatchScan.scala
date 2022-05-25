81c48b0c61a1f553b3370344a164f26e6aa91698
jiaoqingbo <1178404354@qq.com>
2022-05-25 11:29:03 +0800

    [KYUUBI #2663] Kyuubi Spark TPC-H Connector - Initial implementation
    ### _Why are the changes needed?_
    
    fix #2663
    
    ### _How was this patch tested?_
    - [x] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [x] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #2731 from jiaoqingbo/kyuubi-2663.
    
    Closes #2663
    
    1553c59d [jiaoqingbo] code review
    beb72754 [jiaoqingbo] [KYUUBI #2663] Kyuubi Spark TPC-H Connector - Initial implementation
    
    Authored-by: jiaoqingbo <1178404354@qq.com>
    Signed-off-by: Cheng Pan <chengpan@apache.org>

---
+++ extensions/spark/kyuubi-spark-connector-tpch/src/main/scala/org/apache/kyuubi/spark/connector/tpch/TPCHBatchScan.scala
@@ -0,0 +1,135 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.spark.connector.tpch
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+import scala.collection.mutable.ArrayBuffer
+
+import io.trino.tpch._
+import io.trino.tpch.GenerateUtils.formatDate
+import io.trino.tpch.TpchColumnType.Base._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.connector.read._
+import org.apache.spark.sql.types._
+import org.apache.spark.unsafe.types.UTF8String
+
+case class TPCHTableChuck(table: String, scale: Int, parallelism: Int, index: Int)
+  extends InputPartition
+
+class TPCHBatchScan(
+    @transient table: TpchTable[_],
+    scale: Int,
+    schema: StructType) extends ScanBuilder
+  with Scan with Batch with Serializable {
+
+  private val _numRows: Long = TPCHStatisticsUtils.numRows(table, scale)
+
+  private val rowCountPerTask: Int = 1000000
+
+  private val parallelism: Int =
+    if (table.equals(TpchTable.NATION) || table.equals(TpchTable.REGION)) 1
+    else math.max(
+      SparkSession.active.sparkContext.defaultParallelism,
+      (_numRows / rowCountPerTask.toDouble).ceil.toInt)
+
+  override def build: Scan = this
+
+  override def toBatch: Batch = this
+
+  override def description: String =
+    s"Scan TPC-H sf$scale.${table.getTableName}, count: ${_numRows}, parallelism: $parallelism"
+
+  override def readSchema: StructType = schema
+
+  override def planInputPartitions: Array[InputPartition] =
+    (1 to parallelism).map { i =>
+      TPCHTableChuck(table.getTableName, scale, parallelism, i)
+    }.toArray
+
+  def createReaderFactory: PartitionReaderFactory = (partition: InputPartition) => {
+    val chuck = partition.asInstanceOf[TPCHTableChuck]
+    new TPCHPartitionReader(chuck.table, chuck.scale, chuck.parallelism, chuck.index, schema)
+  }
+
+}
+
+class TPCHPartitionReader(
+    table: String,
+    scale: Int,
+    parallelism: Int,
+    index: Int,
+    schema: StructType) extends PartitionReader[InternalRow] {
+
+  private val tpchTable = TpchTable.getTable(table)
+
+  private val columns = tpchTable.getColumns
+    .asInstanceOf[java.util.List[TpchColumn[TpchEntity]]]
+
+  private lazy val dateFmt: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+  private val iterator = tpchTable.createGenerator(scale, index, parallelism).iterator
+
+  private var currentRow: InternalRow = _
+
+  override def next(): Boolean = {
+    val hasNext = iterator.hasNext
+    if (hasNext) currentRow = {
+      val row = iterator.next().asInstanceOf[TpchEntity]
+      val rowValue = new ArrayBuffer[String]()
+      columns.stream().forEach(column => {
+        val baseType = column.getType.getBase
+        var value: String = ""
+        baseType match {
+          case IDENTIFIER => value += column.getIdentifier(row)
+          case INTEGER => value += column.getInteger(row)
+          case DATE => value += column.getDate(row)
+          case DOUBLE => value += column.getDouble(row)
+          case VARCHAR => value += column.getString(row)
+        }
+        rowValue += value
+      })
+      val rowAny = new ArrayBuffer[Any]()
+      rowValue.zipWithIndex.map { case (value, i) =>
+        (value, schema(i).dataType) match {
+          case (null, _) => null
+          case ("", _) => null
+          case (value, IntegerType) => rowAny += value.toInt
+          case (value, LongType) => rowAny += value.toLong
+          case (value, DoubleType) => rowAny += value.toDouble
+          case (value, DateType) => rowAny += LocalDate.parse(formatDate(value.toInt), dateFmt)
+              .toEpochDay.toInt
+          case (value, StringType) => rowAny += UTF8String.fromString(value)
+          case (value, CharType(_)) => rowAny += UTF8String.fromString(value)
+          case (value, VarcharType(_)) => rowAny += UTF8String.fromString(value)
+          case (value, DecimalType()) => rowAny += Decimal(value)
+          case (value, dt) => throw new IllegalArgumentException(s"value: $value, type: $dt")
+        }
+      }
+      InternalRow.fromSeq(rowAny)
+    }
+    hasNext
+  }
+
+  override def get(): InternalRow = currentRow
+
+  override def close(): Unit = {}
+
+}
---
+++ extensions/spark/kyuubi-spark-connector-tpch/src/test/scala/org/apache/kyuubi/spark/connector/tpch/TPCHCatalogSuite.scala
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.spark.connector.tpch
+
+import org.apache.spark.sql.SparkSession
+
+import org.apache.kyuubi.KyuubiFunSuite
+
+class TPCHCatalogSuite extends KyuubiFunSuite {
+
+  protected lazy val spark: SparkSession = {
+    SparkSession.builder()
+      .master("local[*]")
+      .config("spark.ui.enabled", "false")
+      .config("spark.sql.catalogImplementation", "in-memory")
+      .config("spark.sql.catalog.tpch", classOf[TPCHCatalog].getName)
+      .config("spark.sql.cbo.enabled", "true")
+      .config("spark.sql.cbo.planStats.enabled", "true")
+      .getOrCreate()
+  }
+
+  test("tpch.sf1 count") {
+    assert(spark.table("tpch.sf1.customer").count === 150000)
+    assert(spark.table("tpch.sf1.orders").count === 1500000)
+    assert(spark.table("tpch.sf1.lineitem").count === 6001215)
+    assert(spark.table("tpch.sf1.part").count === 200000)
+    assert(spark.table("tpch.sf1.partsupp").count === 800000)
+    assert(spark.table("tpch.sf1.supplier").count === 10000)
+    assert(spark.table("tpch.sf1.nation").count === 25)
+    assert(spark.table("tpch.sf1.region").count === 5)
+  }
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+    spark.stop()
+  }
+}
