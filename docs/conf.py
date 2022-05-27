414d1a8620db1fd9abc8f0888c55ee98e8880389
Kent Yao <yao@apache.org>
2022-05-27 17:21:42 +0800

    [KYUUBI #2764] [DOCS] Fix tables in docs being coverd by right toc sidebar
    ### _Why are the changes needed?_
    
    make table width fit browsers with 100% width, and 1st column where contains the kyuubi conf fit the table with 25% width.
    
    ### _How was this patch tested?_
    - [ ] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [x] Add screenshots for manual tests if appropriate
    
    <img width="1716" alt="image" src="https://user-images.githubusercontent.com/8326978/170655840-2309574e-4098-4b0f-b91d-50a30033b9ac.png">
    
    - [ ] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #2764 from yaooqinn/layout.
    
    Closes #2764
    
    9391a638 [Kent Yao] [DOCS] Fix tables in docs being coverd by right toc sidebar
    33817406 [Kent Yao] [DOCS] Fix tables in docs being coverd by right toc sidebar
    
    Authored-by: Kent Yao <yao@apache.org>
    Signed-off-by: Cheng Pan <chengpan@apache.org>

--- docs/conf.py
+++ docs/conf.py
@@ -132,6 +132,7 @@ pygments_style = 'sphinx'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ["css/custom.css"]
 htmlhelp_basename = 'Recommonmarkdoc'
 
 github_doc_root = 'https://github.com/apache/incubator-kyuubi/tree/master/docs/'
