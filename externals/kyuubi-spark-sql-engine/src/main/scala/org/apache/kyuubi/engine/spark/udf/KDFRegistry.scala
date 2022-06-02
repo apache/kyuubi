1023c56c1092b2ec60fa7475a5c3ed7fd64a8724
Mahmoud Bahaa <mahmoud.bahaa@incorta.com>
2022-06-02 13:28:35 +0200

    register java spark udfs through spark custom configurations

--- externals/kyuubi-spark-sql-engine/src/main/scala/org/apache/kyuubi/engine/spark/udf/KDFRegistry.scala
+++ externals/kyuubi-spark-sql-engine/src/main/scala/org/apache/kyuubi/engine/spark/udf/KDFRegistry.scala
@@ -21,16 +21,22 @@ import scala.collection.mutable.ArrayBuffer
 
 import org.apache.spark.{SparkEnv, TaskContext}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.api.java._
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.types.{DataType, DataTypes}
 
 import org.apache.kyuubi.{KYUUBI_VERSION, Utils}
+import org.apache.kyuubi.config.KyuubiConf
 import org.apache.kyuubi.config.KyuubiReservedKeys.KYUUBI_SESSION_USER_KEY
 
 object KDFRegistry {
 
   @transient
   val registeredFunctions = new ArrayBuffer[KyuubiDefinedFunction]()
+
+  lazy val udfs: Map[String, String] = new KyuubiConf().loadFileDefaults()
+    .getAllWithPrefix("kyuubi.spark.udf", "");
 
   lazy val appName = SparkEnv.get.conf.get("spark.app.name")
   lazy val appId = SparkEnv.get.conf.get("spark.app.id")
@@ -88,5 +94,313 @@ object KDFRegistry {
     for (func <- registeredFunctions) {
       spark.udf.register(func.name, func.udf)
     }
+
+    udfs.foreach(entry => {
+      val udfName = entry._1
+      val defParts = entry._2.split(",")
+      val udfFunc = Class.forName(defParts(0)).newInstance()
+      val numArgs = defParts(1).toInt
+      val field = classOf[DataTypes].getDeclaredField(defParts(2))
+      val returnedDataType: DataType = field.get(null).asInstanceOf[DataType]
+
+      numArgs match {
+        case 0 => spark.udf.register(udfName, udfFunc.asInstanceOf[UDF0[Any]], returnedDataType)
+        case 1 =>
+          spark.udf.register(udfName, udfFunc.asInstanceOf[UDF1[Any, Any]], returnedDataType)
+        case 2 =>
+          spark.udf.register(udfName, udfFunc.asInstanceOf[UDF2[Any, Any, Any]], returnedDataType)
+        case 3 =>
+          spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF3[Any, Any, Any, Any]],
+            returnedDataType)
+        case 4 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF4[Any, Any, Any, Any, Any]],
+            returnedDataType)
+        case 5 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF5[Any, Any, Any, Any, Any, Any]],
+            returnedDataType)
+        case 6 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF6[Any, Any, Any, Any, Any, Any, Any]],
+            returnedDataType)
+        case 7 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF7[Any, Any, Any, Any, Any, Any, Any, Any]],
+            returnedDataType)
+        case 8 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF8[Any, Any, Any, Any, Any, Any, Any, Any, Any]],
+            returnedDataType)
+        case 9 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF9[Any, Any, Any, Any, Any, Any, Any, Any, Any, Any]],
+            returnedDataType)
+        case 10 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF10[Any, Any, Any, Any, Any, Any, Any, Any, Any, Any, Any]],
+            returnedDataType)
+        case 11 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF11[
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any]],
+            returnedDataType)
+        case 12 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF12[
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any]],
+            returnedDataType)
+        case 13 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF13[
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any]],
+            returnedDataType)
+        case 14 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF14[
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any]],
+            returnedDataType)
+        case 15 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF15[
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any]],
+            returnedDataType)
+        case 16 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF16[
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any]],
+            returnedDataType)
+        case 17 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF17[
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any]],
+            returnedDataType)
+        case 18 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF18[
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any]],
+            returnedDataType)
+        case 19 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF19[
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any]],
+            returnedDataType)
+        case 20 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF20[
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any]],
+            returnedDataType)
+        case 21 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF21[
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any]],
+            returnedDataType)
+        case 22 => spark.udf.register(
+            udfName,
+            udfFunc.asInstanceOf[UDF22[
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any,
+              Any]],
+            returnedDataType)
+      }
+    })
   }
 }
