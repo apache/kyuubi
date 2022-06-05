d0141ddec6a58cea9606b5a0030d52df0e1118b7
Min Zhao <zhaomin1423@163.com>
2022-06-05 09:08:47 +0800

    fix

--- externals/kyuubi-jdbc-engine/src/test/scala/org/apache/kyuubi/engine/jdbc/doris/DorisOperationSuite.scala
+++ externals/kyuubi-jdbc-engine/src/test/scala/org/apache/kyuubi/engine/jdbc/doris/DorisOperationSuite.scala
@@ -16,63 +16,144 @@
  */
 package org.apache.kyuubi.engine.jdbc.doris
 
+import scala.collection.mutable.ArrayBuffer
+
 import org.apache.kyuubi.operation.HiveJDBCTestHelper
-import org.apache.kyuubi.operation.meta.ResultSetSchemaConstant.{TABLE_NAME, TABLE_SCHEMA}
+import org.apache.kyuubi.operation.meta.ResultSetSchemaConstant.{TABLE_CATALOG, TABLE_NAME, TABLE_SCHEMA, TABLE_TYPE}
 
 class DorisOperationSuite extends WithDorisEngine with HiveJDBCTestHelper {
 
   test("doris - get tables") {
+    case class Table(catalog: String, schema: String, tableName: String, tableType: String)
+
     withJdbcStatement() { statement =>
-      statement.execute("create database if not exists db1")
-      statement.execute("use db1")
-      statement.execute("create table db1.test1(id bigint)" +
-        "ENGINE=OLAP\n" +
-        "DISTRIBUTED BY HASH(`id`) BUCKETS 32\n" +
-        "PROPERTIES (\n\"replication_num\" = \"1\")")
-
       val meta = statement.getConnection.getMetaData
-      val table1 = meta.getTables(null, "db1", "test1", null)
-      table1.next()
-      val tableSchema1 = table1.getString(TABLE_SCHEMA)
-      val tableName1 = table1.getString(TABLE_NAME)
-      assert(tableSchema1 == "db1")
-      assert(tableName1 == "test1")
+      val resultBuffer = ArrayBuffer[Table]()
 
-      val table2 = meta.getTables(null, "db1", null, null)
-      table2.next()
-      val tableSchema2 = table2.getString(TABLE_SCHEMA)
-      val tableName2 = table2.getString(TABLE_NAME)
-      assert(tableSchema2 == "db1")
-      assert(tableName2 == "test1")
+      var tables = meta.getTables(null, null, null, null)
+      while (tables.next()) {
+        resultBuffer +=
+          Table(
+            tables.getString(TABLE_CATALOG),
+            tables.getString(TABLE_SCHEMA),
+            tables.getString(TABLE_NAME),
+            tables.getString(TABLE_TYPE))
+      }
+      assert(resultBuffer.contains(Table(null, "information_schema", "tables", "SYSTEM VIEW")))
+      assert(resultBuffer.contains(Table(null, "information_schema", "views", "SYSTEM VIEW")))
+      resultBuffer.clear()
 
-      val table3 = meta.getTables(null, null, "test1", null)
-      table3.next()
-      val tableSchema3 = table3.getString(TABLE_SCHEMA)
-      val tableName3 = table3.getString(TABLE_NAME)
-      assert(tableSchema3 == "db1")
-      assert(tableName3 == "test1")
+      statement.execute("create database if not exists db1")
+      statement.execute("create table db1.test1(id bigint)" +
+        "ENGINE=OLAP DISTRIBUTED BY HASH(`id`) BUCKETS 32 " +
+        "PROPERTIES ('replication_num' = '1')")
+      statement.execute("create table db1.test2(id bigint)" +
+        "ENGINE=OLAP DISTRIBUTED BY HASH(`id`) BUCKETS 32 " +
+        "PROPERTIES ('replication_num' = '1')")
 
-      val table4 = meta.getTables(null, "db%", null, null)
-      table4.next()
-      val tableSchema4 = table4.getString(TABLE_SCHEMA)
-      val tableName4 = table4.getString(TABLE_NAME)
-      assert(tableSchema4 == "db1")
-      assert(tableName4 == "test1")
-
-      val table5 = meta.getTables(null, "db1", "test1", Array("SYSTEM VIEW"))
-      assert(!table5.next())
+      statement.execute("create database if not exists db2")
+      statement.execute("create table db2.test1(id bigint)" +
+        "ENGINE=OLAP DISTRIBUTED BY HASH(`id`) BUCKETS 32 " +
+        "PROPERTIES ('replication_num' = '1')")
+      statement.execute("create table db2.test2(id bigint)" +
+        "ENGINE=OLAP DISTRIBUTED BY HASH(`id`) BUCKETS 32 " +
+        "PROPERTIES ('replication_num' = '1')")
 
       statement.execute("create view db1.view1 (k1) as select id from db1.test1")
-      val table6 = meta.getTables(null, null, null, Array("VIEW"))
-      table6.next()
-      val tableSchema6 = table6.getString(TABLE_SCHEMA)
-      val tableName6 = table6.getString(TABLE_NAME)
-      assert(tableSchema6 == "db1")
-      assert(tableName6 == "view1")
+
+      tables = meta.getTables(null, "db1", "test1", Array("BASE TABLE"))
+      while (tables.next()) {
+        val table = Table(
+          tables.getString(TABLE_CATALOG),
+          tables.getString(TABLE_SCHEMA),
+          tables.getString(TABLE_NAME),
+          tables.getString(TABLE_TYPE))
+        assert(table == Table(null, "db1", "test1", "BASE TABLE"))
+      }
+
+      tables = meta.getTables(null, "db1", null, null)
+      while (tables.next()) {
+        resultBuffer += Table(
+          tables.getString(TABLE_CATALOG),
+          tables.getString(TABLE_SCHEMA),
+          tables.getString(TABLE_NAME),
+          tables.getString(TABLE_TYPE))
+      }
+      assert(resultBuffer.contains(Table(null, "db1", "test1", "BASE TABLE")))
+      assert(resultBuffer.contains(Table(null, "db1", "test2", "BASE TABLE")))
+      resultBuffer.clear()
+
+      tables = meta.getTables(null, null, "test1", null)
+      while (tables.next()) {
+        resultBuffer += Table(
+          tables.getString(TABLE_CATALOG),
+          tables.getString(TABLE_SCHEMA),
+          tables.getString(TABLE_NAME),
+          tables.getString(TABLE_TYPE))
+      }
+      assert(resultBuffer.contains(Table(null, "db1", "test1", "BASE TABLE")))
+      assert(resultBuffer.contains(Table(null, "db2", "test1", "BASE TABLE")))
+      resultBuffer.clear()
+
+      tables = meta.getTables(null, "db%", "test1", null)
+      while (tables.next()) {
+        resultBuffer += Table(
+          tables.getString(TABLE_CATALOG),
+          tables.getString(TABLE_SCHEMA),
+          tables.getString(TABLE_NAME),
+          tables.getString(TABLE_TYPE))
+      }
+      assert(resultBuffer.contains(Table(null, "db1", "test1", "BASE TABLE")))
+      assert(resultBuffer.contains(Table(null, "db2", "test1", "BASE TABLE")))
+      resultBuffer.clear()
+
+      tables = meta.getTables(null, "db2", "test%", null)
+      while (tables.next()) {
+        resultBuffer += Table(
+          tables.getString(TABLE_CATALOG),
+          tables.getString(TABLE_SCHEMA),
+          tables.getString(TABLE_NAME),
+          tables.getString(TABLE_TYPE))
+      }
+      assert(resultBuffer.contains(Table(null, "db2", "test1", "BASE TABLE")))
+      assert(resultBuffer.contains(Table(null, "db2", "test2", "BASE TABLE")))
+      resultBuffer.clear()
+
+      tables = meta.getTables(null, "fake_db", "test1", null)
+      assert(!tables.next())
+
+      tables = meta.getTables(null, null, null, Array("VIEW"))
+      while (tables.next()) {
+        val table = Table(
+          tables.getString(TABLE_CATALOG),
+          tables.getString(TABLE_SCHEMA),
+          tables.getString(TABLE_NAME),
+          tables.getString(TABLE_TYPE))
+        assert(table == Table(null, "db1", "view1", "VIEW"))
+      }
+
+      tables = meta.getTables(null, null, null, Array("VIEW", "BASE TABLE"))
+      while (tables.next()) {
+        resultBuffer += Table(
+          tables.getString(TABLE_CATALOG),
+          tables.getString(TABLE_SCHEMA),
+          tables.getString(TABLE_NAME),
+          tables.getString(TABLE_TYPE))
+      }
+      assert(resultBuffer.contains(Table(null, "db1", "test1", "BASE TABLE")))
+      assert(resultBuffer.contains(Table(null, "db1", "test2", "BASE TABLE")))
+      assert(resultBuffer.contains(Table(null, "db2", "test1", "BASE TABLE")))
+      assert(resultBuffer.contains(Table(null, "db2", "test2", "BASE TABLE")))
+      assert(resultBuffer.contains(Table(null, "db1", "view1", "VIEW")))
+      resultBuffer.clear()
 
       statement.execute("drop view db1.view1")
       statement.execute("drop table db1.test1")
+      statement.execute("drop table db1.test2")
+      statement.execute("drop table db2.test1")
+      statement.execute("drop table db2.test2")
       statement.execute("drop database db1")
+      statement.execute("drop database db2")
     }
   }
 
