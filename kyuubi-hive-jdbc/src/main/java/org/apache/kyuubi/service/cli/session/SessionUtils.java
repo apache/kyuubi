fead185cec172b5fbaa4964d97b84b17e81c1ee2
jiaoqingbo <1178404354@qq.com>
2022-06-29 10:20:22 +0800

    modify license

--- kyuubi-hive-jdbc/src/main/java/org/apache/kyuubi/service/cli/session/SessionUtils.java
+++ kyuubi-hive-jdbc/src/main/java/org/apache/kyuubi/service/cli/session/SessionUtils.java
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.kyuubi.service.cli.session;
 
 import java.io.IOException;
