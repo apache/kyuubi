0d10ff8abb2ac1895c348a060f84a67ab2220526
Fei Wang <fwang12@ebay.com>
2022-07-13 16:04:03 +0800

    prevent unsupported exception

--- kyuubi-server/src/main/scala/org/apache/kyuubi/session/KyuubiSessionManager.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/session/KyuubiSessionManager.scala
@@ -114,7 +114,7 @@ class KyuubiSessionManager private (name: String) extends SessionManager(name) {
       batchRequest: BatchRequest,
       recoveryMetadata: Option[Metadata] = None): KyuubiBatchSessionImpl = {
     val username = Option(user).filter(_.nonEmpty).getOrElse("anonymous")
-    batchRequest.getConf.put(CLIENT_IP_KEY, ipAddress)
+    batchRequest.setConf((batchRequest.getConf.asScala ++ Map(CLIENT_IP_KEY -> ipAddress)).asJava)
     new KyuubiBatchSessionImpl(
       username,
       password,
