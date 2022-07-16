a452321dbadb7cdf16b8d11f2840acc0a25098b0
Min <zhaomin1423@163.com>
2022-07-16 09:44:24 +0800

    [KYUUBI #3098] Unify the event log code path

---
+++ kyuubi-events/src/main/scala/org/apache/kyuubi/events/EventHandlerRegister.scala
@@ -0,0 +1,78 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.kyuubi.events
+
+import org.apache.kyuubi.{KyuubiException, Logging}
+import org.apache.kyuubi.config.KyuubiConf
+import org.apache.kyuubi.config.KyuubiConf.{ENGINE_EVENT_LOGGERS, SERVER_EVENT_LOGGERS}
+import org.apache.kyuubi.events.EventLoggerType.EventLoggerType
+import org.apache.kyuubi.events.handler.EventHandler
+
+trait EventHandlerRegister extends Logging {
+
+  def registerEngineEventLoggers(conf: KyuubiConf): Unit = {
+    val loggers = conf.get(ENGINE_EVENT_LOGGERS)
+    register(loggers, conf)
+  }
+
+  def registerServiceEventLoggers(conf: KyuubiConf): Unit = {
+    val loggers = conf.get(SERVER_EVENT_LOGGERS)
+    register(loggers, conf)
+  }
+
+  private def register(loggers: Seq[String], conf: KyuubiConf): Unit = {
+    loggers
+      .map(EventLoggerType.withName)
+      .foreach {
+        loadEventHandler(_, conf)
+          .foreach(EventBus.register)
+      }
+  }
+
+  protected def createSparkEventHandler(kyuubiConf: KyuubiConf)
+      : Option[EventHandler[KyuubiEvent]] = {
+    throw new KyuubiException(s"unsupported spark event logger.")
+  }
+
+  protected def createJsonEventHandler(kyuubiConf: KyuubiConf)
+      : Option[EventHandler[KyuubiEvent]] = {
+    throw new KyuubiException(s"Unsupported json event logger.")
+  }
+
+  protected def createJdbcEventHandler(kyuubiConf: KyuubiConf)
+      : Option[EventHandler[KyuubiEvent]] = {
+    throw new KyuubiException(s"Unsupported jdbc event logger.")
+  }
+
+  private def loadEventHandler(
+      eventLoggerType: EventLoggerType,
+      kyuubiConf: KyuubiConf): Option[EventHandler[KyuubiEvent]] = {
+    eventLoggerType match {
+      case EventLoggerType.SPARK =>
+        createSparkEventHandler(kyuubiConf)
+
+      case EventLoggerType.JSON =>
+        createJsonEventHandler(kyuubiConf)
+
+      case EventLoggerType.JDBC =>
+        createJdbcEventHandler(kyuubiConf)
+
+      case other =>
+        throw new KyuubiException(s"Unsupported event logger: ${other.toString}")
+    }
+  }
+}
