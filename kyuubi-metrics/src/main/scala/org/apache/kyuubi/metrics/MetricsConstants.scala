23ad7801c1d636c98c631e4b61129d67fdc7215a
Tianlin Liao <tiliao@ebay.com>
2022-08-15 14:49:04 +0800

    [KYUUBI #3170] Expose thrift binary connection metrics
    ### _Why are the changes needed?_
    
    ### _How was this patch tested?_
    - [x] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [ ] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #3229 from lightning-L/kyuubi-3170.
    
    Closes #3170
    
    ccb14571 [Tianlin Liao] [KYUUBI #3170] Expose thrift binary connection metrics
    
    Authored-by: Tianlin Liao <tiliao@ebay.com>
    Signed-off-by: Cheng Pan <chengpan@apache.org>

--- kyuubi-metrics/src/main/scala/org/apache/kyuubi/metrics/MetricsConstants.scala
+++ kyuubi-metrics/src/main/scala/org/apache/kyuubi/metrics/MetricsConstants.scala
@@ -32,6 +32,7 @@ object MetricsConstants {
 
   final private val CONN = KYUUBI + "connection."
   final private val THRIFT_HTTP_CONN = KYUUBI + "thrift.http.connection."
+  final private val THRIFT_BINARY_CONN = KYUUBI + "thrift.binary.connection."
 
   final val CONN_OPEN: String = CONN + "opened"
   final val CONN_FAIL: String = CONN + "failed"
@@ -40,6 +41,10 @@ object MetricsConstants {
   final val THRIFT_HTTP_CONN_OPEN: String = THRIFT_HTTP_CONN + "opened"
   final val THRIFT_HTTP_CONN_FAIL: String = THRIFT_HTTP_CONN + "failed"
   final val THRIFT_HTTP_CONN_TOTAL: String = THRIFT_HTTP_CONN + "total"
+
+  final val THRIFT_BINARY_CONN_OPEN: String = THRIFT_BINARY_CONN + "opened"
+  final val THRIFT_BINARY_CONN_FAIL: String = THRIFT_BINARY_CONN + "failed"
+  final val THRIFT_BINARY_CONN_TOTAL: String = THRIFT_BINARY_CONN + "total"
 
   final private val ENGINE = KYUUBI + "engine."
   final val ENGINE_FAIL: String = ENGINE + "failed"
