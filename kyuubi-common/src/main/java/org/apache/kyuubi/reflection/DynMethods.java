004c05d9de2801a645ac2b4212942db7b5fecf35
Cheng Pan <chengpan@apache.org>
2022-08-15 13:31:23 +0800

    nit

--- kyuubi-common/src/main/java/org/apache/kyuubi/reflection/DynMethods.java
+++ kyuubi-common/src/main/java/org/apache/kyuubi/reflection/DynMethods.java
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.apache.kyuubi.reflection;
 
 import java.lang.reflect.InvocationTargetException;
