4ab298fb88cc62a83d999000445691d568ac0a28
William Tong <weitong@cisco.com>
2022-08-19 11:11:08 +0800

    transfer element-plus auto import to full import

--- kyuubi-server/web-ui/src/main.ts
+++ kyuubi-server/web-ui/src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import router from '@/router'
 import { store } from '@/pinia'
 import i18n from '@/locales'
+import ElementPlus from 'element-plus'
+import '@/styles/element/index.scss'
 import './styles/index.scss'
 import App from './App.vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
@@ -10,4 +12,4 @@ const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
-app.use(router).use(store).use(i18n).mount('#app')
+app.use(router).use(store).use(i18n).use(ElementPlus).mount('#app')
--- kyuubi-server/web-ui/vite.config.ts
+++ kyuubi-server/web-ui/vite.config.ts
@@ -18,31 +18,9 @@
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import path from 'path'
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
-  plugins: [
-    Vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()]
-    }),
-    Components({
-      resolvers: [
-        ElementPlusResolver({
-          importStyle: 'sass'
-        })
-      ]
-    })
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@use "@/styles/element/index.scss" as *;'
-      }
-    }
-  },
+  plugins: [Vue()],
   resolve: {
     alias: [
       {
