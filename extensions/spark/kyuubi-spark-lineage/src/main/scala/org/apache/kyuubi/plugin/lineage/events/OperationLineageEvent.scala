9716548380a21f2608ec1f413f1f6986366b4f18
odone <odone.zhang@gmail.com>
2022-08-24 14:11:35 +0800

    [KYUUBI #2282] Add lineage records for sql statement execution in Kyuubi engine logs
    ### _Why are the changes needed?_
    
    Lineage information:
    ```
    col0 -> (table.a, table.b)
    col1 -> (table.c, table.a)
    ```
    
    SQL lineage logger JSON format example.
    **SQL:**
    ```
    select a as col0, b as col1 from test_table0
    ```
    **Lineage:**
    ```
    {
       "inputTables": ["default.test_table0"],
       "outputTables": [],
       "columnLineage": [{
          "column": "col0",
          "originalColumns": ["default.test_table0.a"]
       }, {
          "column": "col1",
          "originalColumns": ["default.test_table0.b"]
       }]
    }
    ```
    
    Currently supported column lineage for spark `Command` and `Query` `TreeNode`:
    
    ### Query
    - `Select`
    
    ### Command
    - `CreateDataSourceTableAsSelectCommand`
    - `CreateHiveTableAsSelectCommand`
    - `OptimizedCreateHiveTableAsSelectCommand`
    - `CreateTableAsSelect`
    - `ReplaceTableAsSelect`
    - `InsertIntoDataSourceCommand`
    - `InsertIntoHadoopFsRelationCommand`
    - `InsertIntoDataSourceDirCommand`
    - `InsertIntoHiveDirCommand`
    - `InsertIntoHiveTable`
    
    ### _How was this patch tested?_
    - [x] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [x] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #3185 from iodone/kyuubi-2282.
    
    Closes #2282
    
    002c6d61 [odone] delete spark-sql-engine test for lineage
    e1728a79 [odone] update lineage entity schema
    de2a3e9a [odone] change kyuubi-spark-listener module to kyuubi-spark-lineage module
    9258125e [odone] optimize lineage output
    834669ed [odone] delete engine lineage parse
    d9c7a3dc [odone] add spark listener to support lineage
    4bae8c2f [odone] update for code cleaning
    32b3392b [odone] update for review
    fe09e478 [odone] add some test
    
    Authored-by: odone <odone.zhang@gmail.com>
    Signed-off-by: Kent Yao <yao@apache.org>

---
+++ extensions/spark/kyuubi-spark-lineage/src/main/scala/org/apache/kyuubi/plugin/lineage/events/OperationLineageEvent.scala
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.plugin.lineage.events
+
+import org.apache.kyuubi.Utils
+import org.apache.kyuubi.events.KyuubiEvent
+
+case class ColumnLineage(column: String, originalColumns: Set[String])
+
+/**
+ * @param inputTables the tables of the operation will read
+ * @param outputTables the tables of the operation will write
+ * @param columnLineage the output columns are associated to columns of the real table's columns
+ */
+
+class Lineage(
+    val inputTables: List[String],
+    val outputTables: List[String],
+    val columnLineage: List[ColumnLineage]) {
+
+  override def equals(other: Any): Boolean = other match {
+    case otherLineage: Lineage =>
+      otherLineage.inputTables == inputTables && otherLineage.outputTables == outputTables &&
+        otherLineage.columnLineage == columnLineage
+    case _ => false
+  }
+
+  override def hashCode(): Int = super.hashCode()
+}
+
+object Lineage {
+  def apply(
+      inputTables: List[String],
+      outputTables: List[String],
+      columnLineage: List[(String, Set[String])]): Lineage = {
+    val newColumnLineage = columnLineage.map {
+      case (column, originalColumns) =>
+        ColumnLineage(column, originalColumns)
+    }
+    new Lineage(inputTables, outputTables, newColumnLineage)
+  }
+}
+
+case class OperationLineageEvent(
+    executionId: Long,
+    eventTime: Long,
+    lineage: Option[Lineage],
+    exception: Option[Throwable]) extends KyuubiEvent {
+
+  override def partitions: Seq[(String, String)] =
+    ("day", Utils.getDateFromTimestamp(eventTime)) :: Nil
+}
---
+++ extensions/spark/kyuubi-spark-lineage/src/test/scala/org/apache/kyuubi/plugin/lineage/events/OperationLineageEventSuite.scala
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.plugin.lineage.events
+
+import java.util.concurrent.CountDownLatch
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkListenerExtensionTest
+
+import org.apache.kyuubi.KyuubiFunSuite
+import org.apache.kyuubi.events.EventBus
+import org.apache.kyuubi.plugin.lineage.helper.SparkListenerHelper.isSparkVersionAtMost
+
+class OperationLineageEventSuite extends KyuubiFunSuite with SparkListenerExtensionTest {
+
+  val catalogName =
+    if (isSparkVersionAtMost("3.1")) "org.apache.spark.sql.connector.InMemoryTableCatalog"
+    else "org.apache.spark.sql.connector.catalog.InMemoryTableCatalog"
+
+  override protected val catalogImpl: String = "hive"
+
+  override def sparkConf(): SparkConf = {
+    super.sparkConf()
+      .set("spark.sql.catalog.v2_catalog", catalogName)
+      .set(
+        "spark.sql.queryExecutionListeners",
+        "org.apache.kyuubi.plugin.lineage.SparkOperationLineageQueryExecutionListener")
+  }
+
+  test("operation lineage event capture: for execute sql") {
+    val countDownLatch = new CountDownLatch(1)
+    var actual: Lineage = null
+    EventBus.register[OperationLineageEvent] { event =>
+      event.lineage.foreach {
+        case lineage if lineage.inputTables.nonEmpty =>
+          actual = lineage
+          countDownLatch.countDown()
+      }
+    }
+
+    withTable("test_table0") { _ =>
+      spark.sql("create table test_table0(a string, b string)")
+      spark.sql("select a as col0, b as col1 from test_table0").collect()
+      val expected = Lineage(
+        List("default.test_table0"),
+        List(),
+        List(
+          ("col0", Set("default.test_table0.a")),
+          ("col1", Set("default.test_table0.b"))))
+      countDownLatch.await()
+      assert(actual == expected)
+    }
+  }
+
+}
