5b16a9a25c1b5d72c6a1a33c241f4090713d99a1
Fei Wang <fwang12@ebay.com>
2022-08-26 13:52:15 +0800

    use eventually

--- kyuubi-server/src/test/scala/org/apache/kyuubi/server/metadata/MetadataManagerSuite.scala
+++ kyuubi-server/src/test/scala/org/apache/kyuubi/server/metadata/MetadataManagerSuite.scala
@@ -19,6 +19,8 @@ package org.apache.kyuubi.server.metadata
 
 import java.util.UUID
 
+import scala.collection.JavaConverters._
+
 import org.scalatest.time.SpanSugar.convertIntToGrainOfTime
 
 import org.apache.kyuubi.{KyuubiException, KyuubiFunSuite}
@@ -96,9 +98,12 @@ class MetadataManagerSuite extends KyuubiFunSuite {
       assert(!retryRef2.hasRemainingRequests())
       assert(metadataManager.getBatch(metadata2.identifier).getState === "RUNNING")
     }
-    metadataManager.deRegisterRequestsRetryRef(metadata.identifier)
-    metadataManager.deRegisterRequestsRetryRef(metadata2.identifier)
-    Thread.sleep(conf.get(KyuubiConf.METADATA_REQUEST_RETRY_INTERVAL) * 2)
+
+    metadataManager.identifierRequestsRetryRefs.clear()
+    eventually(timeout(3.seconds)) {
+      metadataManager.identifierRequestsRetryingCounts.asScala.forall(_._2.get() == 0)
+    }
+    metadataManager.identifierRequestsRetryingCounts.clear()
   }
 
   test("metadata request metrics") {
