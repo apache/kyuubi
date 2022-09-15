1c8938beae1297128f50552ae0b9f17930714d6a
jiaoqingbo <1178404354@qq.com>
2022-09-15 10:28:50 +0800

    fix ut failed

--- kyuubi-server/src/test/scala/org/apache/kyuubi/server/rest/client/BatchRestApiSuite.scala
+++ kyuubi-server/src/test/scala/org/apache/kyuubi/server/rest/client/BatchRestApiSuite.scala
@@ -80,7 +80,8 @@ class BatchRestApiSuite extends RestClientTestHelper with BatchTestHelper {
       // get batch by id
       batchRestApi.getBatchById("1")
     }
-    assert(e.getCause.toString.contains(s"Error validating LDAP user: uid=${customUser}"))
+    assert(e.getCause.toString.contains(
+      s"Error validating LDAP user: $customUser, bindDn: uid=$customUser,$ldapBaseDn"))
 
     basicKyuubiRestClient.close()
 
