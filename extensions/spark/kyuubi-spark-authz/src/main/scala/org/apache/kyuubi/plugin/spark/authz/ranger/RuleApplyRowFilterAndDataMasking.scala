b161d70f76e6a152f170e0d5abe1ec517201327f
Bowen Liang <liangbowen@gf.com.cn>
2022-09-20 18:04:35 +0800

    nit

--- extensions/spark/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/ranger/RuleApplyRowFilterAndDataMasking.scala
+++ extensions/spark/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/ranger/RuleApplyRowFilterAndDataMasking.scala
@@ -53,27 +53,29 @@ class RuleApplyRowFilterAndDataMasking(spark: SparkSession) extends Rule[Logical
   override def apply(plan: LogicalPlan): LogicalPlan = {
     // Apply FilterAndMasking and wrap HiveTableRelation/LogicalRelation/DataSourceV2Relation with
     // RowFilterAndDataMaskingMarker if it is not wrapped yet.
-    mapPlanChildren(plan, {
-      case p: RowFilterAndDataMaskingMarker => p
-      case hiveTableRelation if hasResolvedHiveTable(hiveTableRelation) =>
-        val table = getHiveTable(hiveTableRelation)
-        applyFilterAndMasking(hiveTableRelation, table.identifier, spark)
-      case logicalRelation if hasResolvedDatasourceTable(logicalRelation) =>
-        val table = getDatasourceTable(logicalRelation)
-        if (table.isEmpty) {
-          logicalRelation
-        } else {
-          applyFilterAndMasking(logicalRelation, table.get.identifier, spark)
-        }
-      case datasourceV2Relation if hasResolvedDatasourceV2Table(datasourceV2Relation) =>
-        val tableIdentifier = getDatasourceV2Identifier(datasourceV2Relation)
-        if (tableIdentifier.isEmpty) {
-          datasourceV2Relation
-        } else {
-          applyFilterAndMasking(datasourceV2Relation, tableIdentifier.get, spark)
-        }
-      case other => apply(other)
-    })
+    mapPlanChildren(
+      plan,
+      {
+        case p: RowFilterAndDataMaskingMarker => p
+        case hiveTableRelation if hasResolvedHiveTable(hiveTableRelation) =>
+          val table = getHiveTable(hiveTableRelation)
+          applyFilterAndMasking(hiveTableRelation, table.identifier, spark)
+        case logicalRelation if hasResolvedDatasourceTable(logicalRelation) =>
+          val table = getDatasourceTable(logicalRelation)
+          if (table.isEmpty) {
+            logicalRelation
+          } else {
+            applyFilterAndMasking(logicalRelation, table.get.identifier, spark)
+          }
+        case datasourceV2Relation if hasResolvedDatasourceV2Table(datasourceV2Relation) =>
+          val tableIdentifier = getDatasourceV2Identifier(datasourceV2Relation)
+          if (tableIdentifier.isEmpty) {
+            datasourceV2Relation
+          } else {
+            applyFilterAndMasking(datasourceV2Relation, tableIdentifier.get, spark)
+          }
+        case other => apply(other)
+      })
   }
 
   private def applyFilterAndMasking(
