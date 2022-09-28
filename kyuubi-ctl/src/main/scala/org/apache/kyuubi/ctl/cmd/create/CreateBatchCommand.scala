68ee31017888336553430530687bf411befa20e7
Fei Wang <fwang12@ebay.com>
2022-09-28 14:34:23 +0800

    fix npe

--- kyuubi-ctl/src/main/scala/org/apache/kyuubi/ctl/cmd/create/CreateBatchCommand.scala
+++ kyuubi-ctl/src/main/scala/org/apache/kyuubi/ctl/cmd/create/CreateBatchCommand.scala
@@ -16,13 +16,13 @@
  */
 package org.apache.kyuubi.ctl.cmd.create
 
-import java.util.{List => JList, Map => JMap}
+import java.util.{Collections, List => JList, Map => JMap}
 
 import scala.collection.JavaConverters._
 
 import org.apache.kyuubi.client.BatchRestApi
 import org.apache.kyuubi.client.api.v1.dto.{Batch, BatchRequest}
-import org.apache.kyuubi.ctl.CliConfig
+import org.apache.kyuubi.ctl.{CliConfig, ControlCliException}
 import org.apache.kyuubi.ctl.RestClientFactory.withKyuubiRestClient
 import org.apache.kyuubi.ctl.cmd.Command
 import org.apache.kyuubi.ctl.util.{CtlUtils, Render, Validator}
@@ -40,10 +40,16 @@ class CreateBatchCommand(cliConfig: CliConfig) extends Command[Batch](cliConfig)
       val batchRestApi: BatchRestApi = new BatchRestApi(kyuubiRestClient)
 
       val request = map.get("request").asInstanceOf[JMap[String, Object]]
-      val config = request.get("configs").asInstanceOf[JMap[Object, Object]].asScala
-        .map { case (k, v) => (k.toString, v.toString) }.asJava
-      val args = request.get("args").asInstanceOf[JList[Object]].asScala
-        .map(x => x.toString).asJava
+      if (request == null) {
+        error(s"No batch request field specified in yaml")
+        throw ControlCliException(1)
+      }
+      val config = Option(request.get("configs").asInstanceOf[JMap[Object, Object]].asScala)
+        .map(_.map { case (k, v) => (k.toString, v.toString) }.asJava)
+        .getOrElse(Collections.emptyMap())
+      val args = Option(request.get("args").asInstanceOf[JList[Object]].asScala)
+        .map(_.map(x => x.toString).asJava)
+        .getOrElse(Collections.emptyList())
       val batchRequest = new BatchRequest(
         request.get("batchType").asInstanceOf[String],
         request.get("resource").asInstanceOf[String],
