bb50c52c2f6f2068dbd9897cc04280b63b299f2f
Fu Chen <cfmcgrady@gmail.com>
2022-09-30 15:43:58 +0800

    [KYUUBI #3545][KYUUBI #3563] Support restrict spark configurations
    ### _Why are the changes needed?_
    
    ban end-user from security settings
    
    ### _How was this patch tested?_
    - [x] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [ ] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #3564 from cfmcgrady/kyuubi-3563.
    
    Closes #3545
    
    Closes #3563
    
    9d912b11 [Fu Chen] rename
    ee44f7df [Fu Chen] fix ci
    158f1552 [Fu Chen] address comment
    3cbf4794 [Fu Chen] doc
    0125a862 [Fu Chen] address comment
    e4e554f5 [Kent Yao] Update extensions/spark/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/ranger/AuthzConfigurationCheckExtension.scala
    a8d35efa [Fu Chen] ban end-user from security settings
    
    Lead-authored-by: Fu Chen <cfmcgrady@gmail.com>
    Co-authored-by: Kent Yao <yao@apache.org>
    Signed-off-by: Kent Yao <yao@apache.org>

---
+++ extensions/spark/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/ranger/AuthzConfigurationChecker.scala
@@ -0,0 +1,46 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.plugin.spark.authz.ranger
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
+import org.apache.spark.sql.execution.command.SetCommand
+
+import org.apache.kyuubi.plugin.spark.authz.AccessControlException
+
+/**
+ * For banning end-users from set restricted spark configurations
+ */
+case class AuthzConfigurationChecker(spark: SparkSession) extends (LogicalPlan => Unit) {
+
+  final val RESTRICT_LIST_KEY = "spark.kyuubi.conf.restricted.list"
+
+  private val restrictedConfList: Set[String] =
+    Set(RESTRICT_LIST_KEY, "spark.sql.runSQLOnFiles", "spark.sql.extensions") ++
+      spark.conf.getOption(RESTRICT_LIST_KEY).map(_.split(',').toSet).getOrElse(Set.empty)
+
+  override def apply(plan: LogicalPlan): Unit = plan match {
+    case SetCommand(Some((
+          "spark.sql.optimizer.excludedRules",
+          Some(v)))) if v.contains("org.apache.kyuubi.plugin.spark.authz.ranger") =>
+      throw new AccessControlException("Excluding Authz security rules is not allowed")
+    case SetCommand(Some((k, Some(_)))) if restrictedConfList.contains(k) =>
+      throw new AccessControlException(s"Modifying config $k is not allowed")
+    case _ =>
+  }
+}
