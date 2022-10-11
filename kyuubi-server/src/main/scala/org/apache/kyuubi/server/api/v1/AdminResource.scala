590e993a4dbb3970aa1450057332519bcb22ec55
Tianlin Liao <tiliao@ebay.com>
2022-10-11 17:26:13 +0800

    Support to delete the engine node from engine discovery space with AdminResource

--- kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/v1/AdminResource.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/v1/AdminResource.scala
@@ -17,14 +17,18 @@
 
 package org.apache.kyuubi.server.api.v1
 
-import javax.ws.rs.{NotAllowedException, Path, POST, Produces}
+import javax.ws.rs._
 import javax.ws.rs.core.{MediaType, Response}
 
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 
-import org.apache.kyuubi.{Logging, Utils}
+import org.apache.kyuubi.{KYUUBI_VERSION, Logging, Utils}
+import org.apache.kyuubi.config.KyuubiConf._
+import org.apache.kyuubi.ha.HighAvailabilityConf.HA_NAMESPACE
+import org.apache.kyuubi.ha.client.DiscoveryClientProvider.withDiscoveryClient
+import org.apache.kyuubi.ha.client.DiscoveryPaths
 import org.apache.kyuubi.server.KyuubiServer
 import org.apache.kyuubi.server.api.ApiRequestContext
 
@@ -52,5 +56,73 @@ private[v1] class AdminResource extends ApiRequestContext with Logging {
     info(s"Reloading the Kyuubi server hadoop conf")
     KyuubiServer.reloadHadoopConf()
     Response.ok(s"Refresh the hadoop conf for ${fe.connectionUrl} successfully.").build()
+  }
+
+  @ApiResponse(
+    responseCode = "200",
+    content = Array(new Content(
+      mediaType = MediaType.APPLICATION_JSON)),
+    description = "delete kyuubi engine")
+  @DELETE
+  @Path("engine")
+  def deleteEngine(
+      @QueryParam("user") user: String,
+      @QueryParam("type") engineType: String,
+      @QueryParam("subdomain") engineSubdomain: String,
+      @QueryParam("sharelevel") engineShareLevel: String,
+      @QueryParam("version") version: String,
+      @QueryParam("namespace") namespace: String): Response = {
+    val userName = fe.getUserName(Map.empty)
+    val ipAddress = fe.getIpAddress
+    info(s"Received delete engine request from $userName/$ipAddress")
+    if (!userName.equals(administrator)) {
+      throw new NotAllowedException(
+        s"$userName is not allowed to delete kyuubi engine")
+    }
+
+    // validate parameters
+    require(user != null && !user.isEmpty, "user is a required parameter")
+
+    // use default value from kyuubi conf when param is not provided
+    val normalizedEngineType = Some(engineType)
+      .filter(_ != null).filter(_.nonEmpty)
+      .getOrElse(fe.getConf.get(ENGINE_TYPE))
+    val normalizedEngineSubdomain = Some(engineSubdomain)
+      .filter(_ != null).filter(_.nonEmpty)
+      .getOrElse(fe.getConf.get(ENGINE_SHARE_LEVEL_SUBDOMAIN).getOrElse("default"))
+    val normalizedEngineShareLevel = Some(engineShareLevel)
+      .filter(_ != null).filter(_.nonEmpty)
+      .getOrElse(fe.getConf.get(ENGINE_SHARE_LEVEL))
+    val normalizedNamespace = Some(namespace)
+      .filter(_ != null).filter(_.nonEmpty)
+      .getOrElse(fe.getConf.get(HA_NAMESPACE))
+    val normalizedVersion = Some(version)
+      .filter(_ != null).filter(_.nonEmpty)
+      .getOrElse(KYUUBI_VERSION)
+
+    val engineSpace = DiscoveryPaths.makePath(
+      s"${normalizedNamespace}_" +
+        s"${normalizedVersion}_" +
+        s"${normalizedEngineShareLevel}_${normalizedEngineType}",
+      user,
+      Array(normalizedEngineSubdomain))
+
+    withDiscoveryClient(fe.getConf) { discoveryClient =>
+      val serviceNodes = discoveryClient.getServiceNodesInfo(engineSpace)
+      serviceNodes.foreach { node =>
+        val nodePath = s"$engineSpace/${node.nodeName}"
+        info(s"Deleting zookeeper engine node:$nodePath")
+        try {
+          discoveryClient.delete(nodePath)
+        } catch {
+          case e: Exception =>
+            error(s"Failed to delete zookeeper engine node:$nodePath", e)
+            throw new NotFoundException(s"Failed to delete zookeeper engine node:$nodePath," +
+              s"${e.getMessage}")
+        }
+      }
+    }
+
+    Response.ok(s"Engine ${engineSpace} is deleted successfully.").build()
   }
 }
