9b7c15c617f57d47660e45ec1f05938ca9884454
jinsilei <jinsilei@corp.netease.com>
2022-11-11 14:56:31 +0800

    remove replace

--- extensions/spark/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/PrivilegesBuilder.scala
+++ extensions/spark/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/PrivilegesBuilder.scala
@@ -524,18 +524,6 @@ object PrivilegesBuilder {
         val databases = getFieldVal[Seq[String]](resolvedNamespace, "namespace")
         outputObjs += databasePrivileges(quote(databases))
 
-      case "ReplaceArcticData" =>
-        val relation = getPlanField[Any]("table")
-        val identifier = getFieldVal[AnyRef](relation, "identifier").asInstanceOf[Option[AnyRef]]
-        val namespace = invoke(identifier.get, "namespace").asInstanceOf[Array[String]]
-        val table = invoke(identifier.get, "name").asInstanceOf[String]
-        outputObjs += PrivilegeObject(
-          TABLE_OR_VIEW,
-          PrivilegeObjectActionType.UPDATE,
-          quote(namespace),
-          table,
-          Nil)
-
       case _ =>
       // AddArchivesCommand
       // AddFileCommand
