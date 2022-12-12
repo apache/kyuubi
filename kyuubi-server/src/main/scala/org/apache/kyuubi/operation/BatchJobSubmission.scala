30dda80c377435af6769436235c3a9a78bbe1246
fwang12 <fwang12@ebay.com>
2022-12-12 13:47:13 +0800

    check state

--- kyuubi-server/src/main/scala/org/apache/kyuubi/operation/BatchJobSubmission.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/operation/BatchJobSubmission.scala
@@ -298,20 +298,20 @@ class BatchJobSubmission(
 
       MetricsSystem.tracing(_.decCount(MetricRegistry.name(OPERATION_OPEN, opType)))
 
-      if (!builder.processLaunched) {
-        builder.close()
-        if (recoveryMetadata.isDefined) {
-          killMessage = killBatchApplication()
-        }
-        setState(OperationState.CANCELED)
-        updateBatchMetadata()
-        return
-      }
-
       // fast fail
       if (isTerminalState(state)) {
         killMessage = (false, s"batch $batchId is already terminal so can not kill it.")
         builder.close()
+        return
+      }
+
+      // the builder might not be launched for recovery mode
+      if (recoveryMetadata.isEmpty && !builder.processLaunched) {
+        builder.close()
+        if (isTerminalState(state)) {
+          setState(OperationState.CANCELED)
+        }
+        updateBatchMetadata()
         return
       }
 
