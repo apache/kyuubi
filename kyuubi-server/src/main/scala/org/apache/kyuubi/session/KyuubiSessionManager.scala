7b5af4ae09d265953d0f26561c668e6c7e77090d
Yikf <yikaifei1@gmail.com>
2022-12-15 17:07:05 +0800

    [KYUUBI #3926] Introduce antlr4 to parse query statement
    ### _Why are the changes needed?_
    
    Close https://github.com/apache/incubator-kyuubi/issues/3926, we intend to introduce the parser module based on antlr4 in the Apache kyuubi server. Through this module, we can achieve:
    
    1. Expand kyuubi capabilities, for example, implement `SHOW KYUUBI_SESSIONS` through SQL.
    2. Sql translation for mysql front-end protocol.
    3. Sql translation for trino front-end protocol.
    
    This issue is the first step of the parser module, which introduces the parser module and implements `SHOW KYUUBI_SESSION`, and then improves the Apache kyuubi parser module based on this initial pr.
    
    ### _How was this patch tested?_
    - [x] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [x] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #3944 from Yikf/parser-init.
    
    Closes #3926
    
    4463b298 [Yikf] [KYUUBI #3926] Introduce antlr4 to parse query statement
    
    Authored-by: Yikf <yikaifei1@gmail.com>
    Signed-off-by: Cheng Pan <chengpan@apache.org>

--- kyuubi-server/src/main/scala/org/apache/kyuubi/session/KyuubiSessionManager.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/session/KyuubiSessionManager.scala
@@ -35,11 +35,14 @@ import org.apache.kyuubi.operation.{KyuubiOperationManager, OperationState}
 import org.apache.kyuubi.plugin.{GroupProvider, PluginLoader, SessionConfAdvisor}
 import org.apache.kyuubi.server.metadata.{MetadataManager, MetadataRequestsRetryRef}
 import org.apache.kyuubi.server.metadata.api.Metadata
+import org.apache.kyuubi.sql.parser.KyuubiParser
 import org.apache.kyuubi.util.SignUtils
 
 class KyuubiSessionManager private (name: String) extends SessionManager(name) {
 
   def this() = this(classOf[KyuubiSessionManager].getSimpleName)
+
+  private val parser = new KyuubiParser()
 
   val operationManager = new KyuubiOperationManager()
   val credentialsManager = new HadoopCredentialsManager()
@@ -85,7 +88,8 @@ class KyuubiSessionManager private (name: String) extends SessionManager(name) {
       ipAddress,
       conf,
       this,
-      this.getConf.getUserDefaults(user))
+      this.getConf.getUserDefaults(user),
+      parser)
   }
 
   override def openSession(
