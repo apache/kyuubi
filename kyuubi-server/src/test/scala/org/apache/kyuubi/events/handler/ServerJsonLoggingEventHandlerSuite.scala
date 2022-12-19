a69fc25e9f3d6b1a5a3deede30305691d1643c7c
fwang12 <fwang12@ebay.com>
2022-12-19 11:41:42 +0800

    save

--- kyuubi-server/src/test/scala/org/apache/kyuubi/events/handler/ServerJsonLoggingEventHandlerSuite.scala
+++ kyuubi-server/src/test/scala/org/apache/kyuubi/events/handler/ServerJsonLoggingEventHandlerSuite.scala
@@ -20,23 +20,22 @@ package org.apache.kyuubi.events.handler
 import java.net.InetAddress
 import java.nio.file.Paths
 import java.util.UUID
-
 import scala.collection.JavaConverters._
 import scala.util.matching.Regex
-
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.hive.service.rpc.thrift.{TOpenSessionReq, TStatusCode}
-
 import org.apache.kyuubi._
 import org.apache.kyuubi.config.KyuubiConf
 import org.apache.kyuubi.operation.HiveJDBCTestHelper
 import org.apache.kyuubi.operation.OperationState._
 import org.apache.kyuubi.server.KyuubiServer
 import org.apache.kyuubi.service.ServiceState
+import org.apache.kyuubi.session.KyuubiSessionManager
 
-class ServerJsonLoggingEventHandlerSuite extends WithKyuubiServer with HiveJDBCTestHelper {
+class ServerJsonLoggingEventHandlerSuite extends WithKyuubiServer with HiveJDBCTestHelper
+  with BatchTestHelper {
 
   private val engineLogRoot = "file://" + Utils.createTempDir().toString
   private val serverLogRoot = "file://" + Utils.createTempDir().toString
@@ -116,6 +115,7 @@ class ServerJsonLoggingEventHandlerSuite extends WithKyuubiServer with HiveJDBCT
         assert(res.getString("remoteSessionId") == "")
         assert(res.getLong("startTime") > 0)
         assert(res.getInt("totalOperations") == 0)
+        assert(res.getString("sessionType") === "SQL")
         assert(res.next())
         assert(res.getInt("totalOperations") == 0)
         assert(res.getString("sessionId") == sid)
@@ -123,10 +123,37 @@ class ServerJsonLoggingEventHandlerSuite extends WithKyuubiServer with HiveJDBCT
         assert(res.getLong("openedTime") > 0)
         assert(res.next())
         assert(res.getInt("totalOperations") == 1)
+        assert(res.getString("sessionType") === "SQL")
         assert(res.getLong("endTime") > 0)
         assert(!res.next())
       }
     }
+
+    val batchConf = Map(KyuubiConf.SESSION_NAME.key -> sparkBatchTestAppName)
+    val batchRequest = newSparkBatchRequest(batchConf)
+    val sessionMgr = server.backendService.sessionManager.asInstanceOf[KyuubiSessionManager]
+    val batchSessionHandle = sessionMgr.openBatchSession(
+      Utils.currentUser,
+      "kyuubi",
+      "127.0.0.1",
+      batchConf,
+      batchRequest)
+    withSessionConf()(Map.empty)(Map("spark.sql.shuffle.partitions" -> "2")) {
+      withJdbcStatement() { statement =>
+        val res = statement.executeQuery(
+          s"SELECT * FROM `json`.`$serverSessionEventPath` " +
+            s"where sessionName = '${sparkBatchTestAppName}' order by totalOperations")
+        assert(res.next())
+        assert(res.getString("user") == Utils.currentUser)
+        assert(res.getString("sessionName") == sparkBatchTestAppName)
+        assert(res.getString("sessionId") === batchSessionHandle.identifier.toString)
+        assert(res.getString("remoteSessionId") == "")
+        assert(res.getLong("startTime") > 0)
+        assert(res.getInt("totalOperations") == 0)
+        assert(res.getString("sessionType") === "BATCH")
+      }
+    }
+
   }
 
   test("engine session id is not same with server session id") {
