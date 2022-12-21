3a0f08e367590324092c8f75215da09dc5922842
fwang12 <fwang12@ebay.com>
2022-12-22 00:01:09 +0800

    [KYUUBI #4018] Execute python code supports asynchronous and query timeout
    ### _Why are the changes needed?_
    
    ### _How was this patch tested?_
    - [x] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [ ] [Run test](https://kyuubi.apache.org/docs/latest/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #4018 from turboFei/python_async.
    
    Closes #4018
    
    2afe3979 [fwang12] move ut
    0d9d2f10 [fwang12] only OK
    46d14f4c [fwang12] add ut
    0e3a0399 [fwang12] add ut
    0f2eba5e [fwang12] add ut
    e2718ab2 [fwang12] async python
    
    Authored-by: fwang12 <fwang12@ebay.com>
    Signed-off-by: Cheng Pan <chengpan@apache.org>

--- externals/kyuubi-spark-sql-engine/src/test/scala/org/apache/kyuubi/engine/spark/operation/PySparkTests.scala
+++ externals/kyuubi-spark-sql-engine/src/test/scala/org/apache/kyuubi/engine/spark/operation/PySparkTests.scala
@@ -19,10 +19,14 @@ package org.apache.kyuubi.engine.spark.operation
 
 import java.io.PrintWriter
 import java.nio.file.Files
+import java.sql.SQLTimeoutException
+import java.util.Properties
 
 import scala.sys.process._
 
 import org.apache.kyuubi.engine.spark.WithSparkSQLEngine
+import org.apache.kyuubi.jdbc.KyuubiHiveDriver
+import org.apache.kyuubi.jdbc.hive.{KyuubiSQLException, KyuubiStatement}
 import org.apache.kyuubi.operation.HiveJDBCTestHelper
 import org.apache.kyuubi.tags.PySparkTest
 
@@ -60,6 +64,30 @@ class PySparkTests extends WithSparkSQLEngine with HiveJDBCTestHelper {
          ||  1|
          |+---+""".stripMargin
     runPySparkTest(code, output)
+  }
+
+  test("executePython support timeout") {
+    val driver = new KyuubiHiveDriver()
+    val connection = driver.connect(getJdbcUrl, new Properties())
+    val statement = connection.createStatement().asInstanceOf[KyuubiStatement]
+    statement.setQueryTimeout(5)
+    try {
+      var code =
+        """
+          |import time
+          |time.sleep(10)
+          |""".stripMargin
+      var e = intercept[SQLTimeoutException] {
+        statement.executePython(code)
+      }.getMessage
+      assert(e.contains("Query timed out"))
+      code = "bad_code"
+      e = intercept[KyuubiSQLException](statement.executePython(code)).getMessage
+      assert(e.contains("Interpret error"))
+    } finally {
+      statement.close()
+      connection.close()
+    }
   }
 
   private def runPySparkTest(
