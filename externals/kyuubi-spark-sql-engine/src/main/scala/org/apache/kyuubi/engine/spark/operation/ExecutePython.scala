26ee6027501cb801d697e1ff980c26afdca4a655
fwang12 <fwang12@ebay.com>
2022-12-26 15:00:51 +0800

    remove not needed

--- externals/kyuubi-spark-sql-engine/src/main/scala/org/apache/kyuubi/engine/spark/operation/ExecutePython.scala
+++ externals/kyuubi-spark-sql-engine/src/main/scala/org/apache/kyuubi/engine/spark/operation/ExecutePython.scala
@@ -37,7 +37,6 @@ import org.apache.spark.sql.types.StructType
 
 import org.apache.kyuubi.{KyuubiSQLException, Logging, Utils}
 import org.apache.kyuubi.config.KyuubiConf.{ENGINE_SPARK_PYTHON_ENV_ARCHIVE, ENGINE_SPARK_PYTHON_ENV_ARCHIVE_EXEC_PATH, ENGINE_SPARK_PYTHON_HOME_ARCHIVE}
-import org.apache.kyuubi.config.KyuubiReservedKeys.{KYUUBI_SESSION_USER_KEY, KYUUBI_STATEMENT_ID_KEY}
 import org.apache.kyuubi.engine.spark.KyuubiSparkUtil._
 import org.apache.kyuubi.operation.{ArrayFetchIterator, OperationState}
 import org.apache.kyuubi.operation.log.OperationLog
@@ -99,7 +98,7 @@ class ExecutePython(
     }
   }
 
-  override protected def runInternal(): Unit = withLocalProperties {
+  override protected def runInternal(): Unit = {
     addTimeoutMonitor(queryTimeout)
     if (shouldRunAsync) {
       val asyncOperation = new Runnable {
@@ -132,33 +131,6 @@ class ExecutePython(
       worker.runCode(s"spark.sparkContext.setLocalProperty('$key', $valueStr)")
       ()
     }
-
-  override protected def withLocalProperties[T](f: => T): T = {
-    try {
-      worker.runCode("spark.sparkContext.setJobGroup" +
-        s"($statementId, $redactedStatement, $forceCancel)")
-      setSparkLocalProperty(KYUUBI_SESSION_USER_KEY, session.user)
-      setSparkLocalProperty(KYUUBI_STATEMENT_ID_KEY, statementId)
-      schedulerPool match {
-        case Some(pool) =>
-          setSparkLocalProperty(SPARK_SCHEDULER_POOL_KEY, pool)
-        case None =>
-      }
-      if (isSessionUserSignEnabled) {
-        setSessionUserSign()
-      }
-
-      f
-    } finally {
-      setSparkLocalProperty(KYUUBI_SESSION_USER_KEY, "")
-      setSparkLocalProperty(KYUUBI_STATEMENT_ID_KEY, "")
-      setSparkLocalProperty(SPARK_SCHEDULER_POOL_KEY, "")
-      worker.runCode("spark.sparkContext.clearJobGroup()")
-      if (isSessionUserSignEnabled) {
-        clearSessionUserSign()
-      }
-    }
-  }
 }
 
 case class SessionPythonWorker(
