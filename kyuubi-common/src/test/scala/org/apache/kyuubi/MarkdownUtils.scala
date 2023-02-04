8250a55f376f1bddba45050b84bc67bdb069aaef
liangbowen <liangbowen@gf.com.cn>
2023-02-04 09:16:54 +0800

    remove licence.md

--- kyuubi-common/src/test/scala/org/apache/kyuubi/MarkdownUtils.scala
+++ kyuubi-common/src/test/scala/org/apache/kyuubi/MarkdownUtils.scala
@@ -18,7 +18,7 @@
 package org.apache.kyuubi
 
 import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Path, Paths, StandardOpenOption}
+import java.nio.file.{Files, Path, StandardOpenOption}
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ArrayBuffer
@@ -100,8 +100,24 @@ class MarkdownBuilder {
    * @return
    */
   def licence(): MarkdownBuilder = {
-    file(Paths.get(getClass.getClassLoader.getResource("licence.md").toURI))
-    this
+    lines("""
+        |<!--
+        |- Licensed to the Apache Software Foundation (ASF) under one or more
+        |- contributor license agreements.  See the NOTICE file distributed with
+        |- this work for additional information regarding copyright ownership.
+        |- The ASF licenses this file to You under the Apache License, Version 2.0
+        |- (the "License"); you may not use this file except in compliance with
+        |- the License.  You may obtain a copy of the License at
+        |-
+        |-   http://www.apache.org/licenses/LICENSE-2.0
+        |-
+        |- Unless required by applicable law or agreed to in writing, software
+        |- distributed under the License is distributed on an "AS IS" BASIS,
+        |- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        |- See the License for the specific language governing permissions and
+        |- limitations under the License.
+        |-->
+        |""")
   }
 
   /**
