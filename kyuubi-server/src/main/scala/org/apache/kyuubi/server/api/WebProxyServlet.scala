407ca22923251fe1f1538bdf8bf521da7d8567e4
hezhao2 <hezhao2@cisco.com>
2023-02-07 16:11:14 +0800

    fix

--- kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/WebProxyServlet.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/WebProxyServlet.scala
@@ -19,13 +19,18 @@ package org.apache.kyuubi.server.api
 
 import javax.servlet.http.HttpServletRequest
 
+import scala.collection.mutable
+
+import org.apache.http.HttpHost
 import org.eclipse.jetty.proxy.ProxyServlet
 
 import org.apache.kyuubi.Logging
 import org.apache.kyuubi.config.KyuubiConf
 
 private[api] class WebProxyServlet(conf: KyuubiConf) extends ProxyServlet with Logging {
-  val ATTR_TARGET_HOST = this.getClass.getSimpleName + ".targetHost";
+  val ATTR_TARGET_HOST = classOf[ProxyServlet].getSimpleName + ".targetHost"
+  val ATTR_TARGET_URI = classOf[ProxyServlet].getSimpleName() + ".targetUri"
+
   override def rewriteTarget(request: HttpServletRequest): String = {
     var targetUrl = "/no-ui-error"
     val requestUrl = request.getRequestURI
@@ -35,15 +40,18 @@ private[api] class WebProxyServlet(conf: KyuubiConf) extends ProxyServlet with L
     if (url != null && url.length > 0) {
       val ipAddress = url(0).split(":")(1)
       val port = url(0).split(":")(2).toInt
-      /*     request.setAttribute(
+      request.setAttribute(
         ATTR_TARGET_HOST,
-        new HttpHost(ipAddress, port, "http"))*/
+        new HttpHost(ipAddress, port, "http"))
       targetUrl =
         String.format(
-          "http://%s:%s/%s/",
+          "http://%s:%s%s",
           ipAddress,
           port.toString,
-          "jobs")
+          new mutable.StringBuilder().append("/jobs/").toString())
+      request.setAttribute(
+        ATTR_TARGET_URI,
+        targetUrl)
       logger.info("ui -> {}", targetUrl)
     }
     targetUrl
