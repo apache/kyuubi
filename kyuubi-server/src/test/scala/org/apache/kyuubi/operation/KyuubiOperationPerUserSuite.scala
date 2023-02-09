29b290a3ebac39c628e87d33182b0f70d0a48a19
Cheng Pan <chengpan@apache.org>
2023-02-09 17:02:26 +0800

    nit

--- kyuubi-server/src/test/scala/org/apache/kyuubi/operation/KyuubiOperationPerUserSuite.scala
+++ kyuubi-server/src/test/scala/org/apache/kyuubi/operation/KyuubiOperationPerUserSuite.scala
@@ -166,28 +166,6 @@ class KyuubiOperationPerUserSuite
     assert(r1 !== r2)
   }
 
-  test("max result rows") {
-    Seq("true", "false").foreach { incremental =>
-      Seq("thrift", "arrow").foreach { resultFormat =>
-        Seq("0", "1").foreach { maxResultRows =>
-          withSessionConf()(Map.empty)(Map(
-            KyuubiConf.OPERATION_RESULT_FORMAT.key -> resultFormat,
-            KyuubiConf.OPERATION_RESULT_MAX_ROWS.key -> maxResultRows,
-            KyuubiConf.OPERATION_INCREMENTAL_COLLECT.key -> incremental)) {
-            withJdbcStatement("va") { statement =>
-              statement.executeQuery("create temporary view va as select * from values(1),(2)")
-              val resultLimit = statement.executeQuery("select * from va")
-              assert(resultLimit.next())
-              // always ignore max result rows on incremental collect mode
-              if (incremental == "true" || maxResultRows == "0") assert(resultLimit.next())
-              assert(!resultLimit.next())
-            }
-          }
-        }
-      }
-    }
-  }
-
   test("support to interrupt the thrift request if remote engine is broken") {
     assume(!httpMode)
     withSessionConf(Map(
@@ -228,6 +206,28 @@ class KyuubiOperationPerUserSuite
         val elapsedTime = System.currentTimeMillis() - startTime
         assert(elapsedTime < 20 * 1000)
         assert(session.client.asyncRequestInterrupted)
+      }
+    }
+  }
+
+  test("max result rows") {
+    Seq("true", "false").foreach { incremental =>
+      Seq("thrift", "arrow").foreach { resultFormat =>
+        Seq("0", "1").foreach { maxResultRows =>
+          withSessionConf()(Map.empty)(Map(
+            KyuubiConf.OPERATION_RESULT_FORMAT.key -> resultFormat,
+            KyuubiConf.OPERATION_RESULT_MAX_ROWS.key -> maxResultRows,
+            KyuubiConf.OPERATION_INCREMENTAL_COLLECT.key -> incremental)) {
+            withJdbcStatement("va") { statement =>
+              statement.executeQuery("create temporary view va as select * from values(1),(2)")
+              val resultLimit = statement.executeQuery("select * from va")
+              assert(resultLimit.next())
+              // always ignore max result rows on incremental collect mode
+              if (incremental == "true" || maxResultRows == "0") assert(resultLimit.next())
+              assert(!resultLimit.next())
+            }
+          }
+        }
       }
     }
   }
