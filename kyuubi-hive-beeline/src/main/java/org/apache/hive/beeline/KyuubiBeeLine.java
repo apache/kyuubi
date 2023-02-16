31483c1dc42fe0621c606d0038e783db3015d76c
senmiaoliu <senmiaoliu@trip.com>
2023-02-16 17:05:18 +0800

    [KYUUBI #4305][Bug] Backport HIVE-15820: comment at the head of beeline -e
    close [#4305](https://github.com/apache/kyuubi/issues/4305)
    
    ```sql
    bin/beeline -u jdbc:hive2://X:10009 -e "
    --asd
    select 1 as a
    "
    ```
    
    ![image](https://user-images.githubusercontent.com/18713676/218910222-b829d447-e5b7-4d80-842b-2ddd4f47a26d.png)
    
    - [ ] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [x] Add screenshots for manual tests if appropriate
    
    - [ ] [Run test](https://kyuubi.readthedocs.io/en/master/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #4333 from lsm1/fix/beeline_comment_header.
    
    Closes #4305
    
    f932d4ead [senmiaoliu] reformat
    9a071fb40 [senmiaoliu] added multi line ut
    425c53631 [senmiaoliu] added ut
    d4dc21a61 [senmiaoliu] comment at the head of beeline -e
    
    Authored-by: senmiaoliu <senmiaoliu@trip.com>
    Signed-off-by: Fu Chen <cfmcgrady@gmail.com>
    (cherry picked from commit c489e29697c73e3eec96aa3ff602e40f9a2c798a)
    Signed-off-by: Fu Chen <cfmcgrady@gmail.com>

--- kyuubi-hive-beeline/src/main/java/org/apache/hive/beeline/KyuubiBeeLine.java
+++ kyuubi-hive-beeline/src/main/java/org/apache/hive/beeline/KyuubiBeeLine.java
@@ -29,6 +29,7 @@ import java.util.List;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.hive.common.util.HiveStringUtils;
 
 public class KyuubiBeeLine extends BeeLine {
   public static final String KYUUBI_BEELINE_DEFAULT_JDBC_DRIVER =
@@ -190,5 +191,10 @@ public class KyuubiBeeLine extends BeeLine {
       }
     }
     return code;
+  }
+
+  @Override
+  boolean dispatch(String line) {
+    return super.dispatch(HiveStringUtils.removeComments(line));
   }
 }
