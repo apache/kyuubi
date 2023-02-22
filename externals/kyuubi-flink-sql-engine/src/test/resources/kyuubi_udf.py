9777510b555683085de6b715631d1540a018041d
Luning Wang <wang4luning@gmail.com>
2023-02-22 11:11:56 +0800

    Install PyFlink in CI

--- externals/kyuubi-flink-sql-engine/src/test/resources/kyuubi_udf.py
+++ externals/kyuubi-flink-sql-engine/src/test/resources/kyuubi_udf.py
@@ -18,6 +18,7 @@
 from pyflink.table import DataTypes
 from pyflink.table.udf import udf
 
+
 @udf(input_types=[DataTypes.STRING()], result_type=DataTypes.STRING())
 def upper_udf(s):
     return s.upper()
