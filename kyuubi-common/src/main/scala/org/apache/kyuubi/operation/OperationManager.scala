14d3ebd80ad90ebbd3c747d10bfbdfb4d0797429
Tianlin Liao <tiliao@ebay.com>
2023-03-03 14:35:32 +0800

    [KYUUBI #4439] add list/close operation method for AdminResouce

--- kyuubi-common/src/main/scala/org/apache/kyuubi/operation/OperationManager.scala
+++ kyuubi-common/src/main/scala/org/apache/kyuubi/operation/OperationManager.scala
@@ -17,6 +17,8 @@
 
 package org.apache.kyuubi.operation
 
+import scala.collection.JavaConverters._
+
 import org.apache.hive.service.rpc.thrift._
 
 import org.apache.kyuubi.KyuubiSQLException
@@ -40,6 +42,8 @@ abstract class OperationManager(name: String) extends AbstractService(name) {
   protected def skipOperationLog: Boolean = false
 
   def getOperationCount: Int = handleToOperation.size()
+
+  def allOperations(): Iterable[Operation] = handleToOperation.values().asScala
 
   override def initialize(conf: KyuubiConf): Unit = {
     LogDivertAppender.initialize(skipOperationLog)
