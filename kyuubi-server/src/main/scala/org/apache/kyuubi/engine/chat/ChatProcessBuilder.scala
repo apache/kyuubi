28bd9d9fa3c3a0d29f40c145eda7b7c5fcb236fd
Cheng Pan <chengpan@apache.org>
2023-03-20 03:33:26 +0800

    [CHAT] Pretty chat engine launch command

--- kyuubi-server/src/main/scala/org/apache/kyuubi/engine/chat/ChatProcessBuilder.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/engine/chat/ChatProcessBuilder.scala
@@ -106,10 +106,11 @@ class ChatProcessBuilder(
       super.toString()
     } else {
       Utils.redactCommandLineArgs(conf, commands).map {
+        case arg if arg.startsWith("--") => s"\\\n\t$arg"
         case arg if arg.contains(ENGINE_CHAT_GPT_API_KEY.key) =>
           s"${ENGINE_CHAT_GPT_API_KEY.key}=$REDACTION_REPLACEMENT_TEXT"
         case arg => arg
-      }.mkString("\n")
+      }.mkString(" ")
     }
   }
 }
