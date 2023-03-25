8bdb672c40b4c1a8c8583dd97213f75727df2e36
Paul Lin <paullin3280@gmail.com>
2023-03-25 21:10:04 +0800

    [KYUUBI #1652] Explicitly load Kyuubi conf on Flink engine start

--- externals/kyuubi-flink-sql-engine/src/main/scala/org/apache/kyuubi/engine/flink/FlinkSQLEngine.scala
+++ externals/kyuubi-flink-sql-engine/src/main/scala/org/apache/kyuubi/engine/flink/FlinkSQLEngine.scala
@@ -75,6 +75,7 @@ object FlinkSQLEngine extends Logging {
     FlinkEngineUtils.checkFlinkVersion()
 
     try {
+      kyuubiConf.loadFileDefaults()
       Utils.fromCommandLineArgs(args, kyuubiConf)
       val flinkConfDir = sys.env.getOrElse(
         "FLINK_CONF_DIR", {
