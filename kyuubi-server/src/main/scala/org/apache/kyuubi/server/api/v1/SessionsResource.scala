7db90d5706b6ada92bfb6023c43f37cd1d00f661
Tianlin Liao <tiliao@ebay.com>
2023-03-26 18:16:01 +0800

    [KYUUBI #4611] support all sessions api in SessionRestApi

--- kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/v1/SessionsResource.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/v1/SessionsResource.scala
@@ -67,10 +67,28 @@ private[v1] class SessionsResource extends ApiRequestContext with Logging {
     description = "get a session event via session handle identifier")
   @GET
   @Path("{sessionHandle}")
-  def sessionInfo(@PathParam("sessionHandle") sessionHandleStr: String): KyuubiEvent = {
+  def sessionInfo(@PathParam("sessionHandle") sessionHandleStr: String): dto.KyuubiEvent = {
     try {
       sessionManager.getSession(sessionHandleStr)
-        .asInstanceOf[KyuubiSession].getSessionEvent.get
+        .asInstanceOf[KyuubiSession].getSessionEvent.map(event =>
+          dto.KyuubiSessionEvent.builder
+            .sessionId(event.sessionId)
+            .clientVersion(event.clientVersion)
+            .sessionType(event.sessionType)
+            .sessionName(event.sessionName)
+            .user(event.user)
+            .clientIp(event.clientIP)
+            .serverIp(event.serverIP)
+            .conf(event.conf.asJava)
+            .remoteSessionId(event.remoteSessionId)
+            .engineId(event.engineId)
+            .eventTime(event.eventTime)
+            .openedTime(event.openedTime)
+            .startTime(event.startTime)
+            .endTime(event.endTime)
+            .totalOperations(event.totalOperations)
+            .exception(event.exception.getOrElse(null))
+            .build).get
     } catch {
       case NonFatal(e) =>
         error(s"Invalid $sessionHandleStr", e)
