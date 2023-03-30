eb49537ea0c3d11234d5f8e93f59d65176d7f02c
fwang12 <fwang12@ebay.com>
2023-03-30 18:28:28 +0800

    conf overlay

--- kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/v1/SessionsResource.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/v1/SessionsResource.scala
@@ -175,7 +175,7 @@ private[v1] class SessionsResource extends ApiRequestContext with Logging {
       fe.be.executeStatement(
         sessionHandleStr,
         request.getStatement,
-        Map.empty,
+        request.getConfOverlay.asScala,
         request.isRunAsync,
         request.getQueryTimeout)
     } catch {
