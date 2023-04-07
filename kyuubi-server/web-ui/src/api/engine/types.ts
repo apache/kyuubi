da61ea2fed167586470551a95140a3035ee39e3c
zwangsheng <2213335496@qq.com>
2023-04-07 14:10:55 +0800

    [KYUUBI #3654][UI] Engine Manager Page

--- kyuubi-server/web-ui/src/api/engine/types.ts
+++ kyuubi-server/web-ui/src/api/engine/types.ts
@@ -15,41 +15,11 @@
  * limitations under the License.
  */
 
-import { createRouter, createWebHistory } from 'vue-router'
-import overviewRoutes from './overview'
-import workloadRoutes from './workload'
-import operationRoutes from './operation'
-import contactRoutes from './contact'
-import sessionRoutes from './session'
-import engineRoutes from './engine'
+interface IEngineSearch {
+  type: null | string
+  sharelevel: null | string
+  'hive.server2.proxy.user': null | string
+  subdomain?: null | string
+}
 
-const routes = [
-  {
-    path: '/',
-    name: 'main',
-    redirect: {
-      name: 'layout'
-    }
-  },
-  {
-    path: '/layout',
-    name: 'layout',
-    component: () => import('@/views/layout/index.vue'),
-    redirect: 'overview',
-    children: [
-      ...overviewRoutes,
-      ...sessionRoutes,
-      ...workloadRoutes,
-      ...operationRoutes,
-      ...engineRoutes,
-      ...contactRoutes
-    ]
-  }
-]
-
-const router = createRouter({
-  history: createWebHistory('/ui'),
-  routes
-})
-
-export default router
+export { IEngineSearch }
--- kyuubi-server/web-ui/src/router/engine/index.ts
+++ kyuubi-server/web-ui/src/router/engine/index.ts
@@ -15,41 +15,12 @@
  * limitations under the License.
  */
 
-import { createRouter, createWebHistory } from 'vue-router'
-import overviewRoutes from './overview'
-import workloadRoutes from './workload'
-import operationRoutes from './operation'
-import contactRoutes from './contact'
-import sessionRoutes from './session'
-import engineRoutes from './engine'
-
 const routes = [
   {
-    path: '/',
-    name: 'main',
-    redirect: {
-      name: 'layout'
-    }
-  },
-  {
-    path: '/layout',
-    name: 'layout',
-    component: () => import('@/views/layout/index.vue'),
-    redirect: 'overview',
-    children: [
-      ...overviewRoutes,
-      ...sessionRoutes,
-      ...workloadRoutes,
-      ...operationRoutes,
-      ...engineRoutes,
-      ...contactRoutes
-    ]
+    path: '/engine/engine-statistics',
+    name: 'engine-statistics',
+    component: () => import('@/views/engine/index.vue')
   }
 ]
 
-const router = createRouter({
-  history: createWebHistory('/ui'),
-  routes
-})
-
-export default router
+export default routes
