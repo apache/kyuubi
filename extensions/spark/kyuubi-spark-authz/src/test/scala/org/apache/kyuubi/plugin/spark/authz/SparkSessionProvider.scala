2ed0990b7304ca5986ab116e02f7508e3d732549
liangbowen <liangbowen@gf.com.cn>
2023-04-19 18:06:06 +0800

    [KYUUBI #4676] [AUTHZ] Reuse users and namespaces in both tests and policy file generation
    ### _Why are the changes needed?_
    
    - align the same list of users and namespaces used in tests and in policy file generation, as users and namespaces are the most important elements of Ranger policy's conditions and resources.
    - help to improve and simplify the decision in Authz testing and make a clear view of what's exactly tested and authorized, and very handy and easy to see the usage link in IDE
    - reduce possible abuse and untracable uses of authorized and unauthorized users, rules, resources. (We have up to 4 unauthorized users in separated tests!)
    
    ### _How was this patch tested?_
    - [ ] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [x] [Run test](https://kyuubi.readthedocs.io/en/master/develop_tools/testing.html#running-tests) locally before make a pull request
    
    Closes #4676 from bowenliang123/authz-gen-common.
    
    Closes #4676
    
    dc535a4d8 [liangbowen] authz-gen-common
    
    Authored-by: liangbowen <liangbowen@gf.com.cn>
    Signed-off-by: liangbowen <liangbowen@gf.com.cn>

--- extensions/spark/kyuubi-spark-authz/src/test/scala/org/apache/kyuubi/plugin/spark/authz/SparkSessionProvider.scala
+++ extensions/spark/kyuubi-spark-authz/src/test/scala/org/apache/kyuubi/plugin/spark/authz/SparkSessionProvider.scala
@@ -26,6 +26,7 @@ import org.apache.spark.sql.{DataFrame, Row, SparkSession, SparkSessionExtension
 import org.scalatest.Assertions.convertToEqualizer
 
 import org.apache.kyuubi.Utils
+import org.apache.kyuubi.plugin.spark.authz.RangerTestUsers._
 import org.apache.kyuubi.plugin.spark.authz.util.AuthZUtils._
 
 trait SparkSessionProvider {
@@ -39,7 +40,6 @@ trait SparkSessionProvider {
   protected val extension: SparkSessionExtensions => Unit = _ => Unit
   protected val sqlExtensions: String = ""
 
-  protected val defaultTableOwner = "default_table_owner"
   protected val extraSparkConf: SparkConf = new SparkConf()
 
   protected lazy val spark: SparkSession = {
@@ -83,12 +83,12 @@ trait SparkSessionProvider {
       f
     } finally {
       res.foreach {
-        case (t, "table") => doAs("admin", sql(s"DROP TABLE IF EXISTS $t"))
-        case (db, "database") => doAs("admin", sql(s"DROP DATABASE IF EXISTS $db"))
-        case (fn, "function") => doAs("admin", sql(s"DROP FUNCTION IF EXISTS $fn"))
-        case (view, "view") => doAs("admin", sql(s"DROP VIEW IF EXISTS $view"))
+        case (t, "table") => doAs(admin, sql(s"DROP TABLE IF EXISTS $t"))
+        case (db, "database") => doAs(admin, sql(s"DROP DATABASE IF EXISTS $db"))
+        case (fn, "function") => doAs(admin, sql(s"DROP FUNCTION IF EXISTS $fn"))
+        case (view, "view") => doAs(admin, sql(s"DROP VIEW IF EXISTS $view"))
         case (cacheTable, "cache") => if (isSparkV32OrGreater) {
-            doAs("admin", sql(s"UNCACHE TABLE IF EXISTS $cacheTable"))
+            doAs(admin, sql(s"UNCACHE TABLE IF EXISTS $cacheTable"))
           }
         case (_, e) =>
           throw new RuntimeException(s"the resource whose resource type is $e cannot be cleared")
--- extensions/spark/kyuubi-spark-authz/src/test/scala/org/apache/kyuubi/plugin/spark/authz/ranger/IcebergCatalogRangerSparkExtensionSuite.scala
+++ extensions/spark/kyuubi-spark-authz/src/test/scala/org/apache/kyuubi/plugin/spark/authz/ranger/IcebergCatalogRangerSparkExtensionSuite.scala
@@ -23,6 +23,8 @@ import org.scalatest.Outcome
 
 import org.apache.kyuubi.Utils
 import org.apache.kyuubi.plugin.spark.authz.AccessControlException
+import org.apache.kyuubi.plugin.spark.authz.RangerTestNamespace._
+import org.apache.kyuubi.plugin.spark.authz.RangerTestUsers._
 
 /**
  * Tests for RangerSparkExtensionSuite
@@ -36,7 +38,7 @@ class IcebergCatalogRangerSparkExtensionSuite extends RangerSparkExtensionSuite 
     else ""
 
   val catalogV2 = "local"
-  val namespace1 = "iceberg_ns"
+  val namespace1 = icebergNamespace
   val table1 = "table1"
   val outputTable1 = "outputTable1"
 
@@ -57,18 +59,18 @@ class IcebergCatalogRangerSparkExtensionSuite extends RangerSparkExtensionSuite 
 
       super.beforeAll()
 
-      doAs("admin", sql(s"CREATE DATABASE IF NOT EXISTS $catalogV2.$namespace1"))
+      doAs(admin, sql(s"CREATE DATABASE IF NOT EXISTS $catalogV2.$namespace1"))
       doAs(
-        "admin",
+        admin,
         sql(s"CREATE TABLE IF NOT EXISTS $catalogV2.$namespace1.$table1" +
           " (id int, name string, city string) USING iceberg"))
 
       doAs(
-        "admin",
+        admin,
         sql(s"INSERT INTO $catalogV2.$namespace1.$table1" +
           " (id , name , city ) VALUES (1, 'liangbowen','Guangzhou')"))
       doAs(
-        "admin",
+        admin,
         sql(s"CREATE TABLE IF NOT EXISTS $catalogV2.$namespace1.$outputTable1" +
           " (id int, name string, city string) USING iceberg"))
     }
@@ -93,7 +95,7 @@ class IcebergCatalogRangerSparkExtensionSuite extends RangerSparkExtensionSuite 
     // MergeIntoTable:  Using a MERGE INTO Statement
     val e1 = intercept[AccessControlException](
       doAs(
-        "someone",
+        someone,
         sql(mergeIntoSql)))
     assert(e1.getMessage.contains(s"does not have [select] privilege" +
       s" on [$namespace1/$table1/id]"))
@@ -104,7 +106,7 @@ class IcebergCatalogRangerSparkExtensionSuite extends RangerSparkExtensionSuite 
         true)
       val e2 = intercept[AccessControlException](
         doAs(
-          "someone",
+          someone,
           sql(mergeIntoSql)))
       assert(e2.getMessage.contains(s"does not have" +
         s" [select] privilege" +
@@ -116,21 +118,21 @@ class IcebergCatalogRangerSparkExtensionSuite extends RangerSparkExtensionSuite 
         false)
     }
 
-    doAs("admin", sql(mergeIntoSql))
+    doAs(admin, sql(mergeIntoSql))
   }
 
   test("[KYUUBI #3515] UPDATE TABLE") {
     // UpdateTable
     val e1 = intercept[AccessControlException](
       doAs(
-        "someone",
+        someone,
         sql(s"UPDATE $catalogV2.$namespace1.$table1 SET city='Guangzhou' " +
           " WHERE id=1")))
     assert(e1.getMessage.contains(s"does not have [update] privilege" +
       s" on [$namespace1/$table1]"))
 
     doAs(
-      "admin",
+      admin,
       sql(s"UPDATE $catalogV2.$namespace1.$table1 SET city='Guangzhou' " +
         " WHERE id=1"))
   }
@@ -138,11 +140,11 @@ class IcebergCatalogRangerSparkExtensionSuite extends RangerSparkExtensionSuite 
   test("[KYUUBI #3515] DELETE FROM TABLE") {
     // DeleteFromTable
     val e6 = intercept[AccessControlException](
-      doAs("someone", sql(s"DELETE FROM $catalogV2.$namespace1.$table1 WHERE id=2")))
+      doAs(someone, sql(s"DELETE FROM $catalogV2.$namespace1.$table1 WHERE id=2")))
     assert(e6.getMessage.contains(s"does not have [update] privilege" +
       s" on [$namespace1/$table1]"))
 
-    doAs("admin", sql(s"DELETE FROM $catalogV2.$namespace1.$table1 WHERE id=2"))
+    doAs(admin, sql(s"DELETE FROM $catalogV2.$namespace1.$table1 WHERE id=2"))
   }
 
   test("[KYUUBI #3666] Support {OWNER} variable for queries run on CatalogV2") {
@@ -163,7 +165,7 @@ class IcebergCatalogRangerSparkExtensionSuite extends RangerSparkExtensionSuite 
         }.isSuccess))
 
       doAs(
-        "create_only_user", {
+        createOnlyUser, {
           val e = intercept[AccessControlException](sql(select).collect())
           assert(e.getMessage === errorMessage("select", s"$namespace1/$table/key"))
         })
@@ -178,17 +180,17 @@ class IcebergCatalogRangerSparkExtensionSuite extends RangerSparkExtensionSuite 
       (s"$catalogV2.default.src", "table"),
       (s"$catalogV2.default.outputTable2", "table"))) {
       doAs(
-        "admin",
+        admin,
         sql(s"CREATE TABLE IF NOT EXISTS $catalogV2.default.src" +
           " (id int, name string, key string) USING iceberg"))
       doAs(
-        "admin",
+        admin,
         sql(s"INSERT INTO $catalogV2.default.src" +
           " (id , name , key ) VALUES " +
           "(1, 'liangbowen1','10')" +
           ", (2, 'liangbowen2','20')"))
       doAs(
-        "admin",
+        admin,
         sql(s"CREATE TABLE IF NOT EXISTS $catalogV2.$namespace1.$outputTable2" +
           " (id int, name string, key string) USING iceberg"))
 
@@ -200,20 +202,20 @@ class IcebergCatalogRangerSparkExtensionSuite extends RangerSparkExtensionSuite 
            |WHEN NOT MATCHED THEN INSERT (id, name, key) VALUES (source.id, source.name, source.key)
         """.stripMargin
 
-      doAs("admin", sql(mergeIntoSql))
+      doAs(admin, sql(mergeIntoSql))
       doAs(
-        "admin", {
+        admin, {
           val countOutputTable =
             sql(s"select count(1) from $catalogV2.$namespace1.$outputTable2").collect()
           val rowCount = countOutputTable(0).get(0)
           assert(rowCount === 2)
         })
-      doAs("admin", sql(s"truncate table $catalogV2.$namespace1.$outputTable2"))
+      doAs(admin, sql(s"truncate table $catalogV2.$namespace1.$outputTable2"))
 
       // source table with row filter `key`<20
-      doAs("bob", sql(mergeIntoSql))
+      doAs(bob, sql(mergeIntoSql))
       doAs(
-        "admin", {
+        admin, {
           val countOutputTable =
             sql(s"select count(1) from $catalogV2.$namespace1.$outputTable2").collect()
           val rowCount = countOutputTable(0).get(0)
@@ -224,7 +226,7 @@ class IcebergCatalogRangerSparkExtensionSuite extends RangerSparkExtensionSuite 
 
   test("[KYUUBI #4255] DESCRIBE TABLE") {
     val e1 = intercept[AccessControlException](
-      doAs("someone", sql(s"DESCRIBE TABLE $catalogV2.$namespace1.$table1").explain()))
+      doAs(someone, sql(s"DESCRIBE TABLE $catalogV2.$namespace1.$table1").explain()))
     assert(e1.getMessage.contains(s"does not have [select] privilege" +
       s" on [$namespace1/$table1]"))
   }
