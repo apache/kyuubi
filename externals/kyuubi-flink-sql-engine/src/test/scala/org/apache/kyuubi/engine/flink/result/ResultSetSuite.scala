5f9976d81810058d48abb211a5b0fc43a60b7ba1
Paul Lin <paullin3280@gmail.com>
2023-04-22 16:42:25 +0800

    [KYUUBI #4745] Support Flink's LocalZonedTimestamp DataType

--- externals/kyuubi-flink-sql-engine/src/test/scala/org/apache/kyuubi/engine/flink/result/ResultSetSuite.scala
+++ externals/kyuubi-flink-sql-engine/src/test/scala/org/apache/kyuubi/engine/flink/result/ResultSetSuite.scala
@@ -17,6 +17,8 @@
 
 package org.apache.kyuubi.engine.flink.result
 
+import java.time.ZoneId
+
 import org.apache.flink.table.api.{DataTypes, ResultKind}
 import org.apache.flink.table.catalog.Column
 import org.apache.flink.table.data.StringData
@@ -44,9 +46,9 @@ class ResultSetSuite extends KyuubiFunSuite {
       .data(rowsNew)
       .build
 
-    assert(RowSet.toRowBaseSet(rowsNew, resultSetNew)
-      === RowSet.toRowBaseSet(rowsOld, resultSetOld))
-    assert(RowSet.toColumnBasedSet(rowsNew, resultSetNew)
-      === RowSet.toColumnBasedSet(rowsOld, resultSetOld))
+    assert(RowSet.toRowBaseSet(rowsNew, resultSetNew, ZoneId.systemDefault())
+      === RowSet.toRowBaseSet(rowsOld, resultSetOld, ZoneId.systemDefault()))
+    assert(RowSet.toColumnBasedSet(rowsNew, resultSetNew, ZoneId.systemDefault())
+      === RowSet.toColumnBasedSet(rowsOld, resultSetOld, ZoneId.systemDefault()))
   }
 }
