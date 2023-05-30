ee0c0ad88a863720807239424cefc038764756e3
liangbowen <liangbowen@gf.com.cn>
2023-05-30 17:51:28 +0800

    Move AssertionUtils to kyuubi-util-scala module

--- kyuubi-util-scala/src/test/java/org/apache/kyuubi/util/AssertionUtils.scala
+++ kyuubi-util-scala/src/test/java/org/apache/kyuubi/util/AssertionUtils.scala
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.kyuubi.plugin.spark.authz.util
+package org.apache.kyuubi.util
 
 import java.util.Locale
 
