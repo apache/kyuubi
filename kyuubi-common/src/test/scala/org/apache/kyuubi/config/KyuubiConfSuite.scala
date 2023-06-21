6b9520bfd1d34f786126ec008234bd2444d0ad74
fwang12 <fwang12@ebay.com>
2023-06-21 16:29:52 +0800

    save

--- kyuubi-common/src/test/scala/org/apache/kyuubi/config/KyuubiConfSuite.scala
+++ kyuubi-common/src/test/scala/org/apache/kyuubi/config/KyuubiConfSuite.scala
@@ -212,13 +212,13 @@ class KyuubiConfSuite extends KyuubiFunSuite {
       "/var/run/secrets/kubernetes.io/token.ns2")
 
     val kubernetesConf1 = kyuubiConf.getKubernetesConf(Some("28"), Some("ns1"))
-    assert(kubernetesConf1.get(KyuubiConf.KUBERNETES_MASTER) == "k8s://master")
+    assert(kubernetesConf1.get(KyuubiConf.KUBERNETES_MASTER) == Some("k8s://master"))
     assert(kubernetesConf1.get(KyuubiConf.KUBERNETES_AUTHENTICATE_OAUTH_TOKEN_FILE) ==
-      "/var/run/secrets/kubernetes.io/token.ns1")
+      Some("/var/run/secrets/kubernetes.io/token.ns1"))
 
     val kubernetesConf2 = kyuubiConf.getKubernetesConf(Some("28"), Some("ns2"))
-    assert(kubernetesConf2.get(KyuubiConf.KUBERNETES_MASTER) == "k8s://master")
+    assert(kubernetesConf2.get(KyuubiConf.KUBERNETES_MASTER) == Some("k8s://master"))
     assert(kubernetesConf2.get(KyuubiConf.KUBERNETES_AUTHENTICATE_OAUTH_TOKEN_FILE) ==
-      "/var/run/secrets/kubernetes.io/token.ns2")
+      Some("/var/run/secrets/kubernetes.io/token.ns2"))
   }
 }
