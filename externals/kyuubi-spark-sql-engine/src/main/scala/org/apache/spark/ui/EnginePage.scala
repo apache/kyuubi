76daa01681f9ecc04b8e8fcdd3c31ea3dfa56d77
liangbowen <liangbowen@gf.com.cn>
2023-07-07 18:05:05 +0800

    [KYUUBI #5019] Shows details of compilation info in SparkUI's engine tab
    ### _Why are the changes needed?_
    
    Adding compilation details info in SparkUI's Engine tab
    - shows details of kyuubi version, including revision, revision time and branch
    - show compilation version of Spark, Scala, Hadoop and  Hive
    
    ![image](https://github.com/apache/kyuubi/assets/1935105/2d58d637-5eef-47b3-94d6-eef923cbc632)
    
    ### _How was this patch tested?_
    - [ ] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [x] Add screenshots for manual tests if appropriate
    
    - [ ] [Run test](https://kyuubi.readthedocs.io/en/master/contributing/code/testing.html#running-tests) locally before make a pull request
    
    Closes #5019 from bowenliang123/enginetab-info.
    
    Closes #5019
    
    3ea108bd6 [liangbowen] shows Compilation Info in SparkUI's Engine tab
    
    Authored-by: liangbowen <liangbowen@gf.com.cn>
    Signed-off-by: liangbowen <liangbowen@gf.com.cn>

--- externals/kyuubi-spark-sql-engine/src/main/scala/org/apache/spark/ui/EnginePage.scala
+++ externals/kyuubi-spark-sql-engine/src/main/scala/org/apache/spark/ui/EnginePage.scala
@@ -29,7 +29,7 @@ import org.apache.commons.text.StringEscapeUtils
 import org.apache.spark.ui.TableSourceUtil._
 import org.apache.spark.ui.UIUtils._
 
-import org.apache.kyuubi.{KYUUBI_VERSION, Utils}
+import org.apache.kyuubi._
 import org.apache.kyuubi.engine.spark.events.{SessionEvent, SparkOperationEvent}
 
 case class EnginePage(parent: EngineTab) extends WebUIPage("") {
@@ -57,6 +57,15 @@ case class EnginePage(parent: EngineTab) extends WebUIPage("") {
       <li>
         <strong>Kyuubi Version: </strong>
         {KYUUBI_VERSION}
+      </li>
+      <li>
+        <strong>Compilation Revision:</strong>
+        {REVISION.substring(0, 7)} ({REVISION_TIME}), branch {BRANCH}
+      </li>
+      <li>
+        <strong>Compilation with:</strong>
+        Spark {SPARK_COMPILE_VERSION}, Scala {SCALA_COMPILE_VERSION},
+          Hadoop {HADOOP_COMPILE_VERSION}, Hive {HIVE_COMPILE_VERSION}
       </li>
       <li>
         <strong>Started at: </strong>
