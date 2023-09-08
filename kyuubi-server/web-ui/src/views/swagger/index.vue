e562520364b8acb589b48c852321a8fefeec9831
zwangsheng <binjieyang@apache.org>
2023-09-08 16:29:57 +0800

    Try

--- kyuubi-server/web-ui/src/views/swagger/index.vue
+++ kyuubi-server/web-ui/src/views/swagger/index.vue
@@ -1,7 +1,43 @@
 <template>
-  <main> Swagger </main>
+  <main>
+    Swagger
+    <div id="swagger-ui" class="swagger-ui"> </div>
+  </main>
 </template>
 <script lang="ts">
-  export default {}
+  import { SwaggerUIBundle, SwaggerUIStandalonePreset } from 'swagger-ui-dist'
+  export default {
+    name: 'ImportSwagger',
+    data() {
+      return {}
+    },
+    created() {},
+    mounted() {
+      SwaggerUIBundle({
+        url: '/api/openapi.json',
+        dom_id: '#swagger-ui',
+        presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+        layout: 'StandaloneLayout'
+      })
+    }
+  }
 </script>
-<style lang=""></style>
+<style scoped>
+  @import 'swagger-ui-dist/swagger-ui.css';
+  html {
+    box-sizing: border-box;
+    overflow: -moz-scrollbars-vertical;
+    overflow-y: scroll;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    margin: 0;
+    background: #fafafa;
+  }
+</style>
