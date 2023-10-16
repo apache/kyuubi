20415dc441668beddcd5ec4f86bc957c65aeb0bb
senmiaoliu <senmiaoliu@trip.com>
2023-10-16 10:55:07 +0800

    use com.mysql.cj.jdbc.Driver first

--- kyuubi-server/src/main/scala/org/apache/kyuubi/server/metadata/jdbc/JDBCMetadataStore.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/server/metadata/jdbc/JDBCMetadataStore.scala
@@ -46,10 +46,17 @@ class JDBCMetadataStore(conf: KyuubiConf) extends MetadataStore with Logging {
 
   private val dbType = DatabaseType.withName(conf.get(METADATA_STORE_JDBC_DATABASE_TYPE))
   private val driverClassOpt = conf.get(METADATA_STORE_JDBC_DRIVER)
+  private lazy val mysqlDriverClass =
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver", false, Thread.currentThread.getContextClassLoader)
+      "com.mysql.cj.jdbc.Driver"
+    } catch {
+      case _: ClassNotFoundException => "com.mysql.jdbc.Driver"
+    }
   private val driverClass = dbType match {
     case SQLITE => driverClassOpt.getOrElse("org.sqlite.JDBC")
     case DERBY => driverClassOpt.getOrElse("org.apache.derby.jdbc.AutoloadedDriver")
-    case MYSQL => driverClassOpt.getOrElse("com.mysql.jdbc.Driver")
+    case MYSQL => driverClassOpt.getOrElse(mysqlDriverClass)
     case CUSTOM => driverClassOpt.getOrElse(
         throw new IllegalArgumentException("No jdbc driver defined"))
   }
