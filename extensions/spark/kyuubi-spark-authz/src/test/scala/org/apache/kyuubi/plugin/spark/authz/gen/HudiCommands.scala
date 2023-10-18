4319763df018e68f1330d61ff1e04adb39893ce3
Angerszhuuuu <angers.zhu@gmail.com>
2023-10-18 15:03:29 +0800

    [KYUUBI #5457] [AUTHZ] Support RepairTable Commands for Hudi

--- extensions/spark/kyuubi-spark-authz/src/test/scala/org/apache/kyuubi/plugin/spark/authz/gen/HudiCommands.scala
+++ extensions/spark/kyuubi-spark-authz/src/test/scala/org/apache/kyuubi/plugin/spark/authz/gen/HudiCommands.scala
@@ -116,6 +116,11 @@ object HudiCommands {
       DROPTABLE)
   }
 
+  val RepairHoodieTableCommand = {
+    val cmd = "org.apache.spark.sql.hudi.command.RepairHoodieTableCommand"
+    TableCommandSpec(cmd, Seq(TableDesc("tableName", classOf[TableIdentifierTableExtractor])), MSCK)
+  }
+
   val TruncateHoodieTableCommand = {
     val cmd = "org.apache.spark.sql.hudi.command.TruncateHoodieTableCommand"
     val columnDesc = ColumnDesc("partitionSpec", classOf[PartitionOptionColumnExtractor])
@@ -138,5 +143,6 @@ object HudiCommands {
     CreateHoodieTableAsSelectCommand,
     CreateHoodieTableLikeCommand,
     DropHoodieTableCommand,
+    RepairHoodieTableCommand,
     TruncateHoodieTableCommand)
 }
