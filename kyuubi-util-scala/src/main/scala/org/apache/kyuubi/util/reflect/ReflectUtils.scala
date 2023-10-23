78a66a33c85118e536bfae288a402eb2c68a9e75
Kent Yao <yao@apache.org>
2023-10-23 13:15:17 +0800

    [AuthZ] Simplify debug message for missing field/methond in ReflectUtils

--- kyuubi-util-scala/src/main/scala/org/apache/kyuubi/util/reflect/ReflectUtils.scala
+++ kyuubi-util-scala/src/main/scala/org/apache/kyuubi/util/reflect/ReflectUtils.scala
@@ -60,11 +60,7 @@ object ReflectUtils {
       }
     } catch {
       case e: Exception =>
-        val candidates =
-          (clz.getDeclaredFields ++ clz.getFields).map(_.getName).distinct.sorted
-        throw new RuntimeException(
-          s"Field $fieldName not in $clz [${candidates.mkString(",")}]",
-          e)
+        throw new RuntimeException(s"$clz does not have $fieldName field", e)
     }
   }
 
@@ -92,15 +88,8 @@ object ReflectUtils {
       }
     } catch {
       case e: Exception =>
-        val candidates =
-          (clz.getDeclaredMethods ++ clz.getMethods)
-            .map(m => s"${m.getName}(${m.getParameterTypes.map(_.getName).mkString(", ")})")
-            .distinct.sorted
-        val argClassesNames = argClasses.map(_.getName)
         throw new RuntimeException(
-          s"Method $methodName(${argClassesNames.mkString(", ")})" +
-            s" not found in $clz [${candidates.mkString(", ")}]",
-          e)
+          s"$clz does not have $methodName method or incorrect arguments", e)
     }
   }
 
