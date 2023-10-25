70edf9f475c58f599ea9e20adecd6ad8180c6b57
labbomb <739955946@qq.com>
2023-10-25 20:00:09 +0800

    fix: fix some errors

--- kyuubi-server/web-ui/src/components/monaco-editor/index.vue
+++ kyuubi-server/web-ui/src/components/monaco-editor/index.vue
@@ -24,9 +24,6 @@
   import * as monaco from 'monaco-editor'
   import { format } from 'sql-formatter'
   import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-  import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-  import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-  import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
   import EditorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
   import { editorProps } from './type'
   import { useEditorStore } from '@/pinia/editor'
@@ -37,15 +34,6 @@
     getWorker(_: string, label: string) {
       if (label === 'json') {
         return new jsonWorker()
-      }
-      if (['css', 'scss', 'less'].includes(label)) {
-        return new cssWorker()
-      }
-      if (['html', 'handlebars', 'razor'].includes(label)) {
-        return new htmlWorker()
-      }
-      if (['typescript', 'javascript'].includes(label)) {
-        return new tsWorker()
       }
       return new EditorWorker()
     }
