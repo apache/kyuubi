cfd90e0e2f6762467dc5218e4f3dceb2d854f961
zml1206 <zhuml1206@gmail.com>
2023-10-27 16:39:48 +0800

    [KYUUBI #5529][AUTHZ] Support create table command for Delta Lake
    ### _Why are the changes needed?_
    To close #5529.
    Support create table command for Delta Lake in Authz.
    https://docs.delta.io/latest/delta-batch.html#create-a-table
    
    ### _How was this patch tested?_
    - [ ] Add some test cases that check the changes thoroughly including negative and positive cases if possible
    
    - [ ] Add screenshots for manual tests if appropriate
    
    - [x] [Run test](https://kyuubi.readthedocs.io/en/master/contributing/code/testing.html#running-tests) locally before make a pull request
    
    ### _Was this patch authored or co-authored using generative AI tooling?_
    No.
    
    Closes #5530 from zml1206/KYUUBI-5529.
    
    Closes #5529
    
    b8ed2a464 [zml1206] update
    f12ff8ff5 [zml1206] Merge branch 'master' into KYUUBI-5529
    c02523062 [Bowen Liang] resolve conflicts
    036154b04 [Bowen Liang] Merge branch 'master' into KYUUBI-5529
    fb4223b2c [zml1206] fix check spec json files
    10138d83a [zml1206] Merge branch 'master' into KYUUBI-5529
    facd8f7c6 [zml1206] fix show databases ut
    b2a9543ec [zml1206] update ut
    23f6b8172 [zml1206] Support create table command for Delta Lake
    
    Lead-authored-by: zml1206 <zhuml1206@gmail.com>
    Co-authored-by: Bowen Liang <bowenliang@apache.org>
    Co-authored-by: Bowen Liang <liangbowen@gf.com.cn>
    Signed-off-by: Fu Chen <cfmcgrady@gmail.com>

---
+++ extensions/spark/kyuubi-spark-authz/src/test/scala/org/apache/kyuubi/plugin/spark/authz/gen/DeltaCommands.scala
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.plugin.spark.authz.gen
+
+import org.apache.kyuubi.plugin.spark.authz.OperationType._
+import org.apache.kyuubi.plugin.spark.authz.serde._
+
+object DeltaCommands extends CommandSpecs[TableCommandSpec] {
+
+  val CreateDeltaTableCommand = {
+    val cmd = "org.apache.spark.sql.delta.commands.CreateDeltaTableCommand"
+    val tableDesc = TableDesc("table", classOf[CatalogTableTableExtractor])
+    TableCommandSpec(cmd, Seq(tableDesc), CREATETABLE)
+  }
+
+  override def specs: Seq[TableCommandSpec] = Seq(
+    CreateDeltaTableCommand)
+}
