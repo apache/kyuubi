dbeddc9cf2db59887bf2aa1c724ede05f567c7af
wforget <643348094@qq.com>
2023-10-31 19:09:19 +0800

    comment

--- externals/kyuubi-jdbc-engine/src/main/scala/org/apache/kyuubi/engine/jdbc/operation/ExecuteStatement.scala
+++ externals/kyuubi-jdbc-engine/src/main/scala/org/apache/kyuubi/engine/jdbc/operation/ExecuteStatement.scala
@@ -102,7 +102,7 @@ class ExecuteStatement(
   override def validateFetchOrientation(order: FetchOrientation): Unit = {
     if (incrementalCollect && order != FetchOrientation.FETCH_NEXT) {
       throw KyuubiSQLException(s"The fetch type $order is not supported" +
-        s" of incremental collect mode.")
+        " of incremental collect mode.")
     }
     super.validateFetchOrientation(order)
   }
