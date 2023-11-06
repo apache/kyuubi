29b67c4573311dc171cd7886a67f6a5fa165cffc
Angerszhuuuu <angers.zhu@gmail.com>
2023-11-06 18:30:34 +0800

    Update PrivilegesBuilder.scala

--- extensions/spark/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/PrivilegesBuilder.scala
+++ extensions/spark/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/PrivilegesBuilder.scala
@@ -67,8 +67,7 @@ object PrivilegesBuilder {
     def mergeProjection(table: Table, plan: LogicalPlan): Unit = {
       if (projectionList.isEmpty) {
         plan match {
-          case pvm: PermanentViewMarker
-              if pvm.isSubqueryExpressionPlaceHolder || pvm.output.isEmpty =>
+          case pvm: PermanentViewMarker =>
             privilegeObjects += PrivilegeObject(table, pvm.outputColNames)
           case _ =>
             privilegeObjects += PrivilegeObject(table, plan.output.map(_.name))
@@ -133,8 +132,7 @@ object PrivilegesBuilder {
       case p =>
         for (child <- p.children) {
           child match {
-            case pvm: PermanentViewMarker
-                if pvm.isSubqueryExpressionPlaceHolder || pvm.output.isEmpty =>
+            case pvm: PermanentViewMarker =>
               buildQuery(child, privilegeObjects, projectionList, conditionList, spark)
             case _ =>
               val childCols = columnPrune(projectionList ++ conditionList, child.outputSet)
