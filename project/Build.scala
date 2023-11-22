6ba21016820b317631313a596be521821ef88e6a
davidyuan <yuanfuyuan@mafengwo.com>
2023-11-22 10:36:13 +0800

    base Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -14,24 +14,108 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import java.io.{File, PrintWriter}
+import java.nio.charset.StandardCharsets.UTF_8
 
-import java.util.Locale
-
-import sbt._
-import sbt.Keys._
-
-import sbtpomreader.{PomBuild, SbtPomKeys}
-import sbtprotoc.ProtocPlugin.autoImport._
-import sbtassembly.AssemblyPlugin.autoImport._
-
+import scala.io.Source
 import scala.util.Properties
 
+import com.simplytyped.Antlr4Plugin._
+import org.scalastyle.sbt.ScalastylePlugin.autoImport._
+import org.scalastyle.sbt.Tasks
+import sbt._
+import sbt.Classpaths.publishTask
+import sbt.Keys._
+import sbt.librarymanagement.{SemanticSelector, VersionNumber}
+import sbtpomreader.{PomBuild, SbtPomKeys}
+
+object BuildCommons {
+  private val buildLocation = file(".").getAbsoluteFile.getParentFile
+
+  val extensionsServer @ Seq(
+  kyuubiServerPlugin
+  ) = Seq(
+    "kyuubi-server-plugin"
+  ).map(ProjectRef(buildLocation, _))
+
+  val extensionsSpark @ Seq(
+  kyuubiExtenstionSparkCommon,
+  kyuubiExtensionSparkJdbcDialect,
+  kyuubiSparkAuthz,
+  kyuubiSparkAuthzShaded,
+  kyuubiSparkConnectorCommon,
+  kyuubiSparkConnectorHive,
+  kyuubiSparkConnectorTpcds,
+  kyuubiSparkConnectorTpch,
+  kyuubiSparkLineage
+  ) = Seq(
+    "kyuubi-extension-spark-common",
+    "kyuubi-extension-spark-jdbc-dialect",
+    "kyuubi-spark-authz",
+    "kyuubi-spark-authz-shaded",
+    "kyuubi-spark-connector-common",
+    "kyuubi-spark-connector-hive",
+    "kyuubi-spark-connector-tpcds",
+    "kyuubi-spark-connector-tpch",
+    "kyuubi-spark-lineage"
+  ).map(ProjectRef(buildLocation, _))
+
+  val externals @ Seq(
+  kyuubiChatEngine,
+  kyuubiDownload,
+  kyuubiFlinkSqlEngine,
+  kyuubiHiveSqlEngine,
+  kyuubiJdbcEngine,
+  kyuubiSparkSqlEngine,
+  kyuubiTrinoEngine) = Seq(
+    "kyuubi-chat-engine",
+    "kyuubi-download",
+    "kyuubi-flink-sql-engine",
+    "kyuubi-hive-sql-engine",
+    "kyuubi-jdbc-engine",
+    "kyuubi-spark-sql-engine",
+    "kyuubi-trino-engine").map(ProjectRef(buildLocation, _))
+
+  val allProjects @ Seq(
+  kyuubi,
+  kyuubiAssembly,
+  kyuubiCommon,
+  kyuubiCtl,
+  kyuubiEvents,
+  kyuubiHa,
+  kyuubiHiveBeeline,
+  kyuubiHiveJdbc,
+  kyuubiHiveJdbcShaded,
+  kyuubiMetrics,
+  kyuubiRestClient,
+  kyuubiServer,
+  kyuubiUtil,
+  kyuubiUtilScala,
+  kyuubiZookeeper,
+  _*) = Seq(
+    "kyuubi",
+    "kyuubi-assembly",
+    "kyuubi-common",
+    "kyuubi-ctl",
+    "kyuubi-events",
+    "kyuubi-ha",
+    "kyuubi-hive-beeline",
+    "kyuubi-hive-jdbc",
+    "kyuubi-hive-jdbc-shaded",
+    "kyuubi-metrics",
+    "kyuubi-rest-client",
+    "kyuubi-server",
+    "kyuubi-util",
+    "kyuubi-util-scala",
+    "kyuubi-zookeeper")
+    .map(ProjectRef(buildLocation, _)) ++ extensionsSpark ++ extensionsServer ++ externals
+}
 
 object KyuubiBuild extends PomBuild {
-
+  import BuildCommons._
   import scala.collection.mutable.Map
 
-  private val buildLocation = file(".").getAbsoluteFile.getParentFile
+  val projectsMap: Map[String, Seq[Setting[_]]] = Map.empty
 
   override val profiles = {
     val profiles = Properties.envOrNone("SBT_MAVEN_PROFILES")
@@ -40,143 +124,144 @@ object KyuubiBuild extends PomBuild {
       case Some(v) =>
         v.split("(\\s+|,)").filterNot(_.isEmpty).map(_.trim.replaceAll("-P", "")).toSeq
     }
-    if (profiles.contains("jdwp-test-debug")) {
-      sys.props.put("test.jdwp.enabled", "true")
+    if (profiles.contains("jwdp-test-debug")) {
+      sys.props.put("test.jwdp.enabled", "true")
     }
     profiles
   }
 
-  val kyuubiCommon = ProjectRef(buildLocation, "kyuubi-common")
-  val kyuubiServer = ProjectRef(buildLocation, "kyuubi-server")
+  lazy val scalaStyleRules = Project("scalaStyleRules", file("scalastyle"))
+    .settings(
+      libraryDependencies += "org.scalastyle" %% "scalastyle" % "1.0.0")
 
+  lazy val scalaStyleOnCompile = taskKey[Unit]("scalaStyleOnCompile")
 
-  val spark3Client = ProjectRef(buildLocation, "spark-3-shaded")
+  lazy val scalaStyleOnTest = taskKey[Unit]("scalaStyleOnTest")
 
-  val allProjects@Seq(
-  client, server, ha, _*
-  ) = Seq(
-    "kyuubi-rest-client", "kyuubi-servers", "worker"
-  ).map(ProjectRef(buildLocation, _)).++(Seq(kyuubiCommon, kyuubiServer, spark3Client))
-
-
-  lazy val sharedSettings = Seq(
-    SbtPomKeys.profiles.:=(profiles)
-  )
-
-  val projectsMap: Map[String, Seq[Setting[_]]] = Map.empty
-
-  def enable(settings: Seq[Setting[_]])(projectRef: ProjectRef): Any = {
-    val existingSettings = projectsMap.getOrElse(projectRef.project, Seq[Setting[_]]())
-    projectsMap.+=((projectRef.project -> (existingSettings ++ settings)))
+  val scalaStyleOnCompileConfig: String = {
+    val in = "scalastyle-config.xml"
+    val out = "scalastyle-on-compile.generated.xml"
+    val replacements = Map(
+      """customId="println" level="error"""" -> """customId="println" level="warn"""")
+    var contents = Source.fromFile(in).getLines.mkString("\n")
+    for ((k, v) <- replacements) {
+      require(contents.contains(k), s"Could not rewrite '$k' in original scalastyle config.")
+      contents = contents.replace(k, v)
+    }
+    new PrintWriter(out) {
+      write(contents)
+      close()
+    }
+    out
   }
 
-  enable(KyuubiCommon.settings)(kyuubiCommon)
-  enable(KyuubiMaster.settings)(kyuubiServer)
-  enable(KyuubiSpark3Client.settings)(spark3Client)
+  // Return a cached scalastyle task for a given configuration (usually Compile or Test)
+  private def cachedScalaStyle(config: Configuration) = Def.task {
+    val logger = streams.value.log
+    // We need a different cache dir per Configuration, otherwise they collide
+    val cacheDir = target.value / s"scalastyle-cache-${config.name}"
+    val cachedFun = FileFunction.cached(cacheDir, FilesInfo.lastModified, FilesInfo.exists) {
+      (inFiles: Set[File]) =>
+      {
+        val args: Seq[String] = Seq.empty
+        val scalaSourceV = Seq(file((config / scalaSource).value.getAbsolutePath))
+        val configV = (ThisBuild / baseDirectory).value / scalaStyleOnCompileConfig
+        val configUrlV = (config / scalastyleConfigUrl).value
+        val streamsV = ((config / streams).value: @sbtUnchecked)
+        val failOnErrorV = true
+        val failOnWarningV = false
+        val scalastyleTargetV = (config / scalastyleTarget).value
+        val configRefreshHoursV = (config / scalastyleConfigRefreshHours).value
+        val targetV = (config / target).value
+        val configCacheFileV = (config / scalastyleConfigUrlCacheFile).value
 
-  allProjects.foreach(enable(sharedSettings))
+        logger.info(s"Running scalastyle on ${name.value} in ${config.name}")
+        Tasks.doScalastyle(
+          args,
+          configV,
+          configUrlV,
+          failOnErrorV,
+          failOnWarningV,
+          scalaSourceV,
+          scalastyleTargetV,
+          streamsV,
+          configRefreshHoursV,
+          targetV,
+          configCacheFileV)
 
+        Set.empty
+      }
+    }
+
+    cachedFun(findFiles((config / scalaSource).value))
+  }
+
+  private def findFiles(file: File): Set[File] = if (file.isDirectory) {
+    file.listFiles().toSet.flatMap(findFiles) + file
+  } else {
+    Set(file)
+  }
+
+  def enableScalaStyle: Seq[sbt.Def.Setting[_]] = Seq(
+    scalaStyleOnCompile := cachedScalaStyle(Compile).value,
+    scalaStyleOnTest := cachedScalaStyle(Test).value,
+    (scalaStyleOnCompile / logLevel) := Level.Warn,
+    (scalaStyleOnTest / logLevel) := Level.Warn,
+    (Compile / compile) := {
+      scalaStyleOnCompile.value
+      (Compile / compile).value
+    },
+    (Test / compile) := {
+      scalaStyleOnTest.value
+      (Test / compile).value
+    })
+
+  // Silencer: Scala compiler plugin for warning suppression
+  // Aim: enable fatal warnings, but suppress ones related to using of deprecated APIs
+  // depends on scala version:
+  // <2.13.2 - silencer 1.7.12 and compiler settings to enable fatal warnings
+  // 2.13.2+ - no silencer and configured warnings to achieve the same
+  lazy val compilerWarningSettings: Seq[sbt.Def.Setting[_]] = Seq(
+    libraryDependencies ++= {
+      if (VersionNumber(scalaVersion.value).matchesSemVer(SemanticSelector("<2.13.2"))) {
+        val silencerVersion = "1.7.13"
+        Seq(
+          "org.scala-lang.modules" %% "scala-collection-compat" % "2.2.0",
+          compilerPlugin(
+            "com.github.ghik" % "silencer-plugin" % silencerVersion cross CrossVersion.full),
+          "com.github.ghik" % "silencer-lib" % silencerVersion % Provided cross CrossVersion.full)
+      } else {
+        Seq.empty
+      }
+    })
+
+  lazy val sharedSettings = compilerWarningSettings ++
+    (if (sys.env.contains("NOLINT_ON_COMPILE")) Nil else enableScalaStyle) ++ Seq(
+    (Compile / exportJars) := true,
+    (Test / exportJars) := false,
+    javaHome := sys.env.get("JAVA_HOME")
+      .orElse(sys.props.get("java.home").map { p =>
+        new File(p).getParentFile().getAbsolutePath()
+      })
+      .map(file),
+    publishMavenStyle := true,
+    (Compile / javacOptions) ++= Seq(
+      "-encoding",
+      UTF_8.name()),
+    SbtPomKeys.profiles := profiles)
+
+  def enable(settings: Seq[Setting[_]])(projectRef: ProjectRef): Unit = {
+    val existingSettings = projectsMap.getOrElse(projectRef.project, Seq[Setting[_]]())
+    projectsMap += (projectRef.project -> (existingSettings ++ settings))
+  }
+
+  (allProjects).foreach(enable(sharedSettings))
+
+  // TODO: move this to its upstream project.
   override def projectDefinitions(baseDirectory: File): Seq[Project] = {
-    return super.projectDefinitions(baseDirectory).map { x =>
-      if (projectsMap.exists(_._1.==(x.id))) x.settings(projectsMap(x.id): _*)
+    super.projectDefinitions(baseDirectory).map { x =>
+      if (projectsMap.exists(_._1 == x.id)) x.settings(projectsMap(x.id): _*)
       else x.settings(Seq[Setting[_]](): _*)
     }
   }
-}
-
-
-object KyuubiCommon {
-  val protocVersion = "3.19.2"
-  val protoVersion = "3.19.2"
-  lazy val settings = Seq(
-    // Setting version for the protobuf compiler. This has to be propagated to every sub-project
-    // even if the project is not using it.
-    PB.protocVersion := protocVersion,
-    // For some reason the resolution from the imported Maven build does not work for some
-    // of these dependendencies that we need to shade later on.
-    libraryDependencies ++= {
-      Seq(
-        "com.google.protobuf" % "protobuf-java" % protoVersion % "protobuf"
-      )
-    }).++ {
-    Seq(
-      Compile / PB.protoSources := Seq(
-        sourceDirectory.value / "main" / "proto"
-      ),
-      (Compile / PB.targets) := Seq(
-        PB.gens.java -> (Compile / sourceManaged).value
-        // PB.gens.plugin("grpc-java") -> (Compile / sourceManaged).value
-      )
-    )
-  }
-}
-
-object KyuubiMaster {
-  val protocVersion = "3.19.2"
-  val protoVersion = "3.19.2"
-  lazy val settings = Seq(
-    // Setting version for the protobuf compiler. This has to be propagated to every sub-project
-    // even if the project is not using it.
-    PB.protocVersion := protocVersion,
-    // For some reason the resolution from the imported Maven build does not work for some
-    // of these dependendencies that we need to shade later on.
-    libraryDependencies ++= {
-      Seq(
-        "com.google.protobuf" % "protobuf-java" % protoVersion % "protobuf"
-      )
-    }) ++ {
-    Seq(
-      Compile / PB.protoSources := Seq(
-        sourceDirectory.value / "main" / "proto"
-      ),
-      (Compile / PB.targets) := Seq(
-        PB.gens.java -> (Compile / sourceManaged).value
-        // PB.gens.plugin("grpc-java") -> (Compile / sourceManaged).value
-      )
-    )
-  }
-}
-
-object KyuubiSpark3Client {
-  lazy val settings = Seq(
-    (assembly / test) := { },
-
-    (assembly / logLevel) := Level.Info,
-
-    // Exclude `scala-library` from assembly.
-    (assembly / assemblyPackageScala / assembleArtifact) := false,
-
-    // Exclude `pmml-model-*.jar`, `scala-collection-compat_*.jar`,`jsr305-*.jar` and
-    // `netty-*.jar` and `unused-1.0.0.jar` from assembly.
-    (assembly / assemblyExcludedJars) := {
-      val cp = (assembly / fullClasspath).value
-      cp filter { v =>
-        val name = v.data.getName
-        // name.startsWith("pmml-model-") || name.startsWith("scala-collection-compat_") ||
-        //  name.startsWith("jsr305-") || name.startsWith("netty-") || name == "unused-1.0.0.jar"
-        !(name.startsWith("celeborn-") || name.startsWith("protobuf-java-") ||
-          name.startsWith("guava-") || name.startsWith("netty-") || name.startsWith("commons-lang3-"))
-      }
-    },
-
-    (assembly / assemblyShadeRules) := Seq(
-      ShadeRule.rename("com.google.protobuf.**" -> "org.apache.celeborn.shaded.com.google.protobuf.@1").inAll,
-      ShadeRule.rename("com.google.common.**" -> "org.apache.celeborn.shaded.com.google.common.@1").inAll,
-      ShadeRule.rename("io.netty.**" -> "org.apache.celeborn.shaded.io.netty.@1").inAll,
-      ShadeRule.rename("org.apache.commons.**" -> "org.apache.celeborn.shaded.org.apache.commons.@1").inAll
-    ),
-
-    (assembly / assemblyMergeStrategy) := {
-      case m if m.toLowerCase(Locale.ROOT).endsWith("manifest.mf") => MergeStrategy.discard
-      // Drop all proto files that are not needed as artifacts of the build.
-      case m if m.toLowerCase(Locale.ROOT).endsWith(".proto") => MergeStrategy.discard
-      case m if m.toLowerCase(Locale.ROOT).startsWith("meta-inf/native-image") => MergeStrategy.discard
-      // Drop netty jnilib
-      case m if m.toLowerCase(Locale.ROOT).endsWith(".jnilib") => MergeStrategy.discard
-      // rename netty native lib
-      case "META-INF/native/libnetty_transport_native_epoll_x86_64.so" => CustomMergeStrategy.rename( _ => "META-INF/native/liborg_apache_celeborn_shaded_netty_transport_native_epoll_x86_64.so" )
-      case "META-INF/native/libnetty_transport_native_epoll_aarch_64.so" => CustomMergeStrategy.rename( _ => "META-INF/native/liborg_apache_celeborn_shaded_netty_transport_native_epoll_aarch_64.so" )
-      case _ => MergeStrategy.first
-    }
-  )
 }
--- project/plugin.sbt
+++ project/plugin.sbt
@@ -3,3 +3,7 @@ addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "2.1.1")
 addSbtPlugin("com.github.sbt" % "sbt-pom-reader" % "2.4.0")
 
 addSbtPlugin("com.thesamet" % "sbt-protoc" % "1.0.6")
+
+addSbtPlugin("com.simplytyped" % "sbt-antlr4" % "0.8.3")
+
+addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
