301d44e5396853c9090d46d572319cb3150b3e96
fwang12 <fwang12@ebay.com>
2023-11-24 10:55:50 +0800

    typo

--- kyuubi-server/src/main/scala/org/apache/kyuubi/engine/KubernetesApplicationOperation.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/engine/KubernetesApplicationOperation.scala
@@ -344,7 +344,7 @@ object KubernetesApplicationOperation extends Logging {
     val containerStateToBuildAppState = appStateSource match {
       case KubernetesApplicationStateSource.CONTAINER =>
         pod.getStatus.getContainerStatuses.asScala
-          .find(_.getState == appStateContainer).map(_.getState)
+          .find(_.getName == appStateContainer).map(_.getState)
       case KubernetesApplicationStateSource.POD => None
     }
     val applicationState = containerStateToBuildAppState.map(containerStateToApplicationState)
