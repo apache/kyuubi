f4b9a12185babf91582ddf3f5fd23fd4222a6752
davidyuan <yuanfuyuan@mafengwo.com>
2023-11-26 16:32:36 +0800

    support  sbt

--- project/Build.scala
+++ project/Build.scala
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import java.io.{File, PrintWriter}
 import java.nio.charset.StandardCharsets.UTF_8
 
