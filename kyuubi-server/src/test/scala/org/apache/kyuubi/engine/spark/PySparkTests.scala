6f2b193a9519452c73313d2c92ab85480b8152c8
Fei Wang <fwang12@ebay.com>
2023-12-20 19:27:33 -0800

    ut

--- kyuubi-server/src/test/scala/org/apache/kyuubi/engine/spark/PySparkTests.scala
+++ kyuubi-server/src/test/scala/org/apache/kyuubi/engine/spark/PySparkTests.scala
@@ -135,17 +135,16 @@ class PySparkTests extends WithKyuubiServer with HiveJDBCTestHelper {
   test("Support python magic syntax for python notebook") {
     checkPythonRuntimeAndVersion()
     withSessionConf()(Map(KyuubiConf.ENGINE_SPARK_PYTHON_MAGIC_ENABLED.key -> "true"))() {
-      withMultipleConnectionJdbcStatement()({ statement =>
-        statement.executeQuery("SET kyuubi.operation.language=PYTHON")
+      withMultipleConnectionJdbcStatement()({ stmt =>
+        val statement = stmt.asInstanceOf[KyuubiStatement]
+        statement.executePython("x = [[1, 'a'], [3, 'b']]")
 
-        statement.executeQuery("x = [[1, 'a'], [3, 'b']]")
-
-        val resultSet1 = statement.executeQuery("%json x")
+        val resultSet1 = statement.executePython("%json x")
         assert(resultSet1.next())
         val output1 = resultSet1.getString("output")
         assert(output1 == "{\"application/json\":[[1,\"a\"],[3,\"b\"]]}")
 
-        val resultSet2 = statement.executeQuery("%table x")
+        val resultSet2 = statement.executePython("%table x")
         assert(resultSet2.next())
         val output2 = resultSet2.getString("output")
         assert(output2 == "{\"application/vnd.livy.table.v1+json\":{" +
@@ -158,21 +157,32 @@ class PySparkTests extends WithKyuubiServer with HiveJDBCTestHelper {
 
         Seq("table", "json", "matplot").foreach { magic =>
           val e = intercept[KyuubiSQLException] {
-            statement.executeQuery(s"%$magic invalid_value")
+            statement.executePython(s"%$magic invalid_value")
           }.getMessage
           assert(e.contains("KeyError: 'invalid_value'"))
         }
 
-        statement.executeQuery("y = [[1, 2], [3, 'b']]")
+        statement.executePython("y = [[1, 2], [3, 'b']]")
         var e = intercept[KyuubiSQLException] {
-          statement.executeQuery("%table y")
+          statement.executePython("%table y")
         }.getMessage
         assert(e.contains("table rows have different types"))
 
         e = intercept[KyuubiSQLException] {
-          statement.executeQuery("%magic_unknown")
+          statement.executePython("%magic_unknown")
         }.getMessage
         assert(e.contains("unknown magic command 'magic_unknown'"))
+      })
+    }
+
+    withSessionConf()(Map(KyuubiConf.ENGINE_SPARK_PYTHON_MAGIC_ENABLED.key -> "false"))() {
+      withMultipleConnectionJdbcStatement()({ stmt =>
+        val statement = stmt.asInstanceOf[KyuubiStatement]
+        statement.executePython("x = [[1, 'a'], [3, 'b']]")
+        val e = intercept[KyuubiSQLException] {
+          statement.executePython("%json x")
+        }.getMessage
+        assert(e.contains("SyntaxError: invalid syntax"))
       })
     }
   }
