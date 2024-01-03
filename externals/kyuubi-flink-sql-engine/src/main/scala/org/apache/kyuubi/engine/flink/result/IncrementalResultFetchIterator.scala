902877e6f37e7989b9d9cf0188d9d43ac46eb803
waywtdcc <waywtdcc@163.com>
2024-01-03 16:56:02 +0800

    [Flink] Reduce excessively long thread sleep

--- externals/kyuubi-flink-sql-engine/src/main/scala/org/apache/kyuubi/engine/flink/result/IncrementalResultFetchIterator.scala
+++ externals/kyuubi-flink-sql-engine/src/main/scala/org/apache/kyuubi/engine/flink/result/IncrementalResultFetchIterator.scala
@@ -58,7 +58,7 @@ class IncrementalResultFetchIterator(
 
   var hasNext: Boolean = true
 
-  val FETCH_INTERVAL_MS: Long = 1000
+  val FETCH_INTERVAL_MS: Long = 50
 
   // for Flink 1.16 and below, isQueryResult is not supported
   val isQueryResult: Boolean =
