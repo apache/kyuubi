f02083892b0f047e1ab5072dc739b177566a8b8e
davidyuan <yuanfuyuan@mafengwo.com>
2024-03-27 17:43:14 +0800

    Kyuubi Server HA&ZK get server from serverHosts support more strategy

--- kyuubi-hive-jdbc/src/main/java/org/apache/kyuubi/jdbc/hive/strategy/StrategyFactory.java
+++ kyuubi-hive-jdbc/src/main/java/org/apache/kyuubi/jdbc/hive/strategy/StrategyFactory.java
@@ -17,31 +17,34 @@
 
 package org.apache.kyuubi.jdbc.hive.strategy;
 
+import java.lang.reflect.Constructor;
 import org.apache.kyuubi.jdbc.hive.ZooKeeperHiveClientException;
 import org.apache.kyuubi.jdbc.hive.strategy.zk.PollingChooseStrategy;
 import org.apache.kyuubi.jdbc.hive.strategy.zk.RandomChooseStrategy;
 
-import java.lang.reflect.Constructor;
-
 public class StrategyFactory {
-    public static ChooseServerStrategy createStrategy(String strategy) throws ZooKeeperHiveClientException {
-        try {
-            switch (strategy) {
-                case "poll":
-                    return new PollingChooseStrategy();
-                case "random":
-                    return new RandomChooseStrategy();
-                default:
-                    Class<?> clazz = Class.forName(strategy);
-                    if (ChooseServerStrategy.class.isAssignableFrom(clazz)) {
-                        Constructor<? extends ChooseServerStrategy> constructor = clazz.asSubclass(ChooseServerStrategy.class).getConstructor();
-                        return constructor.newInstance();
-                    } else {
-                        throw new ZooKeeperHiveClientException("The loaded class does not implement ChooseServerStrategy");
-                    }
-            }
-        }catch (Exception e){
-            throw new ZooKeeperHiveClientException("Oops, load the chooseStrategy is wrong, please check your connection params");
-        }
+  public static ChooseServerStrategy createStrategy(String strategy)
+      throws ZooKeeperHiveClientException {
+    try {
+      switch (strategy) {
+        case "poll":
+          return new PollingChooseStrategy();
+        case "random":
+          return new RandomChooseStrategy();
+        default:
+          Class<?> clazz = Class.forName(strategy);
+          if (ChooseServerStrategy.class.isAssignableFrom(clazz)) {
+            Constructor<? extends ChooseServerStrategy> constructor =
+                clazz.asSubclass(ChooseServerStrategy.class).getConstructor();
+            return constructor.newInstance();
+          } else {
+            throw new ZooKeeperHiveClientException(
+                "The loaded class does not implement ChooseServerStrategy");
+          }
+      }
+    } catch (Exception e) {
+      throw new ZooKeeperHiveClientException(
+          "Oops, load the chooseStrategy is wrong, please check your connection params");
     }
+  }
 }
--- kyuubi-hive-jdbc/src/main/java/org/apache/kyuubi/jdbc/hive/strategy/zk/PollingChooseStrategy.java
+++ kyuubi-hive-jdbc/src/main/java/org/apache/kyuubi/jdbc/hive/strategy/zk/PollingChooseStrategy.java
@@ -17,46 +17,53 @@
 
 package org.apache.kyuubi.jdbc.hive.strategy.zk;
 
-
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import org.apache.kyuubi.jdbc.hive.ZooKeeperHiveClientException;
 import org.apache.kyuubi.jdbc.hive.strategy.ChooseServerStrategy;
 import org.apache.kyuubi.shaded.curator.framework.CuratorFramework;
 import org.apache.kyuubi.shaded.curator.framework.recipes.locks.InterProcessSemaphoreMutex;
 import org.apache.kyuubi.shaded.zookeeper.CreateMode;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class PollingChooseStrategy implements ChooseServerStrategy {
-    private static final String COUNTER_PATH_PREFIX = "/";
-    private static final String COUNTER_PATH_SUFFIX = "-counter";
-    private static final int COUNTER_RESET_VALUE = 1000;
-    @Override
-    public String chooseServer(List<String> serverHosts, CuratorFramework zkClient, String namespace) throws ZooKeeperHiveClientException {
-        String counter_path = COUNTER_PATH_PREFIX+namespace+COUNTER_PATH_SUFFIX;
-        InterProcessSemaphoreMutex lock = new InterProcessSemaphoreMutex(zkClient,counter_path);
-        try {
-            if (zkClient.checkExists().forPath(counter_path) == null ){
-                zkClient.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath(counter_path, "1".getBytes(StandardCharsets.UTF_8));
-            }
-            if (!lock.acquire(60,TimeUnit.SECONDS)) {
-                return null;
-            }
-            byte[] data = zkClient.getData().forPath(counter_path);
-            String dataStr = new String(data, StandardCharsets.UTF_8);
-            int counter = Integer.parseInt(dataStr);
-            String server = serverHosts.get(counter % serverHosts.size());
-            counter = (counter + 1) % COUNTER_RESET_VALUE; // 避免计数器溢出
-            zkClient.setData().forPath(counter_path, (counter + "").getBytes(StandardCharsets.UTF_8));
-            return server;
-        } catch (Exception e) {
-            throw new ZooKeeperHiveClientException("Oops, PollingChooseStrategy get the server is wrong!",e);
-        } finally {
-            try {
-                lock.release();
-            } catch (Exception e) {
-                throw new ZooKeeperHiveClientException("Oops,PollingChooseStrategy releasing lock is wrong!",e);
-            }
-        }
+  private static final String COUNTER_PATH_PREFIX = "/";
+  private static final String COUNTER_PATH_SUFFIX = "-counter";
+  private static final int COUNTER_RESET_VALUE = 1000;
+
+  @Override
+  public String chooseServer(List<String> serverHosts, CuratorFramework zkClient, String namespace)
+      throws ZooKeeperHiveClientException {
+    String counter_path = COUNTER_PATH_PREFIX + namespace + COUNTER_PATH_SUFFIX;
+    InterProcessSemaphoreMutex lock = new InterProcessSemaphoreMutex(zkClient, counter_path);
+    try {
+      if (zkClient.checkExists().forPath(counter_path) == null) {
+        zkClient
+            .create()
+            .creatingParentsIfNeeded()
+            .withMode(CreateMode.PERSISTENT)
+            .forPath(counter_path, "1".getBytes(StandardCharsets.UTF_8));
+      }
+      if (!lock.acquire(60, TimeUnit.SECONDS)) {
+        return null;
+      }
+      byte[] data = zkClient.getData().forPath(counter_path);
+      String dataStr = new String(data, StandardCharsets.UTF_8);
+      int counter = Integer.parseInt(dataStr);
+      String server = serverHosts.get(counter % serverHosts.size());
+      counter = (counter + 1) % COUNTER_RESET_VALUE; // 避免计数器溢出
+      zkClient.setData().forPath(counter_path, (counter + "").getBytes(StandardCharsets.UTF_8));
+      return server;
+    } catch (Exception e) {
+      throw new ZooKeeperHiveClientException(
+          "Oops, PollingChooseStrategy get the server is wrong!", e);
+    } finally {
+      try {
+        lock.release();
+      } catch (Exception e) {
+        throw new ZooKeeperHiveClientException(
+            "Oops,PollingChooseStrategy releasing lock is wrong!", e);
+      }
     }
+  }
 }
