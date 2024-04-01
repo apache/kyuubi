a10cfa5e0cb1383bfcd0aa510e41fb3cbed0fbb9
Wang, Fei <fwang12@ebay.com>
2024-04-01 13:08:00 -0700

    ut

--- externals/kyuubi-spark-sql-engine/src/main/scala/org/apache/kyuubi/engine/spark/operation/ExecuteScala.scala
+++ externals/kyuubi-spark-sql-engine/src/main/scala/org/apache/kyuubi/engine/spark/operation/ExecuteScala.scala
@@ -20,7 +20,6 @@ package org.apache.kyuubi.engine.spark.operation
 import java.io.File
 import java.util.concurrent.RejectedExecutionException
 
-import scala.reflect.internal.util.ScalaClassLoader.URLClassLoader
 import scala.tools.nsc.interpreter.Results.{Error, Incomplete, Success}
 
 import org.apache.hadoop.fs.Path
