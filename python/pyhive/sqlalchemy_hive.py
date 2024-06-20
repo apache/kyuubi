136c7b795805caded279123cbefacc0d59c007d2
wenjie.wang01 <wenjie.wang01@liulishuo.com>
2024-06-20 15:30:45 +0800

    fix bug for function get_table_names.

--- python/pyhive/sqlalchemy_hive.py
+++ python/pyhive/sqlalchemy_hive.py
@@ -377,7 +377,7 @@ class HiveDialect(default.DefaultDialect):
         query = 'SHOW TABLES'
         if schema:
             query += ' IN ' + self.identifier_preparer.quote_identifier(schema)
-        return [row[0] for row in connection.execute(text(query))]
+        return [row[1] for row in connection.execute(text(query))]
 
     def do_rollback(self, dbapi_connection):
         # No transactions for Hive
