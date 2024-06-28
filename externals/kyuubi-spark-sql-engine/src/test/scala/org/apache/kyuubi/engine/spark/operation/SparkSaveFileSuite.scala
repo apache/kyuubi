433f78cb4c3ff4fb75a29a58a606afcaa488ae94
senmiaoliu <senmiaoliu@trip.com>
2024-06-28 11:41:33 +0800

    fix style

--- externals/kyuubi-spark-sql-engine/src/test/scala/org/apache/kyuubi/engine/spark/operation/SparkSaveFileSuite.scala
+++ externals/kyuubi-spark-sql-engine/src/test/scala/org/apache/kyuubi/engine/spark/operation/SparkSaveFileSuite.scala
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.kyuubi.engine.spark.operation
 
 import org.apache.hadoop.fs.Path
