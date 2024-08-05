8acf1ce5ecef4542233e80cfa44747f6581e23d1
lijie0203 <lijie@qishudi.com>
2024-08-05 15:47:35 +0800

    [KYUUBI #6549] Correctly handle empty Java options for engines
    [KYUUBI #6549] Fix 'Could not find or load main class when launching engine'
    
    This pull request fixes #6549
    
    When obtaining configuration items, if it is null or empty, return none
    
    - [ ] Bugfix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to change)
    
    ---
    
    - [ ] This patch was not authored or co-authored using [Generative Tooling](https://www.apache.org/legal/generative-tooling.html)
    
    **Be nice. Be informative.**
    
    Closes #6556 from LiJie20190102/launch_engine.
    
    Closes #6549
    
    c57a08aff [lijie0203] [KYUUBI #6549] Fix 'Could not find or load main class when launching engine'
    642d807e2 [lijie0203] [KYUUBI #6549] Fix 'Could not find or load main class when launching engine'
    67926094c [lijie0203] [KYUUBI #6549] Fix 'Could not find or load main class when launching engine'
    4ba9fb587 [lijie0203] [KYUUBI #6549] Fix 'Could not find or load main class when launching engine'
    
    Authored-by: lijie0203 <lijie@qishudi.com>
    Signed-off-by: Cheng Pan <chengpan@apache.org>
    (cherry picked from commit edff97d3e9aa586184eb3b77e23eb104515d03d7)
    Signed-off-by: Cheng Pan <chengpan@apache.org>

--- kyuubi-server/src/main/scala/org/apache/kyuubi/engine/hive/HiveProcessBuilder.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/engine/hive/HiveProcessBuilder.scala
@@ -23,6 +23,7 @@ import java.nio.file.{Files, Paths}
 import scala.collection.mutable
 
 import com.google.common.annotations.VisibleForTesting
+import org.apache.commons.lang3.StringUtils
 
 import org.apache.kyuubi._
 import org.apache.kyuubi.config.KyuubiConf
@@ -58,7 +59,7 @@ class HiveProcessBuilder(
 
     val memory = conf.get(ENGINE_HIVE_MEMORY)
     buffer += s"-Xmx$memory"
-    val javaOptions = conf.get(ENGINE_HIVE_JAVA_OPTIONS)
+    val javaOptions = conf.get(ENGINE_HIVE_JAVA_OPTIONS).filter(StringUtils.isNotBlank(_))
     if (javaOptions.isDefined) {
       buffer += javaOptions.get
     }
