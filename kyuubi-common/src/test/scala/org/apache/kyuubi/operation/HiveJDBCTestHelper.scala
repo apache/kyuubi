39c9458026280b756e5452a48653f917be66af5e
Wang, Fei <fwang12@ebay.com>
2024-08-19 18:47:53 -0700

    suffix

--- kyuubi-common/src/test/scala/org/apache/kyuubi/operation/HiveJDBCTestHelper.scala
+++ kyuubi-common/src/test/scala/org/apache/kyuubi/operation/HiveJDBCTestHelper.scala
@@ -75,7 +75,7 @@ trait HiveJDBCTestHelper extends JDBCTestHelper {
       } else {
         "#" + jdbcVars.map(kv => kv._1 + "=" + kv._2).mkString(";")
       }
-    jdbcUrl + sessionConfStr + jdbcConfStr + jdbcVarsStr
+    jdbcUrl.stripSuffix(";") + ";" + sessionConfStr + jdbcConfStr + jdbcVarsStr
   }
 
   def withThriftClient[T](f: TCLIService.Iface => T): T = {
