d2cbcc2fa97790ceeb2bb7b22403d3a4ef5d2178
gabrywu <gabrywu@apache.org>
2024-09-11 19:36:56 +0800

    parser tests pass

---
+++ extensions/spark/kyuubi-extension-spark-3-5/src/main/scala/org/apache/kyuubi/sql/compact/CompactTableResolver.scala
@@ -0,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.sql.compact
+
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.analysis.UnresolvedRelation
+import org.apache.spark.sql.catalyst.catalog.{CatalogTable, HiveTableRelation}
+import org.apache.spark.sql.catalyst.plans.logical._
+import org.apache.spark.sql.catalyst.rules.Rule
+import org.apache.spark.sql.execution.datasources.{HadoopFsRelation, LogicalRelation}
+
+case class CompactTableResolver(sparkSession: SparkSession) extends Rule[LogicalPlan] with Logging {
+  override def apply(plan: LogicalPlan): LogicalPlan =
+    plan match {
+      case RecoverCompactTableStatement(tableParts) =>
+        RecoverCompactTable(UnresolvedRelation(CompactTableUtils.getTableIdentifier(tableParts)))
+
+      case RecoverCompactTable(SubqueryAlias(
+      _,
+      relation@LogicalRelation(
+      baseRelation: HadoopFsRelation,
+      _,
+      Some(catalogTable),
+      _))) =>
+        val originalFileLocation = CompactTableUtils.getCompactDataDir(catalogTable.storage)
+        RecoverCompactTableCommand(
+          Seq(CompactTable.mergedFilesCachedTableName),
+          originalFileLocation)
+
+      case CompactTableStatement(tableParts, targetSize, options) =>
+        CompactTable(
+          UnresolvedRelation(CompactTableUtils.getTableIdentifier(tableParts)),
+          targetSize,
+          options)
+      case CompactTable(
+      SubqueryAlias(
+      _,
+      logicalRelation@LogicalRelation(
+      baseRelation: HadoopFsRelation,
+      _,
+      Some(catalogTable),
+      _)),
+      targetSizeInBytes,
+      options) =>
+        createCacheCommand(
+          logicalRelation,
+          catalogTable,
+          targetSizeInBytes,
+          options,
+          Some(baseRelation))
+
+      case CompactTable(
+      SubqueryAlias(_, hiveTableRelation: HiveTableRelation),
+      targetSizeInBytes,
+      options) =>
+        // TODO: 参考RelationConversions把HiveTableRelation转换成LogicalRelation
+        createCacheCommand(
+          hiveTableRelation,
+          hiveTableRelation.tableMeta,
+          targetSizeInBytes,
+          options)
+      case _ => plan
+    }
+
+  private def createCacheCommand(
+                                  relation: LeafNode,
+                                  catalogTable: CatalogTable,
+                                  targetSizeInBytes: Option[Long],
+                                  options: CompactTableOption,
+                                  baseRelation: Option[HadoopFsRelation] = None): CachePerformanceViewCommand = {
+
+    val smallFileCollect = SmallFileCollect(relation, targetSizeInBytes)
+    val repartitionByExpression =
+      RepartitionByExpression(Seq(smallFileCollect.output.head), smallFileCollect, None)
+    val smallFileMerge =
+      SmallFileMerge(repartitionByExpression, options == CompactTableOptions.DryRun)
+    val originalFileLocation = CompactTableUtils.getCompactDataDir(catalogTable.storage)
+    CachePerformanceViewCommand(
+      Seq(CompactTable.mergedFilesCachedTableName),
+      smallFileMerge,
+      originalFileLocation,
+      options)
+  }
+}
