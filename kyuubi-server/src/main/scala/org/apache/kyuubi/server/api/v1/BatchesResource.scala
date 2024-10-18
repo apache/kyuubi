928e3d243f2c0e60732293e9934458eeb2ac8300
Bowen Liang <liangbowen@gf.com.cn>
2024-10-18 08:44:13 +0800

    [KYUUBI #6731] [REST] Check all required extra resource files uploaded in creating batch request
    # :mag: Description
    ## Issue References 🔗
    
    This pull request fixes #
    
    ## Describe Your Solution 🔧
    
    - check all the required extra resource files are uploaded in POST multi-part request as expected, when creating batch with REST Batch API
    
    ## Types of changes :bookmark:
    
    - [ ] Bugfix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to change)
    
    ## Test Plan 🧪
    
    #### Behavior Without This Pull Request :coffin:
    
    #### Behavior With This Pull Request :tada:
    
    #### Related Unit Tests
    
    ---
    
    # Checklist 📝
    
    - [ ] This patch was not authored or co-authored using [Generative Tooling](https://www.apache.org/legal/generative-tooling.html)
    
    **Be nice. Be informative.**
    
    Closes #6731 from bowenliang123/extra-resource-check.
    
    Closes #6731
    
    116a47ea5 [Bowen Liang] update
    cd4433a8c [Bowen Liang] update
    4852b1569 [Bowen Liang] update
    5bb2955e8 [Bowen Liang] update
    1696e7328 [Bowen Liang] update
    911a9c195 [Bowen Liang] update
    042e42d23 [Bowen Liang] update
    56dc7fb8a [Bowen Liang] update
    
    Authored-by: Bowen Liang <liangbowen@gf.com.cn>
    Signed-off-by: Bowen Liang <liangbowen@gf.com.cn>

--- kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/v1/BatchesResource.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/server/api/v1/BatchesResource.scala
@@ -26,6 +26,7 @@ import javax.ws.rs._
 import javax.ws.rs.core.MediaType
 
 import scala.collection.JavaConverters._
+import scala.collection.convert.ImplicitConversions.`collection AsScalaIterable`
 import scala.util.{Failure, Success, Try}
 import scala.util.control.NonFatal
 
@@ -200,6 +201,17 @@ private[v1] class BatchesResource extends ApiRequestContext with Logging {
       batchRequest != null,
       "batchRequest is required and please check the content type" +
         " of batchRequest is application/json")
+
+    val unUploadedExtraResourceFileNames =
+      batchRequest.getExtraResourcesMap.values.asScala.flatMap(_.split(",")).toSet.diff(
+        formDataMultiPart.getFields.values.flatten.map(_.getContentDisposition.getFileName).toSet)
+        .filter(StringUtils.isNotBlank(_))
+    require(
+      unUploadedExtraResourceFileNames.isEmpty,
+      f"required extra resource files " +
+        f"[${unUploadedExtraResourceFileNames.toList.sorted.mkString(",")}]" +
+        f" are not uploaded in the multipart form data")
+
     openBatchSessionInternal(
       batchRequest,
       isResourceFromUpload = true,
