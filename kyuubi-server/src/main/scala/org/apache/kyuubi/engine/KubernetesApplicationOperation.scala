f0c3e3134977e0c326b42043a6b641b5ba2138e6
Wang, Fei <fwang12@ebay.com>
2024-10-28 12:58:29 -0700

    prevent npe

--- kyuubi-server/src/main/scala/org/apache/kyuubi/engine/KubernetesApplicationOperation.scala
+++ kyuubi-server/src/main/scala/org/apache/kyuubi/engine/KubernetesApplicationOperation.scala
@@ -151,6 +151,7 @@ class KubernetesApplicationOperation extends ApplicationOperation with Logging {
       expireCleanUpTriggerCacheExecutor,
       () => {
         try {
+          if (cleanupTerminatedAppInfoTrigger == null) return
           cleanupTerminatedAppInfoTrigger.asMap().asScala.foreach {
             case (key, _) =>
               // do get to trigger cache eviction
@@ -266,7 +267,6 @@ class KubernetesApplicationOperation extends ApplicationOperation with Logging {
 
     if (cleanupTerminatedAppInfoTrigger != null) {
       cleanupTerminatedAppInfoTrigger.invalidateAll()
-      ThreadUtils.shutdown(expireCleanUpTriggerCacheExecutor)
       cleanupTerminatedAppInfoTrigger = null
     }
 
@@ -274,6 +274,16 @@ class KubernetesApplicationOperation extends ApplicationOperation with Logging {
       Utils.tryLogNonFatalError(client.close())
     }
     kubernetesClients.clear()
+
+    if (expireCleanUpTriggerCacheExecutor != null) {
+      ThreadUtils.shutdown(expireCleanUpTriggerCacheExecutor)
+      expireCleanUpTriggerCacheExecutor = null
+    }
+
+    if (cleanupCanceledAppPodExecutor != null) {
+      ThreadUtils.shutdown(cleanupCanceledAppPodExecutor)
+      cleanupCanceledAppPodExecutor = null
+    }
   }
 
   private class SparkEnginePodEventHandler(kubernetesInfo: KubernetesInfo)
