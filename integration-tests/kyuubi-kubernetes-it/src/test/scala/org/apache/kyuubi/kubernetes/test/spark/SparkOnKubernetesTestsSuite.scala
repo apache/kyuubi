1d1e8a0a3b122c1f4baf4af6e26a729ab23c9cdd
Cheng Pan <chengpan@apache.org>
2024-12-23 11:21:45 +0800

    [KYUUBI #6842] Bump Spark 3.5.4
    ### Why are the changes needed?
    
    Spark 3.5.4 is released https://spark.apache.org/releases/spark-release-3-5-4.html
    
    ### How was this patch tested?
    
    Pas GHA
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No
    
    Closes #6842 from pan3793/spark-3.5.4.
    
    Closes #6842
    
    0fb7ad8a0 [Cheng Pan] ga
    8eacc9c97 [Cheng Pan] Spark 3.5.4 RC2
    0721fa401 [Cheng Pan] fix
    49e98a201 [Cheng Pan] maven repo
    951db0c82 [Cheng Pan] Spark 3.5.4
    
    Authored-by: Cheng Pan <chengpan@apache.org>
    Signed-off-by: Cheng Pan <chengpan@apache.org>

--- integration-tests/kyuubi-kubernetes-it/src/test/scala/org/apache/kyuubi/kubernetes/test/spark/SparkOnKubernetesTestsSuite.scala
+++ integration-tests/kyuubi-kubernetes-it/src/test/scala/org/apache/kyuubi/kubernetes/test/spark/SparkOnKubernetesTestsSuite.scala
@@ -51,7 +51,7 @@ abstract class SparkOnKubernetesSuiteBase
     // TODO Support more Spark version
     // Spark official docker image: https://hub.docker.com/r/apache/spark/tags
     KyuubiConf().set("spark.master", s"k8s://$apiServerAddress")
-      .set("spark.kubernetes.container.image", "apache/spark:3.5.2")
+      .set("spark.kubernetes.container.image", "apache/spark:3.5.4")
       .set("spark.kubernetes.container.image.pullPolicy", "IfNotPresent")
       .set("spark.executor.instances", "1")
       .set("spark.executor.memory", "512M")
