b59a30a9f18d6e15eb11806db920e328e2da4e84
wforget <643348094@qq.com>
2025-02-14 20:52:17 +0800

    [KYUUBI #6918] Cache client ipAddress in kyuubi jdbc connection
    ### Why are the changes needed?
    
    add Utils.CLIENT_IP_ADDRESS to cache local ipAddress
    
    closes #6918
    
    ### How was this patch tested?
    
    minor fix
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No
    
    Closes #6919 from wForget/KYUUBI-6918.
    
    Closes #6918
    
    5b0efe722 [Cheng Pan] Update kyuubi-hive-jdbc/src/main/java/org/apache/kyuubi/jdbc/hive/Utils.java
    b8660c83b [wforget] address comment
    835dfb8ef [wforget] address comment
    29dd13a2e [wforget] fix style
    843b29fe8 [wforget] [KYUUBI #6918] Cache client ipAddress in kyuubi jdbc connection
    
    Lead-authored-by: wforget <643348094@qq.com>
    Co-authored-by: Cheng Pan <pan3793@gmail.com>
    Signed-off-by: wforget <643348094@qq.com>
    (cherry picked from commit 51b8e7b8eb5bd64b70a2d76a57ca5b7d52088d9a)
    Signed-off-by: wforget <643348094@qq.com>

--- kyuubi-hive-jdbc/src/main/java/org/apache/kyuubi/jdbc/hive/KyuubiConnection.java
+++ kyuubi-hive-jdbc/src/main/java/org/apache/kyuubi/jdbc/hive/KyuubiConnection.java
@@ -27,8 +27,6 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
@@ -835,11 +833,7 @@ public class KyuubiConnection implements SQLConnection, KyuubiLoggable {
           "set:hiveconf:hive.server2.thrift.resultset.default.fetch.size",
           Integer.toString(fetchSize));
     }
-    try {
-      openConf.put("kyuubi.client.ipAddress", InetAddress.getLocalHost().getHostAddress());
-    } catch (UnknownHostException e) {
-      LOG.debug("Error getting Kyuubi session local client ip address", e);
-    }
+    openConf.put("kyuubi.client.ipAddress", Utils.CLIENT_IP_ADDRESS);
     openConf.put(Utils.KYUUBI_CLIENT_VERSION_KEY, Utils.getVersion());
     openReq.setConfiguration(openConf);
 
