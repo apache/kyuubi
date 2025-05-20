abf947a7ac175a8e9d45d6035619027129294ce9
davidyuan <yuanfuyuan@mafengwo.com>
2025-05-20 14:53:52 +0800

    [KYUUBI #7065] [#7066] Iceberg Support add partition field check
    #7066
    ### Why are the changes needed?
    
    Iceberg missing some check, this pr try to fix add partition field check
    
    ### How was this patch tested?
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    Closes #7065 from davidyuan1223/icerberg_authz.
    
    Closes #7065
    
    be2684671 [davidyuan] update
    231ed3356 [davidyuan] sort spi file
    6d2a5bf20 [davidyuan] sort spi file
    bc21310cc [davidyuan] update
    52ca367f1 [davidyuan] update
    
    Authored-by: davidyuan <yuanfuyuan@mafengwo.com>
    Signed-off-by: Kent Yao <yao@apache.org>

--- extensions/spark/kyuubi-spark-authz/src/test/scala/org/apache/kyuubi/plugin/spark/authz/gen/IcebergCommands.scala
+++ extensions/spark/kyuubi-spark-authz/src/test/scala/org/apache/kyuubi/plugin/spark/authz/gen/IcebergCommands.scala
@@ -58,11 +58,52 @@ object IcebergCommands extends CommandSpecs[TableCommandSpec] {
     TableCommandSpec(cmd, Seq(td), opType = OperationType.ALTERTABLE_PROPERTIES)
   }
 
+  val AddPartitionFiled = {
+    val cmd = "org.apache.spark.sql.catalyst.plans.logical.AddPartitionField"
+    val tableDesc =
+      TableDesc(
+        "table",
+        classOf[ArrayBufferTableExtractor],
+        comment = "Iceberg")
+    TableCommandSpec(cmd, Seq(tableDesc), opType = OperationType.ALTERTABLE_PROPERTIES)
+  }
+
+  val DropPartitionField = {
+    val cmd = "org.apache.spark.sql.catalyst.plans.logical.DropPartitionField"
+    AddPartitionFiled.copy(cmd)
+  }
+
+  val ReplacePartitionField = {
+    val cmd = "org.apache.spark.sql.catalyst.plans.logical.ReplacePartitionField"
+    AddPartitionFiled.copy(cmd)
+  }
+
+  val WriteDistributionAndOrdering = {
+    val cmd = "org.apache.spark.sql.catalyst.plans.logical.SetWriteDistributionAndOrdering"
+    AddPartitionFiled.copy(cmd)
+  }
+
+  val SetIdentifierFields = {
+    val cmd = "org.apache.spark.sql.catalyst.plans.logical.SetIdentifierFields"
+    AddPartitionFiled.copy(cmd)
+  }
+
+  val DropIdentifierFields = {
+    val cmd = "org.apache.spark.sql.catalyst.plans.logical.DropIdentifierFields"
+    AddPartitionFiled.copy(cmd)
+  }
+
   override def specs: Seq[TableCommandSpec] = Seq(
     CallProcedure,
     DeleteFromIcebergTable,
     UpdateIcebergTable,
     MergeIntoIcebergTable,
+    AddPartitionFiled,
+    DropPartitionField,
+    ReplacePartitionField,
+    WriteDistributionAndOrdering,
+    SetIdentifierFields,
+    DropIdentifierFields,
     MergeIntoIcebergTable.copy(classname =
       "org.apache.spark.sql.catalyst.plans.logical.UnresolvedMergeIntoIcebergTable"))
 }
