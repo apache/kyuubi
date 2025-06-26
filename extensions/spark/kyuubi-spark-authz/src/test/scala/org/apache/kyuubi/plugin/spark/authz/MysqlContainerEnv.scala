31bbb536f295e4c544e8a53098e3dcf85472d5f6
davidyuan <yuanfuyuan@mafengwo.com>
2025-06-26 10:11:43 +0800

    [KYUUBI #7100] [#7099] Ranger Support Check Iceberg Alter Table Command & Change Iceberg Test Use Jdbc Catalog
    Parent Issue #7040
    Support Check Iceberg Alter Table Command
    ### Why are the changes needed?
    
    - [x] Alter Table Rename To
    - [x] Alter Table Set Properties
    - [x] Alter Table Unset Properties
    - [x] Alter Table Add Column
    - [x] Alter Table Rename Column
    - [x] Alter Table Alter Column
    - [x] Alter Table Drop Column
    
    ### How was this patch tested?
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    Closes #7100 from davidyuan1223/iceberg_alter_table_check.
    
    Closes #7100
    
    4be2210f1 [davidyuan] update
    53eda10eb [davidyuan] update
    
    Authored-by: davidyuan <yuanfuyuan@mafengwo.com>
    Signed-off-by: Kent Yao <yao@apache.org>

---
+++ extensions/spark/kyuubi-spark-authz/src/test/scala/org/apache/kyuubi/plugin/spark/authz/MysqlContainerEnv.scala
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.kyuubi.plugin.spark.authz
+
+import com.dimafeng.testcontainers.MySQLContainer
+import org.testcontainers.utility.DockerImageName
+
+trait MysqlContainerEnv {
+
+  val containerDef: MySQLContainer = MySQLContainer.Def(
+    dockerImageName = DockerImageName.parse("mysql:5.7"),
+    databaseName = "hive_metastore",
+    username = "root",
+    password = "123456")
+    .createContainer()
+
+  def startEngine(): Unit = {
+    containerDef.start()
+  }
+
+  def stopEngine(): Unit = {
+    containerDef.stop()
+  }
+}
