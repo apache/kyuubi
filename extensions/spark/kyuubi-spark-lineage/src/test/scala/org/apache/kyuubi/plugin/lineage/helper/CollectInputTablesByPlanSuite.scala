b566c0a4532370510791b6f29e7bb8d7226b3098
chenliang.lu <chenlianglu@tencent.com>
2025-09-02 08:08:40 +0800

    [KYUUBI #7183][LINEAGE] Support collect all the input tables by the plan

--- extensions/spark/kyuubi-spark-lineage/src/test/scala/org/apache/kyuubi/plugin/lineage/helper/CollectInputTablesByPlanSuite.scala
+++ extensions/spark/kyuubi-spark-lineage/src/test/scala/org/apache/kyuubi/plugin/lineage/helper/CollectInputTablesByPlanSuite.scala
@@ -20,8 +20,6 @@ package org.apache.kyuubi.plugin.lineage.helper
 import org.apache.spark.SparkConf
 import org.apache.spark.kyuubi.lineage.LineageConf.COLLECT_INPUT_TABLES_BY_PLAN_ENABLED
 
-import org.apache.kyuubi.plugin.lineage.Lineage
-
 class CollectInputTablesByPlanSuite extends SparkSQLLineageParserHelperSuite {
 
   override def catalogName: String = {
@@ -31,32 +29,5 @@ class CollectInputTablesByPlanSuite extends SparkSQLLineageParserHelperSuite {
   override def sparkConf(): SparkConf = {
     super.sparkConf()
       .set(COLLECT_INPUT_TABLES_BY_PLAN_ENABLED.key, "true")
-  }
-
-  test("columns lineage extract - MergeIntoTable") {
-    val ddls =
-      """
-        |create table v2_catalog.db.target_t(id int, name string, price float)
-        |create table v2_catalog.db.source_t(id int, name string, price float)
-        |create table v2_catalog.db.pivot_t(id int, price float)
-        |""".stripMargin
-    ddls.split("\n").filter(_.nonEmpty).foreach(spark.sql(_).collect())
-    withTable("v2_catalog.db.target_t", "v2_catalog.db.source_t", "v2_catalog.db.pivot_t") { _ =>
-      val ret0 = extractLineageWithoutExecuting("MERGE INTO v2_catalog.db.target_t AS target " +
-        "USING v2_catalog.db.source_t AS source " +
-        "ON target.id = source.id " +
-        "WHEN MATCHED THEN " +
-        "  UPDATE SET target.name = source.name, target.price = source.price " +
-        "WHEN NOT MATCHED THEN " +
-        "  INSERT (id, name, price) VALUES (cast(source.id as int), source.name, source.price)")
-      assert(ret0 == Lineage(
-        List("v2_catalog.db.source_t", "v2_catalog.db.target_t"),
-        List("v2_catalog.db.target_t"),
-        List(
-          ("v2_catalog.db.target_t.id", Set("v2_catalog.db.source_t.id")),
-          ("v2_catalog.db.target_t.name", Set("v2_catalog.db.source_t.name")),
-          ("v2_catalog.db.target_t.price", Set("v2_catalog.db.source_t.price")))))
-    }
-
   }
 }
