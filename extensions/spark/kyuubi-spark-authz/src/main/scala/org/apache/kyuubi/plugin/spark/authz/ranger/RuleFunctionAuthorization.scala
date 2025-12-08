cbb0225d33c875f616dd63276e65e6bee73a2eae
Yaguang Jia <yaguangjia@tencent.com>
2025-12-08 17:22:53 +0800

    fix scalastyle check

--- extensions/spark/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/ranger/RuleFunctionAuthorization.scala
+++ extensions/spark/kyuubi-spark-authz/src/main/scala/org/apache/kyuubi/plugin/spark/authz/ranger/RuleFunctionAuthorization.scala
@@ -32,7 +32,6 @@ case class RuleFunctionAuthorization(spark: SparkSession) extends (LogicalPlan =
   final val AUTHZ_UDF_KEY: String = "spark.kyuubi.conf.authz.udf.enabled"
   private val authzUDFEnabled: Boolean =
     spark.conf.getOption(AUTHZ_UDF_KEY).exists(_.equalsIgnoreCase("true"))
-  
   override def apply(plan: LogicalPlan): Unit = {
     if (!authzUDFEnabled) {
       return
